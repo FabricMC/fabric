2b462b0c51240ab48a95e09162f49e2622a0aa3b
Modmuss50 <modmuss50@gmail.com>
2018-12-24 22:15:53 +0000

    Item Group API (#44)
    * Inital proof of concept on the item group api
    
    * Some changes
    
    * Show hotbar tab + fix bug with inv slots not clearing out
    
    * Better button textures + docs + tweaks/auto format
    
    * Add an api to provide the stack to display and expand the example mod
    
    * Use COMMON_GROUPS in all places
    
    * Add a tooltip showing the page numbers

---
+++ src/main/java/net/fabricmc/fabric/mixin/client/itemgroup/MixinItemGroup.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.client.itemgroup;
+
+import net.fabricmc.fabric.client.itemgroup.FabricCreativeGuiComponents;
+import net.fabricmc.fabric.client.itemgroup.ItemGroupExtensions;
+import net.minecraft.item.ItemGroup;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemGroup.class)
+public abstract class MixinItemGroup implements ItemGroupExtensions {
+
+	@Shadow
+	@Final
+	@Mutable
+	public static ItemGroup[] GROUPS;
+
+	@Shadow
+	public abstract int getId();
+
+	@Shadow
+	public abstract boolean isTopRow();
+
+	@Shadow
+	@Final
+	private int id;
+
+	@Override
+	public void fabric_expandArray() {
+		ItemGroup[] tempGroups = GROUPS;
+		GROUPS = new ItemGroup[GROUPS.length + 1];
+		for (ItemGroup group : tempGroups) {
+			GROUPS[group.getId()] = group;
+		}
+	}
+
+	@Inject(method = "isTopRow", cancellable = true, at = @At("HEAD"))
+	private void isTopRow(CallbackInfoReturnable<Boolean> info) {
+		if (getId() > 11) {
+			info.setReturnValue((id - 12) % (12 - FabricCreativeGuiComponents.COMMON_GROUPS.size()) < 4);
+		}
+	}
+
+	@Inject(method = "getColumn", cancellable = true, at = @At("HEAD"))
+	private void getColumn(CallbackInfoReturnable<Integer> info) {
+		if (getId() > 11) {
+			if (isTopRow()) {
+				info.setReturnValue((id - 12) % (12 - FabricCreativeGuiComponents.COMMON_GROUPS.size()));
+			} else {
+				info.setReturnValue((id - 12) % (12 - FabricCreativeGuiComponents.COMMON_GROUPS.size()) - 4);
+			}
+
+		}
+	}
+}
