e3b6ba3c7dd2730c10dac349c614ba9e329f9bf5
grondag <grondag@gmail.com>
2019-01-18 08:01:53 -0800

    Make vertex builder less stateful/complicated

--- src/main/java/net/fabricmc/fabric/api/client/model/fabric/VertexBuilder.java
+++ src/main/java/net/fabricmc/fabric/api/client/model/fabric/VertexBuilder.java
@@ -49,7 +49,7 @@ public interface VertexBuilder {
      * Call to explicitly flush a completed quad. 
      * Useful in dynamic rendering contexts to flush the last quad.
      * Will be called implicitly when {@link #begin(ModelMaterial)} is
-     * called or when this is a {@link FastVertexBuilder} instance and a model is built.
+     * called.
      */
     void end();
     
@@ -68,46 +68,36 @@ public interface VertexBuilder {
     
     /**
      * Enables bulk vertex data transfer using the standard Minecraft vertex formats.
-     * This method or {@link #putStandardQuadData(int[], int, boolean)} are preferable
-     * whenever caller's vertex representation makes them feasible.<p>
-     * 
-     * When this method (or the full quad variant) are used, it is not valid to call
-     * {@link #vertex(float, float, float)} or the per-element additive methods including
-     * {@link #colorTexture()} and {@link #brightness(int)}.
-     * 
-     * However, it IS valid to call {@link #setBrightness()}, {@link #setColor()} and
-     * other set... methods before calling {@link #end()} or {@link #begin(ModelMaterial)}.
-     * 
-     * @param vertexData array containing vertex data in standard format
-     * @param startIndex index to start of vertex data in array
-     * @param isItem If true, last (7th) integer will be interpreted as packed
-     * vertex normals.  If false, (block format) last integer will be interpreted as brightness.
+     * This method should be performant whenever caller's vertex representation makes it feasible
      */
-    void putStandardVertexData(int[] vertexData, int startIndex, boolean isItem);
+    void putStandardQuadData(int[] quadData, int startIndex, boolean isItem);
     
     /**
-     * Same purpose as {@link #putStandardVertexData(int[], int, boolean)} but accepts
-     * entire quad at once.  Requires that vertex data be closely packed into 28 
-     * contiguous array elements.
-     * 
-     * {@link VertexBuilder} implementations should override this default
-     * implementation if it offers a performance benefit.
-     */
-    default void putStandardQuadData(int[] quadData, int startIndex, boolean isItem) {
-        putStandardVertexData(quadData, startIndex, isItem);
-        putStandardVertexData(quadData, startIndex + 7, isItem);
-        putStandardVertexData(quadData, startIndex + 14, isItem);
-        putStandardVertexData(quadData, startIndex + 21, isItem);
-    }
-    
-    /**
-     * Starts a new vertex and sets the geometric vertex position, 
+     * Sets the geometric vertex position for the given vertex, 
      * relative to block origin. (0,0,0).  Minecraft rendering is designed
      * for models that fit within a single block space and is recommended 
      * that coordinates remain in the 0-1 range, with multi-block meshes
      * split into multiple per-block models.
      */
-    void vertex(float x, float y, float z);
+    void setVertex(int vertexIndex, float x, float y, float z);
+    
+    /**
+     * All-at-once per-vertex method for the major vertex components.
+     * Only handles first texture layer.
+     */
+    default void setVertex(int vertexIndex, float x, float y, float z, int color, float u, float v) {
+        setVertex(vertexIndex, x, y, z);
+        setColorTexture(vertexIndex, color, u, v);
+    }
+    
+    /**
+     * All-at-once per-vertex method for the major vertex components.
+     * Includes normals. Only handles first texture layer.
+     */
+    default void setVertex(int vertexIndex, float x, float y, float z, int color, float u, float v, float normX, float normY, float normZ) {
+        setVertex(vertexIndex, x, y, z, color, u, v);
+        setVertexNormal(vertexIndex, normX, normY, normZ);
+    }
     
     /**
      * Adds a vertex normal. Models that have per-vertex
@@ -120,10 +110,10 @@ public interface VertexBuilder {
      * diffuse lighting - modifying vertex color(s) or packing normals in the vertex 
      * buffer as appropriate for the rendering method/vertex format in effect.<p>
      */
-    void normal(float x, float y, float z);
+    void setVertexNormal(int vertexIndex, float x, float y, float z);
     
     /**
-     * Adds a vertex color and texture coordinate.<p>
+     * Sets vertex color and texture coordinate for the given vertex and texture layer.<p>
      * 
      * TEXTURE COORDINATES MUST BE PRE-BAKED. The {@link ModelRenderer} implementation
      * will not handle texture interpolation or adjustment.<p>
@@ -132,28 +122,41 @@ public interface VertexBuilder {
      * The {@link ModelRenderer} implementation will handle any component 
      * swizzling needed for OpenGL or to account for local endian-ness.<p>
      */
-    void colorTexture(int color, float u, float v);
-    
-    /**
-     * Adds a minimum lightmap to be used when emissive rendering is enabled. 
-     */
-    void brightness(int brightness);
-    
-    /**
-     * Single-call for multiple attributes. Some implementations may
-     * override this if it offers an optimization benefit.
-     */
-    default void colorTextureBrightness(int color, float u, float v, int brightness) {
-        colorTexture(color, u, v);
-        brightness(brightness);
+    default void setColorTexture(int vertexIndex, int textureLayer, int color, float u, float v) {
+        setColor(vertexIndex, textureLayer, color);
+        setTexture(vertexIndex, textureLayer, u, v);
     }
     
     /**
-     * Indexed texture coordinate input. 
+     * Concise version of {@link #setColorTexture(int, int, int, float, float)} for single-layer models.
+     */
+    default void setColorTexture(int vertexIndex, int color, float u, float v) {
+        setColor(vertexIndex, 0, color);
+        setTexture(vertexIndex, 0, u, v);
+    }
+    
+    /**
+     * Per-vertex brightness input.
+     */
+    void setBrightness(int vertexIndex, int brightness);
+    
+    /**
+     * Per-vertex color coordinate input. 
      * 
-     * @param vertexIndex  Vertex must have already been created via {@link #vertex()} or
-     * one of the direct transfer methods: {@link #putStandardVertexData(int[], int, boolean)} or
-     * {@link #putStandardQuadData(int[], int, boolean)}<p>
+     * @param layerIndex Texture layer.  Must be within the range implied by the
+     * texture depth of the material.
+     */
+    void setColor(int vertexIndex, int layerIndex, int color);
+    
+    /**
+     * Concise version of {@link #setColor(int, int, int)} for single-layer models.
+     */
+    default void setColor(int vertexIndex, int color) {
+        setColor(vertexIndex, 0, color);
+    }
+    
+    /**
+     * Per-vertex texture coordinate input. 
      * 
      * @param layerIndex Texture layer.  Must be within the range implied by the
      * texture depth of the material.
@@ -164,14 +167,14 @@ public interface VertexBuilder {
     void setTexture(int vertexIndex, int layerIndex, float u, float v);
     
     /**
-     * Concise version of {@link #setTexture(int, float, float)} for single-layer models.
+     * Concise version of {@link #setTexture(int, int, float, float)} for single-layer models.
      */
     default void setTexture(int vertexIndex, float u, float v) {
         setTexture(vertexIndex, 0, u, v);
     }
     
     /**
-     * Indexed color input - accepts all four vertices at once.<p>
+     * Quad color input - accepts all four vertices at once.<p>
      * 
      * All vertices must have already been created via {@link #vertex()} or
      * one of the direct transfer methods: {@link #putStandardVertexData(int[], int, boolean)} or
@@ -185,21 +188,27 @@ public interface VertexBuilder {
      * @param c2  third vertex color
      * @param c3  fourth vertex color
      */
-    void setColor(int layerIndex, int c0, int c1, int c2, int c3);
+    default void setQuadColor(int layerIndex, int c0, int c1, int c2, int c3) {
+        setColor(0, layerIndex, c0);
+        setColor(1, layerIndex, c1);
+        setColor(2, layerIndex, c2);
+        setColor(3, layerIndex, c3);
+    }
     
     /**
      * Concise version of {@link #setColor(int, int, int, int, int)} for single-layer models.
      */
-    default void setColor(int c0, int c1, int c2, int c3) {
-        setColor(0, c0, c1, c2, c3);
+    default void setQuadColor(int c0, int c1, int c2, int c3) {
+        setQuadColor(0, c0, c1, c2, c3);
     }
     
     /**
-     * Indexed brightness input - accepts all four vertices at once.<p>
-     * 
-     * All vertices must have already been created via {@link #vertex()} or
-     * one of the direct transfer methods: {@link #putStandardVertexData(int[], int, boolean)} or
-     * {@link #putStandardQuadData(int[], int, boolean)}<p>
+     * Quad brightness input - accepts all four vertices at once.<p>
      */
-    void setBrightness(int b0, int b1, int b2, int b3);
+    default void setQuadBrightness(int b0, int b1, int b2, int b3) {
+        setBrightness(0, b0);
+        setBrightness(1, b1);
+        setBrightness(2, b2);
+        setBrightness(3, b3);
+    }
 }
