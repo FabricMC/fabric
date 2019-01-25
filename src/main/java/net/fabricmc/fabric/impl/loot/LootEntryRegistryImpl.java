b88969cf131f8cae58b66e0c755559805f54e6ff
Juuxel <kasperi.kauppi@gmail.com>
2019-01-25 23:53:11 +0100

    (#59) Loot entry extensions

---
+++ src/main/java/net/fabricmc/fabric/impl/loot/LootEntryRegistryImpl.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2016, 2017, 2018 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.impl.loot;
+
+import net.fabricmc.fabric.api.loot.LootEntryRegistry;
+import net.minecraft.world.loot.entry.LootEntries;
+import net.minecraft.world.loot.entry.LootEntry;
+
+import java.util.function.Consumer;
+
+public final class LootEntryRegistryImpl implements LootEntryRegistry {
+	private static Consumer<LootEntry.Serializer<?>> registerFunction;
+	public static final LootEntryRegistryImpl INSTANCE = new LootEntryRegistryImpl();
+
+	static {
+		loadLootEntries();
+	}
+
+	private LootEntryRegistryImpl() {}
+
+	@Override
+	public void registerType(LootEntry.Serializer<?> serializer) {
+		registerFunction.accept(serializer);
+	}
+
+	public static void setRegisterFunction(Consumer<LootEntry.Serializer<?>> registerFunction) {
+		LootEntryRegistryImpl.registerFunction = registerFunction;
+	}
+
+	private static void loadLootEntries() {
+		try { Class.forName(LootEntries.class.getCanonicalName()); } catch (ClassNotFoundException e) {}
+	}
+}
