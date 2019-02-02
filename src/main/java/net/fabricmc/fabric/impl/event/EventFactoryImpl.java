28820f42ff741f55f94186b165189e89be7df3e2
asie <kontakt@asie.pl>
2019-02-02 12:53:40 +0100

    boost performance of empty invoker generator

--- src/main/java/net/fabricmc/fabric/impl/event/EventFactoryImpl.java
+++ src/main/java/net/fabricmc/fabric/impl/event/EventFactoryImpl.java
@@ -6,9 +6,7 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandleProxies;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
-import java.lang.reflect.Array;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
+import java.lang.reflect.*;
 import java.util.function.Function;
 
 public final class EventFactoryImpl {
@@ -43,19 +41,14 @@ public final class EventFactoryImpl {
 			throw new IllegalStateException("No virtual methods in " + handlerClass + "; cannot build empty invoker!");
 		}
 
-		MethodHandle handler;
+		Object defValue = null;
 
 		try {
 			// concert to mh, determine its type without the "this" reference
 			MethodHandle target = MethodHandles.lookup().unreflect(funcIfMethod);
 			MethodType type = target.type().dropParameterTypes(0, 1);
 
-			// build no-op mh, first just to return the default value as if invokerSetup.apply(T[0]) was called (desc: ()R, R = invoker return type)
-			if (type.returnType() == void.class) {
-				// construct void yielding mh via null-const + explicit cast to ()V
-				handler = MethodHandles.constant(Object.class, null);
-				handler = MethodHandles.explicitCastArguments(handler, MethodType.methodType(void.class));
-			} else {
+			if (type.returnType() != void.class) {
 				// determine default return value by invoking invokerSetup.apply(T[0]) with all-jvm-default args (null for refs, false for boolean, etc.)
 				// explicitCastArguments is being used to cast Object=null to the jvm default value for the correct type
 
@@ -70,20 +63,15 @@ public final class EventFactoryImpl {
 				args[0] = invokerSetup.apply((T[]) Array.newInstance(handlerClass, 0));
 
 				// retrieve default by invoking invokerSetup.apply(T[0]).targetName(def,def,...)
-				Object defValue = objTarget.invokeWithArguments(args);
-
-				// wrap def value as constant-yielding mh
-				handler = MethodHandles.constant(type.returnType(), defValue);
+				defValue = objTarget.invokeWithArguments(args);
 			}
-
-			// add dummy args to match the required desc
-			handler = MethodHandles.dropArguments(handler, 0, type.parameterList());
-			//handler = MethodHandles.explicitCastArguments(handler, type);
 		} catch (Throwable t) {
 			throw new RuntimeException(t);
 		}
 
-		// convert MH to object implementing the handler interface
-		return MethodHandleProxies.asInterfaceInstance(handlerClass, handler);
+		final Object returnValue = defValue;
+		//noinspection unchecked
+		return (T) Proxy.newProxyInstance(EventFactoryImpl.class.getClassLoader(), new Class[] { handlerClass },
+			(proxy, method, args) -> returnValue);
 	}
 }
