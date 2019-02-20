bde6adefa36a36337e73af18c9d64e43c29e2318
grondag <grondag@gmail.com>
2019-02-19 19:30:24 -0800

    Missed one

--- src/main/java/net/fabricmc/fabric/impl/client/model/DamageModel.java
+++ src/main/java/net/fabricmc/fabric/impl/client/model/DamageModel.java
@@ -16,29 +16,21 @@
 
 package net.fabricmc.fabric.impl.client.model;
 
-import java.util.List;
 import java.util.Random;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import net.fabricmc.fabric.api.client.model.fabric.FabricBakedModel;
 import net.fabricmc.fabric.api.client.model.fabric.ForwardingBakedModel;
-import net.fabricmc.fabric.api.client.model.fabric.ForwardingQuadMaker;
-import net.fabricmc.fabric.api.client.model.fabric.Mesh;
-import net.fabricmc.fabric.api.client.model.fabric.ModelHelper;
-import net.fabricmc.fabric.api.client.model.fabric.QuadMaker;
+import net.fabricmc.fabric.api.client.model.fabric.MutableQuadView;
 import net.fabricmc.fabric.api.client.model.fabric.RenderContext;
+import net.fabricmc.fabric.api.client.model.fabric.RenderContext.QuadTransform;
 import net.fabricmc.fabric.api.client.model.fabric.RenderMaterial;
 import net.fabricmc.fabric.api.client.model.fabric.RendererAccess;
 import net.fabricmc.fabric.api.client.model.fabric.TerrainBlockView;
-import net.fabricmc.fabric.api.client.model.fabric.TextureHelper;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.render.model.BakedModel;
-import net.minecraft.client.render.model.BakedQuad;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 
 /**
  * Specialized model wrapper that implements a general-purpose 
@@ -50,91 +42,29 @@ import net.minecraft.util.math.Direction;
 public class DamageModel extends ForwardingBakedModel {
     static final RenderMaterial DAMAGE_MATERIAL = RendererAccess.INSTANCE.hasRenderer() ? RendererAccess.INSTANCE.getRenderer().materialFinder().find() : null;
     
-    private final DamageContext damageContext;
-    private BakedModel wrappedModel;
-    
-    public DamageModel() {
-        damageContext = new DamageModel.DamageContext();
-    }
+    private DamageTransform damageTransform = new DamageTransform();
     
     public void prepare(BakedModel wrappedModel, Sprite sprite, BlockState blockState, BlockPos blockPos) {
-        this.wrappedModel = wrappedModel;
-        this.damageContext.damageQuad.damageSprite = sprite;
-        this.damageContext.damageBlockState = blockState;
-        this.damageContext.damageBlockPos = blockPos;
+        this.damageTransform.damageSprite = sprite;
+        this.wrapped = wrappedModel;
     }
     
     @Override
-    protected BakedModel wrapped() {
-        return wrappedModel;
+    public void emitBlockQuads(TerrainBlockView blockView, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
+        context.pushTransform(damageTransform);
+        ((FabricBakedModel)wrapped).emitBlockQuads(blockView, state, pos, randomSupplier, context);
+        context.popTransform();
     }
     
-    @Override
-    public void produceBlockQuads(TerrainBlockView blockView, BlockState state, BlockPos pos, Supplier<Random> randomSupplier, RenderContext context) {
-        damageContext.wrappedContext = context;
-        ((FabricBakedModel)wrappedModel).produceBlockQuads(blockView, state, pos, randomSupplier, damageContext);
-    }
-    
-    private static class DamageContext implements RenderContext {
-        private final DamageQuadMaker damageQuad = new DamageQuadMaker();
-        private final Random random = new Random();
-        private BlockState damageBlockState;
-        private BlockPos damageBlockPos;
-        RenderContext wrappedContext;
-        
-        @Override
-        public BiConsumer<Mesh, Consumer<QuadMaker>> meshConsumer() {
-            return (mesh, editor) -> {
-                mesh.forEach(q -> {
-                    damageQuad.wrappedQuadMaker = wrappedContext.quad(q.material());
-                    q.copyTo(damageQuad.wrappedQuadMaker);
-                    if(editor == null) {
-                        damageQuad.emit();
-                    } else {
-                        editor.accept(damageQuad);
-                    }
-                });
-            };
-        }
-    
-        @Override
-        public Consumer<BakedModel> fallbackConsumer() {
-            return model -> {
-                final long seed = damageBlockState.getRenderingSeed(damageBlockPos);
-                for(int i = 0; i < 7; i++) {
-                    Direction face = ModelHelper.faceFromIndex(i);
-                    random.setSeed(seed);
-                    List<BakedQuad> quads = model.getQuads(damageBlockState, face, random);
-                    final int limit = quads.size();
-                    for(int j = 0; j < limit; j++) {
-                        damageQuad.wrappedQuadMaker = wrappedContext.quad(DAMAGE_MATERIAL);
-                        BakedQuad q = quads.get(i);
-                        damageQuad.fromVanilla(q.getVertexData(), 0, false);
-                        damageQuad.cullFace(face);
-                        damageQuad.nominalFace(q.getFace());
-                        damageQuad.emit();
-                    }
-                }
-            };
-        }
-    
-        @Override
-        public QuadMaker quad(RenderMaterial material) {
-            damageQuad.wrappedQuadMaker = wrappedContext.quad(material);
-            return damageQuad;
+    private static class DamageTransform implements QuadTransform {
         private Sprite damageSprite;
         
         @Override
-        protected QuadMaker wrapped() {
-            return wrappedQuadMaker;
-        }
-        
-        @Override
-        public void emit() {
-            wrappedQuadMaker.material(DAMAGE_MATERIAL);
-            TextureHelper.bakeSprite(wrappedQuadMaker, 0, damageSprite, TextureHelper.LOCK_UV);
-            wrappedQuadMaker.colorIndex(-1);
-            wrappedQuadMaker.emit();
+        public boolean transform(MutableQuadView quad) {
+            quad.material(DAMAGE_MATERIAL);
+            quad.sprite(0, damageSprite, MutableQuadView.BAKE_LOCK_UV);
+            quad.colorIndex(-1);
+            return true;
         }
     }
 }
