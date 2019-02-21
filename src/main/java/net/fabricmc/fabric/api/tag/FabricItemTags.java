34f69adeefa582366714a8635577eceb93ae2092
Meredith Espinosa <Boundarybreaker@users.noreply.github.com>
2019-02-21 13:58:21 -0800

    add shears to item tag registration

--- src/main/java/net/fabricmc/fabric/api/tag/FabricItemTags.java
+++ src/main/java/net/fabricmc/fabric/api/tag/FabricItemTags.java
@@ -29,6 +29,7 @@ public class FabricItemTags {
 	public static final Tag<Item> PICKAXES = register("pickaxes");
 	public static final Tag<Item> SHOVELS = register("shovels");
 	public static final Tag<Item> SWORDS = register("swords");
+	public static final Tag<Item> SHEARS = register("shears");
 
 	private FabricItemTags() {
 
