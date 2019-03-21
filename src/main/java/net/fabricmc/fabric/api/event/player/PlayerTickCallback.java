4447989fd98ca87e714b93d3ed0d7226db9b17be
Meredith Espinosa <goclonefilms@gmail.com>
2019-03-20 22:58:22 -0700

    update javadocs

--- src/main/java/net/fabricmc/fabric/api/event/player/PlayerTickCallback.java
+++ src/main/java/net/fabricmc/fabric/api/event/player/PlayerTickCallback.java
@@ -6,12 +6,7 @@ import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 
 /**
- * Callback for ticking a player's equipment.
- * Takes a player, the inventory to check, the slot ID, and the stack.
- * Can be extended to use any Inventory for use by Baubles-likes. Don't always assume a PlayerInventory.
- *
- * NOTE: The Identifier for vanilla equipment slots is "minecraft:{@link EquipmentSlot#getName()}".
- * Use {@link EquipmentSlot#byName(String)} to get the slot from the ID's path.
+ * Callback for ticking a player. Useful for updating effects given by a player's equipped items.
  */
 public interface PlayerTickCallback {
 	public static final Event<PlayerTickCallback> EVENT = EventFactory.createArrayBacked(PlayerTickCallback.class,
