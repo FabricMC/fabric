b2ce9b036d060046931bf24aa3e94830767e1f97
Yanis48 <doublecraft.official@gmail.com>
2019-04-05 20:47:18 +0200

    /fabric mods command

--- src/main/java/net/fabricmc/fabric/impl/FabricAPIInitializer.java
+++ src/main/java/net/fabricmc/fabric/impl/FabricAPIInitializer.java
@@ -19,7 +19,14 @@ package net.fabricmc.fabric.impl;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.BlockAttackInteractionAware;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.registry.CommandRegistry;
+import net.fabricmc.loader.FabricLoader;
 import net.minecraft.block.BlockState;
+import net.minecraft.server.command.ServerCommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.StringTextComponent;
+import net.minecraft.text.TextFormat;
+import net.minecraft.text.TranslatableTextComponent;
 import net.minecraft.util.ActionResult;
 
 public class FabricAPIInitializer implements ModInitializer {
@@ -39,5 +46,20 @@ public class FabricAPIInitializer implements ModInitializer {
 
 			return ActionResult.PASS;
 		});
+		
+		CommandRegistry.INSTANCE.register(false, serverCommandSourceCommandDispatcher ->
+			serverCommandSourceCommandDispatcher.register(ServerCommandManager
+				.literal("fabric")
+				.requires((serverCommandSource_1) -> { return serverCommandSource_1.hasPermissionLevel(2); })
+				.then(ServerCommandManager.literal("mods")
+				.executes(context -> {
+					ServerCommandSource source = context.getSource();
+					String modList = "";
+					for (int i = 0; i < FabricLoader.INSTANCE.getMods().size(); i++) {
+						modList = modList + "\n  " + TextFormat.GREEN + FabricLoader.INSTANCE.getMods().get(i).getInfo().getName() + TextFormat.YELLOW + " " + FabricLoader.INSTANCE.getMods().get(i).getInfo().getVersionString() + TextFormat.RESET + " " + new TranslatableTextComponent("fabric.command.by").getText() + " " + TextFormat.LIGHT_PURPLE + FabricLoader.INSTANCE.getMods().get(i).getInfo().getAuthors();
+					}
+					source.sendFeedback(new StringTextComponent(new TranslatableTextComponent("fabric.command.mods_loaded").getText() + modList), true);
+					return 1;
+		}))));
 	}
 }
