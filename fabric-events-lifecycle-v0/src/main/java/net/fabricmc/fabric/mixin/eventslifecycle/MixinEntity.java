ac3f910ba4e07f7a830bcb22f46bd2873c49d01e
Meredith Espinosa <goclonefilms@gmail.com>
2019-05-17 23:59:43 -0700

    some more cleanup

--- fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/mixin/eventslifecycle/MixinEntity.java
+++ fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/mixin/eventslifecycle/MixinEntity.java
@@ -3,16 +3,21 @@ package net.fabricmc.fabric.mixin.eventslifecycle;
 
 import net.fabricmc.fabric.api.event.entity.EntityTickCallback;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Entity.class)
-public abstract class MixinEntity<T extends Entity> {
+public abstract class MixinEntity {
+
+	@Shadow public abstract EntityType<?> getType();
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void tickEntity(CallbackInfo ci) {
-		((EntityTickCallback<T>)EntityTickCallback.event(((Entity)(Object)this).getType())).tick((T)(Object)this);
+		Entity self = ((Entity)(Object)this);
+		((EntityTickCallback) EntityTickCallback.event(self.getType())).tick(self);
 	}
 }
