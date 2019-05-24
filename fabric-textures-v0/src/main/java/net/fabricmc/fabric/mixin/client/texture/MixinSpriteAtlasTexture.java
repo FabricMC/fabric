1c0b7d32e8e7488dc3357387f794c3bac82dc5e9
asie <kontakt@asie.pl>
2019-05-24 10:20:34 +0200

    LinkedHashMap -> HashMap

--- fabric-textures-v0/src/main/java/net/fabricmc/fabric/mixin/client/texture/MixinSpriteAtlasTexture.java
+++ fabric-textures-v0/src/main/java/net/fabricmc/fabric/mixin/client/texture/MixinSpriteAtlasTexture.java
@@ -78,7 +78,7 @@ public abstract class MixinSpriteAtlasTexture implements SpriteAtlasTextureHooks
 	// Handles DependentSprite + custom sprite object injections.
 	@ModifyVariable(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/texture/SpriteAtlasTexture;loadSprites(Lnet/minecraft/resource/ResourceManager;Ljava/util/Set;)Ljava/util/Collection;"), method = "stitch")
 	public Set<Identifier> beforeSpriteLoad(Set<Identifier> set) {
-		fabric_injectedSprites = new LinkedHashMap<>();
+		fabric_injectedSprites = new HashMap<>();
 		ClientSpriteRegistryCallback.Registry registry = new ClientSpriteRegistryCallback.Registry(fabric_injectedSprites, set::add);
 		//noinspection ConstantConditions
 		for (Identifier id : fabric_localIds) {
