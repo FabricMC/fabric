d58fb00770c90df5b36a3ece88f90ed77f0740ec
Sollace <sollacea@gmail.com>
2019-06-04 14:49:55 +0200

    Some more fixes

--- fabric-gui-v0/src/main/java/net/fabricmc/fabric/api/event/client/gui/ScreenView.java
+++ fabric-gui-v0/src/main/java/net/fabricmc/fabric/api/event/client/gui/ScreenView.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	 http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,11 +17,6 @@ package net.fabricmc.fabric.api.event.client.gui;
 
 import java.util.List;
 
-import org.spongepowered.asm.mixin.Shadow;
-
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.fabric.api.event.EventFactory;
-import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
 
@@ -46,14 +41,16 @@ public interface ScreenView {
 	 * This is the same as calling `addButton(button)` on the screen
 	 * itself and likewise add the button to the screen's elements list.
 	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	default <T extends AbstractButtonWidget> T addButton(T button, int tabOrdinal) {
 		tabOrdinal = Math.min(Math.max(0, tabOrdinal), getButtons().size());
 
-		getButtons().add(ordinal, button);
-		return getScreen().children().add(ordinal, button);
+		getButtons().add(tabOrdinal, button);
+		((List)getScreen().children()).add(tabOrdinal, button);
+		return button;
 	}
 
-	default void removeButton(T button) {
+	default <T extends AbstractButtonWidget> void removeButton(T button) {
 		getScreen().children().remove(button);
 		getButtons().remove(button);
 	}
--- fabric-gui-v0/src/main/java/net/fabricmc/fabric/mixin/eventsgui/MixinScreen.java
+++ fabric-gui-v0/src/main/java/net/fabricmc/fabric/mixin/eventsgui/MixinScreen.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	 http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,26 +15,20 @@
  */
 package net.fabricmc.fabric.mixin.eventsgui;
 
-import net.fabricmc.fabric.api.event.client.gui.ScreenInitCallback;
-import net.fabricmc.fabric.api.event.client.gui.ScreenView;
-
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.AbstractButtonWidget;
-import net.minecraft.client.render.item.ItemRenderer;
-
 import java.util.List;
 
-import javax.annotation.Nullable;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.google.common.collect.Lists;
+import net.fabricmc.fabric.api.event.client.gui.ScreenInitCallback;
+import net.fabricmc.fabric.api.event.client.gui.ScreenView;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
 
 @Mixin(Screen.class)
 public abstract class MixinScreen implements ScreenView {
