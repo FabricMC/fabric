409f401abb94ef205aa863240829ec59d6454e5c
Sollace <sollacea@gmail.com>
2019-06-04 17:25:58 +0200

    Add a proper builder for key bindings

--- fabric-keybindings-v0/src/main/java/net/fabricmc/fabric/api/client/keybinding/FabricKeyBinding.java
+++ fabric-keybindings-v0/src/main/java/net/fabricmc/fabric/api/client/keybinding/FabricKeyBinding.java
@@ -16,21 +16,53 @@
 
 package net.fabricmc.fabric.api.client.keybinding;
 
+import net.fabricmc.fabric.mixin.client.keybinding.KeyCodeAccessor;
+
+import java.util.Objects;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
-
-import net.fabricmc.fabric.mixin.client.keybinding.KeyCodeAccessor;
 
 /**
  * Expanded version of {@link KeyBinding} for use by Fabric mods.
  *
  * <p>*ALL* instantiated FabricKeyBindings should be registered in
  * {@link KeyBindingRegistry#register(FabricKeyBinding)}!
+ * </p>
+ * <pre><code>
+ * FabricKeyBinding.Builder builder = FabricKeyBinding.builder();
+ * FabricKeyBinding left = builder
+ *			.id(new Identifier("example", "left"))
+ *			.code(Keys.Left)
+ *			.build();
+ * FabricKeyBinding right = builder
+ *			.id(new Identifier("example", "right"))
+ *			.code(Keys.Right)
+ *			.build();
+ * KeyBindingRegistry.register(left);
+ * KeyBindingRegistry.register(right);
+ * </code></pre>
  */
 public class FabricKeyBinding extends KeyBinding {
+
+	private final Identifier id;
+
 	protected FabricKeyBinding(Identifier id, InputUtil.Type type, int code, String category) {
-		super("key." + id.toString().replace(':', '.'), type, code, category);
+		super(formatKeyName(id), type, code, category);
+		this.id = id;
+	}
+
+	public static String formatKeyName(Identifier id) {
+		return String.format("key.%s.%s", id.getNamespace(), id.getPath());
+	}
+
+	/**
+	 * Original identifier used to register this key.
+	 *
+	 * May be different from the {@link getId()}.
+	 */
+	public Identifier getIdentifier() {
+		return id;
 	}
 
 	/**
@@ -41,19 +73,64 @@ public class FabricKeyBinding extends KeyBinding {
 		return ((KeyCodeAccessor) this).getKeyCode();
 	}
 
-	public static class Builder {
-		protected final FabricKeyBinding binding;
+	public static Builder builder() {
+		return new Builder();
+	}
 
-		protected Builder(FabricKeyBinding binding) {
-			this.binding = binding;
+	public static class Builder {
+
+		private InputUtil.Type type = InputUtil.Type.KEYSYM;
+
+		private Identifier id = null;
+
+		private boolean unassigned = false;
+		private int code = InputUtil.UNKNOWN_KEYCODE.getKeyCode();
+
+		private String category = KeyCategories.MISC;
+
+		private Builder() {
+		}
+
+		public Builder id(Identifier keyName) {
+			this.id = keyName;
+			return this;
+		}
+
+		public Builder category(String category) {
+			this.category = category;
+			return this;
+		}
+
+		public Builder code(int keyCode) {
+			this.code = keyCode;
+			return this;
+		}
+
+		public Builder unassigned() {
+			return this;
+		}
+
+		public Builder type(InputUtil.Type type) {
+			this.type = type;
+			return this;
 		}
 
 		public FabricKeyBinding build() {
-			return binding;
+			Objects.requireNonNull(id, "Keybindings should be created with an identifier.");
+
+			if (!unassigned && code == InputUtil.UNKNOWN_KEYCODE.getKeyCode()) {
+				throw new IllegalStateException("Keybingings need a default keycode.");
+			}
+
+			return new FabricKeyBinding(id, type, code, category);
 		}
 
+		/**
+		 * @deprecated Prefer using the builder itself.
+		 */
+		@Deprecated
 		public static Builder create(Identifier id, InputUtil.Type type, int code, String category) {
-			return new Builder(new FabricKeyBinding(id, type, code, category));
+			return builder().id(id).type(type).code(code).category(category);
 		}
 	}
 }
