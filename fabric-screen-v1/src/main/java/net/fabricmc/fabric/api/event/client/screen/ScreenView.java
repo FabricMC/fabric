d7cca208361f0aebfc63904709b662be14ae0afd
Sollace <sollacea@gmail.com>
2019-06-04 15:37:18 +0200

    Fix up javadocs and make an invalid index an error condition

--- fabric-screen-v1/src/main/java/net/fabricmc/fabric/api/event/client/screen/ScreenView.java
+++ fabric-screen-v1/src/main/java/net/fabricmc/fabric/api/event/client/screen/ScreenView.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	 http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,8 +20,23 @@ import java.util.List;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.AbstractButtonWidget;
 
+/**
+ * Abstract view of a screen that allows for adding or removing buttons,
+ * maybe more in the future.
+ */
 public interface ScreenView {
 
+	/**
+	 * Utility method to convert a screen into an abstract view of its contents.
+	 */
+	static ScreenView of(Screen screen) {
+		return (ScreenView)screen;
+	}
+
+	/**
+	 * Gets the screen backed by this view.
+	 * @return
+	 */
 	Screen getScreen();
 
 	/**
@@ -31,6 +46,7 @@ public interface ScreenView {
 
 	/**
 	 * Adds a new button to the screen's own button list.
+	 *
 	 * This is the same as calling `addButton(button)` on the screen
 	 * itself and likewise add the button to the screen's elements list.
 	 */
@@ -38,18 +54,20 @@ public interface ScreenView {
 
 	/**
 	 * Adds a new button to the screen's own button list.
+	 *
 	 * This is the same as calling `addButton(button)` on the screen
 	 * itself and likewise add the button to the screen's elements list.
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	default <T extends AbstractButtonWidget> T addButton(T button, int tabOrdinal) {
-		tabOrdinal = Math.min(Math.max(0, tabOrdinal), getButtons().size());
-
-		getButtons().add(tabOrdinal, button);
-		((List)getScreen().children()).add(tabOrdinal, button);
+	default <T extends AbstractButtonWidget> T addButton(int index, T button) {
+		getButtons().add(index, button);
+		((List)getScreen().children()).add(index, button);
 		return button;
 	}
 
+	/**
+	 * Removes a button from the screen's internal buttons and elements lists.
+	 */
 	default <T extends AbstractButtonWidget> void removeButton(T button) {
 		getScreen().children().remove(button);
 		getButtons().remove(button);
