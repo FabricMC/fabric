41f41326b79a8fe7deadb50bb585396f249a8ef4
Draylar <samuelhimes@gmail.com>
2019-06-07 23:17:14 -0500

    pickup item event

---
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PickupItemCallback.java
@@ -0,0 +1,34 @@
+package net.fabricmc.fabric.api.event.player;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+
+/**
+ * Callback for picking up an item.
+ * Is hooked in before the item is picked up.
+ * <p>
+ * Upon return:
+ * - SUCCESS cancels further processing and picks up the item.
+ * - PASS falls back to further processing.
+ * - FAIL cancels further processing and does not pick up the item.
+ */
+public interface PickupItemCallback {
+	public static final Event<PickupItemCallback> EVENT = EventFactory.createArrayBacked(PickupItemCallback.class,
+		(listeners) -> (player, world, entity, amount) -> {
+			for (PickupItemCallback event : listeners) {
+				ActionResult result = event.interact(player, world, entity, amount);
+				if (result != ActionResult.PASS) {
+					return result;
+				}
+			}
+
+			return ActionResult.PASS;
+		}
+	);
+
+	ActionResult interact(PlayerEntity player, World world, Entity pickupEntity, int amount);
+}
---
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/eventsinteraction/MixinItemEntity.java
@@ -0,0 +1,26 @@
+package net.fabricmc.fabric.mixin.eventsinteraction;
+
+import net.fabricmc.fabric.api.event.player.PickupItemCallback;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemEntity.class)
+public abstract class MixinItemEntity
+{
+	@Shadow public abstract ItemStack getStack();
+
+	@Inject(at = @At("HEAD"), method = "onPlayerCollision", cancellable = true)
+	private void pickupItem(final PlayerEntity playerEntity, final CallbackInfo info) {
+		ActionResult result = PickupItemCallback.EVENT.invoker().interact(playerEntity, playerEntity.getEntityWorld(), (ItemEntity) (Object) this, this.getStack().getAmount());
+		if(result != ActionResult.PASS) {
+			info.cancel();
+		}
+	}
+}
