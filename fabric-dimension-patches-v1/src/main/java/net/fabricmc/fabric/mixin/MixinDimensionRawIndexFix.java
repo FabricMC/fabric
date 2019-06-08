08f4e645c719f36844c5daeff64aff31590fde40
Draylar <samuelhimes@gmail.com>
2019-06-07 23:12:50 -0500

    add Mixin to mixin class name

--- fabric-dimension-patches-v1/src/main/java/net/fabricmc/fabric/mixin/MixinDimensionRawIndexFix.java
+++ fabric-dimension-patches-v1/src/main/java/net/fabricmc/fabric/mixin/MixinDimensionRawIndexFix.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(DimensionType.class)
-public class DimensionRawIndexFix
+public class MixinDimensionRawIndexFix
 {
 	@Inject(at = @At("HEAD"), method = "byRawId", cancellable = true)
 	private static void byRawId(final int id, final CallbackInfoReturnable<DimensionType> info) {
