3be147e118a9a964faa45f7c733b7cbf26409d45
Meredith Espinosa <goclonefilms@gmail.com>
2019-06-27 00:02:40 -0700

    resolve feedback

--- fabric-gamerules-v1/src/main/java/net/fabricmc/fabric/api/gamerule/GameRuleRegistry.java
+++ fabric-gamerules-v1/src/main/java/net/fabricmc/fabric/api/gamerule/GameRuleRegistry.java
@@ -1,7 +1,32 @@
 package net.fabricmc.fabric.api.gamerule;
 
 import net.fabricmc.fabric.impl.gamerule.GameRuleRegistryImpl;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.GameRules;
 
 public interface GameRuleRegistry {
 	final GameRuleRegistry INSTANCE = new GameRuleRegistryImpl();
+
+	/**
+	 * Register a new game rule.
+	 * @param name a namespaced ID for your game rule, to prevent collision.
+	 * @param ruleType The type of rule to register, made with createBooleanRule or createIntRule
+	 * @param <T> Either BooleanRule or IntRule
+	 * @return The key of your rule, to check {@link GameRules#getBoolean(GameRules.RuleKey)} or {@link GameRules#getInt(GameRules.RuleKey)} with.
+	 */
+	<T extends GameRules.Rule<T>> GameRules.RuleKey<T> register(Identifier name, GameRules.RuleType<T> ruleType);
+
+	/**
+	 * Create a new game rule with a boolean value.
+	 * @param defaultValue The default value this rule should start out with.
+	 * @return A RuleType to pass to register.
+	 */
+	GameRules.RuleType<GameRules.BooleanRule> createBooleanRule(boolean defaultValue);
+
+	/**
+	 * Create a new game rule with an int value.
+	 * @param defaultValue The default value this rule should start out with.
+	 * @return A RuleType to pass to register.
+	 */
+	GameRules.RuleType<GameRules.IntRule> createIntRule(int defaultValue);
 }
--- fabric-gamerules-v1/src/main/java/net/fabricmc/fabric/impl/gamerule/GameRuleRegistryImpl.java
+++ fabric-gamerules-v1/src/main/java/net/fabricmc/fabric/impl/gamerule/GameRuleRegistryImpl.java
@@ -9,31 +9,14 @@ import net.minecraft.world.GameRules;
 
 public class GameRuleRegistryImpl implements GameRuleRegistry {
 
-	/**
-	 * Register a new game rule.
-	 * @param name a namespaced ID for your game rule, to prevent collision.
-	 * @param ruleType The type of rule to register, made with createBooleanRule or createIntRule
-	 * @param <T> Either BooleanRule or IntRule
-	 * @return The key of your rule, to check {@link GameRules#getBoolean(GameRules.RuleKey)} or {@link GameRules#getInt(GameRules.RuleKey)} with.
-	 */
 	public <T extends GameRules.Rule<T>> GameRules.RuleKey<T> register(Identifier name, GameRules.RuleType<T> ruleType) {
 		return MixinGameRules.callRegister(name.toString(), ruleType);
 	}
 
-	/**
-	 * Create a new game rule with a boolean value.
-	 * @param defaultValue The default value this rule should start out with.
-	 * @return A RuleType to pass to register.
-	 */
 	public GameRules.RuleType<GameRules.BooleanRule> createBooleanRule(boolean defaultValue) {
 		return MixinBooleanRule.callOf(defaultValue);
 	}
 
-	/**
-	 * Create a new game rule with an int value.
-	 * @param defaultValue The default value this rule should start out with.
-	 * @return A RuleType to pass to register.
-	 */
 	public GameRules.RuleType<GameRules.IntRule> createIntRule(int defaultValue) {
 		return MixinIntRule.callOf(defaultValue);
 	}
