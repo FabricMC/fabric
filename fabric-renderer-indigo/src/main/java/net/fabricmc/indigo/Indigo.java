099c1e8b8c8d4cace0bc64db17405b0ca719ba81
grondag <grondag@gmail.com>
2019-07-07 23:14:07 +0200

    Various Indigo Improvements (#295)
    - Fix #199 Vanilla Bug: Glowstone creates AO shade
    - Fix #289 Rendering on the Cutout and Transparent Layers together interferes with water rendering
    - Fix #290 Smooth lighting breaks on continuous angled surfaces
    - Fix #291 QuadEmitter not cleared on MeshBuilder.build()
    - Fix #292 Render context QuadEmitter crash with flat lighting
    - Fix #293 Relax vertex order requirements for modded quads
    - Fix swapped color components for grass and other blocks
    - Add option to partially support non-standard vertex formats

--- fabric-renderer-indigo/src/main/java/net/fabricmc/indigo/Indigo.java
+++ fabric-renderer-indigo/src/main/java/net/fabricmc/indigo/Indigo.java
@@ -34,13 +34,15 @@ import java.util.Properties;
 
 public class Indigo implements ClientModInitializer {
 	public static final boolean ALWAYS_TESSELATE_INDIGO;
+	public static final boolean ENSURE_VERTEX_FORMAT_COMPATIBILITY;
 	public static final AoConfig AMBIENT_OCCLUSION_MODE;
 	/** Set true in dev env to confirm results match vanilla when they should */
 	public static final boolean DEBUG_COMPARE_LIGHTING;
 	public static final boolean FIX_SMOOTH_LIGHTING_OFFSET;
 	public static final boolean FIX_EXTERIOR_VERTEX_LIGHTING;
-
-	private static final Logger LOGGER = LogManager.getLogger();
+	public static final boolean FIX_LUMINOUS_AO_SHADE;
+	
+	public static final Logger LOGGER = LogManager.getLogger();
 
 	private static boolean asBoolean(String property, boolean defValue) {
 		switch (asTriState(property)) {
@@ -53,7 +55,8 @@ public class Indigo implements ClientModInitializer {
 		}
 	}
 
-	private static <T extends Enum> T asEnum(String property, T defValue) {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+    private static <T extends Enum> T asEnum(String property, T defValue) {
 		if (property == null || property.isEmpty()) {
 			return defValue;
 		} else {
@@ -102,12 +105,16 @@ public class Indigo implements ClientModInitializer {
 			}
 		}
 
-		ALWAYS_TESSELATE_INDIGO = asBoolean((String) properties.computeIfAbsent("always-tesselate-blocks", (a) -> "auto"), true);
+		final boolean forceCompatibility = IndigoMixinConfigPlugin.shouldForceCompatibility();
+		ENSURE_VERTEX_FORMAT_COMPATIBILITY = forceCompatibility;
+		// necessary because OF alters the BakedModel vertex format and will confuse the fallback model consumer
+		ALWAYS_TESSELATE_INDIGO = !forceCompatibility && asBoolean((String) properties.computeIfAbsent("always-tesselate-blocks", (a) -> "auto"), true);
 		AMBIENT_OCCLUSION_MODE = asEnum((String) properties.computeIfAbsent("ambient-occlusion-mode", (a) -> "hybrid"), AoConfig.HYBRID);
 		DEBUG_COMPARE_LIGHTING = asBoolean((String) properties.computeIfAbsent("debug-compare-lighting", (a) -> "auto"), false);
 		FIX_SMOOTH_LIGHTING_OFFSET = asBoolean((String) properties.computeIfAbsent("fix-smooth-lighting-offset", (a) -> "auto"), true);
 		FIX_EXTERIOR_VERTEX_LIGHTING = asBoolean((String) properties.computeIfAbsent("fix-exterior-vertex-lighting", (a) -> "auto"), true);
-
+		FIX_LUMINOUS_AO_SHADE = asBoolean((String) properties.computeIfAbsent("fix-luminous-block-ambient-occlusion", (a) -> "auto"), false);
+		
 		try (FileOutputStream stream = new FileOutputStream(configFile)) {
 			properties.store(stream, "Indigo properties file");
 		} catch (IOException e) {
@@ -119,6 +126,9 @@ public class Indigo implements ClientModInitializer {
     public void onInitializeClient() {
     	if (IndigoMixinConfigPlugin.shouldApplyIndigo()) {
 		    LOGGER.info("[Indigo] Registering Indigo renderer!");
+		    if(IndigoMixinConfigPlugin.shouldForceCompatibility()) {
+		    	LOGGER.info("[Indigo] Compatibility mode enabled.");
+		    }
 		    RendererAccess.INSTANCE.registerRenderer(IndigoRenderer.INSTANCE);
 	    } else {
     		LOGGER.info("[Indigo] Different rendering plugin detected; not applying Indigo.");
