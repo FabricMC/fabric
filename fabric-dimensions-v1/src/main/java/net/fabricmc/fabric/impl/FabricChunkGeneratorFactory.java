98526d8fc138940f5a27dc39513b721339d64294
Draylar <samuelhimes@gmail.com>
2019-07-12 15:32:08 -0500

    documentation & fixed supplier type

--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/impl/FabricChunkGeneratorFactory.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/impl/FabricChunkGeneratorFactory.java
@@ -5,6 +5,13 @@ import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.ChunkGeneratorConfig;
 
+/**
+ * Responsible for creating the FabricChunkGeneratorType's ChunkGenerator instance.
+ * Called when a new instance of a ChunkGenerator is requested in the ChunkGeneratorType.
+ * @param <C> ChunkGenerator config
+ * @param <T> ChunkGenerator
+ */
+@FunctionalInterface
 public interface FabricChunkGeneratorFactory<C extends ChunkGeneratorConfig, T extends ChunkGenerator<C>> {
 	T create(World world, BiomeSource source, C config);
 }
--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/impl/FabricChunkGeneratorType.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/impl/FabricChunkGeneratorType.java
@@ -8,19 +8,36 @@ import net.minecraft.world.gen.chunk.ChunkGeneratorType;
 
 import java.util.function.Supplier;
 
+/**
+ * Fabric version of ChunkGeneratorType which utilizes the FabricChunkGeneratorFactory.
+ * ChunkGeneratorType is a registry wrapper for ChunkGenerator, similar to BlockEntityType or EntityType.
+ * @param <C> ChunkGenerator config
+ * @param <T> ChunkGenerator
+ */
 public class FabricChunkGeneratorType<C extends ChunkGeneratorConfig, T extends ChunkGenerator<C>> extends ChunkGeneratorType<C, T>
 {
 	private FabricChunkGeneratorFactory<C, T> factory;
 
-	public FabricChunkGeneratorType(FabricChunkGeneratorFactory<C, T> factory, boolean buffetScreenOption, Supplier settingsSupplier)
+	/**
+	 * @param factory factory instance to provide a ChunkGenerator
+	 * @param buffetScreenOption whether or not the ChunkGeneratorType should appear in the buffet screen options page
+	 * @param settingsSupplier config supplier
+	 */
+	public FabricChunkGeneratorType(FabricChunkGeneratorFactory<C, T> factory, boolean buffetScreenOption, Supplier<C> settingsSupplier)
 	{
 		super(null, buffetScreenOption, settingsSupplier);
 		this.factory = factory;
 	}
 
+	/**
+	 * Called to get an instance of the ChunkGeneratorType's ChunkGenerator.
+	 * @param world DimensionType's world instance
+	 * @param biomeSource BiomeSource to use while generating the world
+	 * @param config ChunkGenerator config instance
+	 * @return returns an instance of the ChunkGeneratorType's ChunkGenerator.
+	 */
 	@Override
-	public T create(World world, BiomeSource biomeSource, C config)
-	{
+	public T create(World world, BiomeSource biomeSource, C config) {
 		return factory.create(world, biomeSource, config);
 	}
 }
