6086f392eff8f13598978cf9da0d3c7ea3ee81d9
Connor Scialdone <cj@swordglowsblue.com>
2019-08-02 11:04:27 -0600

    Revert "Use wildcards instead of DefaultParticleType"
    This reverts commit 0b72df24f5cb4bd114b947a4be083a23c292b924.

--- fabric-particles-v1/src/main/java/net/fabricmc/fabric/api/particles/ParticleRegistry.java
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/api/particles/ParticleRegistry.java
@@ -37,7 +37,7 @@ public interface ParticleRegistry {
 	}
 
 	/** Create a basic particle type that requires no extra information on spawn. */
-	default ParticleType<?> createSimpleParticleType() {
+	default DefaultParticleType createSimpleParticleType() {
 		return createSimpleParticleType(false);
 	}
 
@@ -46,7 +46,7 @@ public interface ParticleRegistry {
 	 *
 	 * @param shouldAlwaysSpawn Whether this particle should spawn regardless of distance or client settings (a la barrier particles).
 	 */
-	default ParticleType<?> createSimpleParticleType(boolean shouldAlwaysSpawn) {
+	default DefaultParticleType createSimpleParticleType(boolean shouldAlwaysSpawn) {
 		return new DefaultParticleType(shouldAlwaysSpawn) {}; // Anonymous class to bypass protected constructor
 	}
 
