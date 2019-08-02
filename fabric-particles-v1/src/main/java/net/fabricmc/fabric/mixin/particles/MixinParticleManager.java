fd395fc890991f9f6f3c7f42aae0839900654ea8
Connor Scialdone <cj@swordglowsblue.com>
2019-08-02 11:07:00 -0600

    Clean up mixin a bit

--- fabric-particles-v1/src/main/java/net/fabricmc/fabric/mixin/particles/MixinParticleManager.java
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/mixin/particles/MixinParticleManager.java
@@ -26,6 +26,7 @@ import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -37,17 +38,16 @@ import java.util.HashMap;
 /** Various hooks into {@link ParticleManager} for registering particles. */
 @Mixin(ParticleManager.class)
 abstract class MixinParticleManager implements ParticleManagerHooks {
-	@Shadow
+	@Shadow @Final
 	private SpriteAtlasTexture particleAtlasTexture;
 	public SpriteAtlasTexture fabric_getSpriteAtlasTexture() { return particleAtlasTexture; }
 
-	@Shadow
-	@Environment(EnvType.CLIENT)
+	@Shadow	@Environment(EnvType.CLIENT)
 	private <T extends ParticleEffect> void registerFactory(ParticleType<T> pt, ParticleFactory<T> pf) {}
 	@Environment(EnvType.CLIENT)
 	public <T extends ParticleEffect> void fabric_registerCustomFactory(ParticleType<T> pt, ParticleFactory<T> pf) { registerFactory(pt,pf); }
 
-	@Inject(method = "net/minecraft/client/particle/ParticleManager.registerDefaultFactories()V", at = @At("RETURN"))
+	@Inject(method = "registerDefaultFactories()V", at = @At("RETURN"))
 	private void registerCustomFactories(CallbackInfo cbi) {
 		HashMap<ParticleType<?>, ParticleFactory<?>> factories = ((ParticleRegistryImpl)ParticleRegistry.INSTANCE).factoriesAwaitingRegistry;
 		if(!factories.isEmpty()) {
