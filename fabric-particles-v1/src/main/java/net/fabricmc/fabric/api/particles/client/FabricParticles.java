1bc83190aa869b4e1ea1cd831a97b68904802059
Connor Scialdone <cj@swordglowsblue.com>
2019-08-14 20:32:58 -0600

    Add example code to FabricParticles javadoc

--- fabric-particles-v1/src/main/java/net/fabricmc/fabric/api/particles/client/FabricParticles.java
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/api/particles/client/FabricParticles.java
@@ -29,7 +29,44 @@ import net.minecraft.particle.ParticleType;
 import net.minecraft.util.Identifier;
 
 /**
- * Core methods for registering and displaying particles with the Fabric API.
+ * Core methods for registering and displaying particles with the Fabric API. Example:
+ * <br><br>
+ * <pre><code>
+ * public class ParticleExample implements ModInitializer, ClientModInitializer {
+ *   // Create a ParticleType for our custom particle
+ *   private static final DefaultParticleType exampleParticleType = FabricParticleTypes.createSimpleParticleType();
+ *   // The ID of our particle, for repeated use in registry
+ *   private static final Identifier exampleParticleID = new Identifier("particleexample:example_particle");
+ *
+ *   public void onInitialize() {
+ *     // Register our particle type
+ *     Registry.register(Registry.PARTICLE_TYPE, exampleParticleID, exampleParticleType);
+ *   }
+ *
+ *  {@literal @}Environment(EnvType.CLIENT)
+ *   public void onInitializeClient() {
+ *     // Register the sprite for our particle
+ *     ClientSpriteRegistryCallback.event(SpriteAtlasTexture.PARTICLE_ATLAS_TEX).register((atlasTexture, registry) -> {
+ *       registry.register(exampleParticleID);
+ *     });
+ *
+ *     // Register a ParticleFactory for our custom particle
+ *     FabricParticles.INSTANCE.registerParticleFactory(exampleParticleType, (type, world, x, y, z, vx, vy, vz) ->
+ *       new ExampleParticle(world, x, y, z, vx, vy, vz));
+ *   }
+ *
+ *   // Implement our custom particle
+ *  {@literal @}Environment(EnvType.CLIENT)
+ *   static class ExampleParticle extends SpriteBillboardParticle {
+ *     ExampleParticle(World w, double x, double y, double z, double vx, double vy, double vz) {
+ *       super(w, x, y, z, vx, vy, vz);
+ *       this.setSprite(FabricParticles.getParticleSpriteAtlas().getSprite(exampleParticleID));
+ *     }
+ *
+ *     public ParticleTextureSheet getType() { return ParticleTextureSheet.PARTICLE_SHEET_OPAQUE; }
+ *   }
+ * }
+ * </code></pre>
  */
 public interface FabricParticles {
 	FabricParticles INSTANCE = new FabricParticlesImpl();
