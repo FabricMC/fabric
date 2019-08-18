7314ce5214a441529f8af53001a170046a7c580f
Sollace <sollacea@gmail.com>
2019-08-18 20:34:09 +0200

    Removed the need for reflection, and added a way to access the sprite sheet directly

--- fabric-particles-v1/src/main/java/net/fabricmc/fabric/impl/particles/ParticleFactoryRegistryImpl.java
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/impl/particles/ParticleFactoryRegistryImpl.java
@@ -16,17 +16,13 @@
 
 package net.fabricmc.fabric.impl.particles;
 
-import java.lang.reflect.Constructor;
+import java.util.HashMap;
 import java.util.Map;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.particles.ParticleFactoryRegistry;
-import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.particle.ParticleFactory;
-import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.particle.SpriteProvider;
 import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleType;
 import net.minecraft.util.Identifier;
@@ -36,8 +32,9 @@ public class ParticleFactoryRegistryImpl implements ParticleFactoryRegistry {
 
     public static final ParticleFactoryRegistryImpl INSTANCE = new ParticleFactoryRegistryImpl();
 
-    private final Int2ObjectMap<ParticleFactory<?>> factories = new Int2ObjectOpenHashMap<>();
-    private final Int2ObjectMap<PendingParticleFactory<?>> constructors = new Int2ObjectOpenHashMap<>();
+    final Int2ObjectMap<ParticleFactory<?>> factories = new Int2ObjectOpenHashMap<>();
+    final Int2ObjectMap<PendingParticleFactory<?>> constructors = new Int2ObjectOpenHashMap<>();
+    final Map<Identifier, Integer> constructorsIdsMap = new HashMap<>();
 
     private ParticleFactoryRegistryImpl() { }
 
@@ -49,46 +46,6 @@ public class ParticleFactoryRegistryImpl implements ParticleFactoryRegistry {
     @Override
     public <T extends ParticleEffect> void register(ParticleType<T> type, PendingParticleFactory<T> factory) {
         constructors.put(Registry.PARTICLE_TYPE.getRawId(type), factory);
-    }
-
-    public void injectValues(Int2ObjectMap<ParticleFactory<?>> factories, Map<Identifier, SpriteProvider> spriteProviders) {
-        factories.putAll(this.factories);
-
-        constructors.forEach((id, factory) -> {
-            SpriteProvider spriteProvider = AccessHack.createSimpleSpriteProvider();
-
-            spriteProviders.put(Registry.PARTICLE_TYPE.getId(Registry.PARTICLE_TYPE.get(id)), spriteProvider);
-            factories.put((int)id, factory.create(spriteProvider));
-        });
-    }
-
-    /**
-     * Uses reflection to obtain new instances of the private inner class `ParticleManager.SimpleSpriteProvider`.
-     */
-    static final class AccessHack {
-        private static Constructor<?> constr;
-
-        private static Constructor<?> getConstructor() throws ReflectiveOperationException {
-            if (constr != null) {
-                return constr;
-            }
-
-            String className = FabricLoader.getInstance().getMappingResolver().mapClassName("intermediary", "net.minecraft.class_702$class_4090");
-
-            Class<?> cls = Class.forName(className);
-
-            constr = cls.getDeclaredConstructor(ParticleManager.class);
-            constr.setAccessible(true);
-
-            return constr;
-        }
-
-        static SpriteProvider createSimpleSpriteProvider() {
-            try {
-                return (SpriteProvider)getConstructor().newInstance(MinecraftClient.getInstance().particleManager);
-            } catch (ReflectiveOperationException e) {
-                throw new IllegalStateException("net.minecraft.client.particle.ParticleManager.SimpleSpriteProvider.<init>() is gone!", e);
-            }
-        }
+        constructorsIdsMap.put(Registry.PARTICLE_TYPE.getId(type), Registry.PARTICLE_TYPE.getRawId(type));
     }
 }
