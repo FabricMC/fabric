5d1dde4ff2b7a852e80f93597dbd788b7311884b
grondag <grondag@gmail.com>
2019-09-14 18:13:29 -0700

    Deprecate all of rendering-data-attachment

--- fabric-rendering-data-attachment-v1/src/main/java/net/fabricmc/fabric/api/rendering/data/v1/RenderAttachedBlockView.java
+++ fabric-rendering-data-attachment-v1/src/main/java/net/fabricmc/fabric/api/rendering/data/v1/RenderAttachedBlockView.java
@@ -21,43 +21,65 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ExtendedBlockView;
 
 /**
- * BlockView-extending interface to be used by {@link net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel} for dynamic model
- * customization. It ensures thread safety and exploits data cached in render 
- * chunks for performance and data consistency.<p>
+ * Do not use.  Read on for explanation and alternative.<p>
  * 
- * There are differences from BlockView consumers must understand:<p>
+ * This module was meant to ensure thread-safe access to BlockEntity state
+ * needed by chunk rebuilds that may run off the main client thread.<p>
  * 
- * BlockEntity implementations that provide data for model customization should implement 
- * {@link RenderAttachmentBlockEntity} which will be queried on the main thread when a render
- * chunk is enqueued for rebuild. The model should retrieve the results via {@link #getBlockEntityRenderAttachment(BlockPos)}.
- * While {@link #getBlockEntity(net.minecraft.util.math.BlockPos)} is not disabled, it
- * is not thread-safe for use on render threads.  Models that violate this guidance are
- * responsible for any necessary synchronization or collision detection.<p>
+ * Read access to block entities in render world views is already thread-safe,
+ * or at least safe enough - a {@code HashMap.get()} call will, at worst, give 
+ * a result that is inconsistent with the block state being rendered.<p>
  * 
- * {@link #getBlockState(net.minecraft.util.math.BlockPos)} and {@link #getFluidState(net.minecraft.util.math.BlockPos)}
- * will always reflect the state cached with the render chunk.  Block and fluid states
- * can thus be different from main-thread world state due to lag between block update
- * application from network packets and render chunk rebuilds. Use of {link #getCachedRenderData()}
- * will ensure consistency of model state with the rest of the chunk being rendered.<p>
+ * Earlier implementations tried to avoid that problem by capturing the block entity 
+ * render state on the client thread while each chunk render cache was built
+ * and preserving that state for later use.<p>
+ * 
+ * Unfortunately, there is nothing that prevents the per-chunk block entity map
+ * from being modified <em>while</em> the cache is being built. This caused
+ * intermittent {@code ConcurrentModificationException} crashes. The most common
+ * cause for this is a common rendering optimization mod that moves chunk rebuilds
+ * off the main client thread.  This works in vanilla without synchronization because
+ * all {@code BlockEntity} map <em>updates</em> are still happening on the main thread
+ * and the vanilla chunk cache creation code does not iterate the {@code BlockEntity} map.<p>
+ * 
+ * It would be possible to work around this problem by building our state cache using 
+ * per-position queries instead of iterating the map, but this introduces an additional
+ * performance burden and, more importantly, does not solve the original problem: the resulting
+ * render state derived from block entities may be inconsistent with the block states in the 
+ * render cache!  In other words, it defeats the primary purpose of this module.<p>
+ * 
+ * The same would be true for an approach that alters the vanilla data structures to use
+ * {@code ConcurrentHashMap} because that a concurrent map (by design) does not guarantee 
+ * a consistent view during iteration. <p>
+ * 
+ * Fully synchronizing all map access would work, but doing so would be highly invasive
+ * and likely brittle and incompatible with mods that rely on the current vanilla structures.<p>
  *
- * Models should avoid using {@link ExtendedBlockView#getBlockEntity(BlockPos)}
- * to ensure thread safety because this view may be accessed outside the main client thread.
- * Models that require Block Entity data should implement {@link RenderAttachmentBlockEntity}
- * and then use {@link #getBlockEntityRenderAttachment(BlockPos)} to retrieve it.  When called from the
- * main thread, that method will simply retrieve the data directly.<p>
+ * This mean model implementations that rely on {@code BlockEntity} state must check for nulls, 
+ * check for inconsistency with the block state in the world view and be well-guarded generally.<p>
  * 
- * This interface is only guaranteed to be present in the client environment.
+ * Given that these precautions must be taken in any case, this module adds needless complexity.<p>
+ * 
+ * Mods that previously called:
+ * 
+ * <pre>
+ *     ((RenderAttachedBlockView)blockView).getBlockEntityRenderAttachment(pos)</pre>
+ * 
+ * should now instead simply use:
+ * 
+ * <pre>
+ *     blockView.getBlockEntity(pos)</pre>
+ * 
+ * ...and take the aformentioned precautions in designing and retrieving state to be used during rendering.
  */
-// XXX can not link net.fabricmc.fabric.api.renderer.v1.model.FabricBakedModel
+@Deprecated
 public interface RenderAttachedBlockView extends ExtendedBlockView {
-    /**
-     * For models associated with Block Entities that implement {@link RenderAttachmentBlockEntity}
-     * this will be the most recent value provided by that implementation for the given block position.<p>
-     *
-     * Null in all other cases, or if the result from the implementation was null.<p>
-     *
-     * @param pos Position of the block for the block model.
-     */
+	/**
+	 * Do not use.<p>
+	 * 
+	 * See header for explanation and recommended alternative.<p>
+	 */
+	@Deprecated
     default Object getBlockEntityRenderAttachment(BlockPos pos) {
         BlockEntity be = this.getBlockEntity(pos);
         return be == null ? null : ((RenderAttachmentBlockEntity) be).getRenderAttachmentData();
