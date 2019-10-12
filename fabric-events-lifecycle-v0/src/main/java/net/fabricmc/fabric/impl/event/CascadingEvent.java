08857583023edc0e923d66f2d5121c85c2b2b09b
Pyrofab <redstoneinfire@gmail.com>
2019-10-12 13:22:30 +0200

    Document entity ticking event and internals

--- fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/impl/event/CascadingEvent.java
+++ fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/impl/event/CascadingEvent.java
@@ -12,28 +12,51 @@ import java.util.List;
  */
 public final class CascadingEvent<E> extends Event<E> {
 	private final Event<E> delegate;
-	private final List<Event<E>> cascades = new ArrayList<>();
+	private final List<Event<E>> descendants = new ArrayList<>();
 	private final List<E> callbacks = new ArrayList<>();
 
+	/**
+	 * Creates a {@code CascadingEvent} that uses another event's {@link Event#invoker invoker}.
+	 *
+	 * @param delegate the event to decorate
+	 */
 	public CascadingEvent(Event<E> delegate) {
 		this.delegate = delegate;
 		this.invoker = this.delegate.invoker();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p> The listener will also be registered to this event's delegate as well as any cascading
+	 * event registered at a past or future time.
+	 *
+	 * @see #registerDescendant(Event)
+	 */
 	@Override
 	public void register(E listener) {
 		this.delegate.register(listener);
 		this.invoker = this.delegate.invoker();
 		this.callbacks.add(listener);
-		for (Event<E> cascade : this.cascades) {
+		for (Event<E> cascade : this.descendants) {
 			cascade.register(listener);
 		}
 	}
 
-	public void registerCascading(Event<E> cascading) {
-		this.cascades.add(cascading);
+	/**
+	 * Registers an event as descendant of this {@code CascadingEvent}.
+	 *
+	 * <p> The {@code descendant} is added to this event's inheritance tree with this event as the direct ancestor.
+	 * It will then receive any {@link #register(Object) callback registration} made to its ancestors.
+	 * This method also immediately registers any callback that has been previously registered to this event to
+	 * the {@code descendant}, allowing descendants to be registered after callback registrations have occurred.
+	 *
+	 * @param descendant an event to register as descendant of this event
+	 */
+	public void registerDescendant(Event<E> descendant) {
+		this.descendants.add(descendant);
 		for (E callback : this.callbacks) {
-			cascading.register(callback);
+			descendant.register(callback);
 		}
 	}
 }
--- fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/impl/event/EntityEventInternals.java
+++ fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/impl/event/EntityEventInternals.java
@@ -12,21 +12,27 @@ import java.util.Map;
 
 public class EntityEventInternals {
 
-	/**Cache of entity class to {@code Event}'*/
+	/** Cache of entity class to {@code Event} */
 	private static final Map<Class<?>, CascadingEvent> TICK_EVENTS = new HashMap<>();
 
 	/**
-	 * Retrieve an entity tick event for a given class.
+	 * Retrieves an entity tick event for a given class. If none exists, this method creates one and registers it
+	 * as a cascading child of the superclass' event.
+	 *
+	 * <p> The returned {@code Event} will have new callbacks registered automatically whenever one is registered
+	 * to a parent event.
+	 *
+	 * @return a tick {@code Event} for entities of the given class
 	 */
 	@SuppressWarnings("unchecked")
 	public static <E extends Entity> CascadingEvent<EntityTickCallback<E>> getOrCreateEntityEvent(Class<?> entityClass) {
 		Preconditions.checkArgument(Entity.class.isAssignableFrom(entityClass));
 		CascadingEvent<EntityTickCallback<E>> event = TICK_EVENTS.get(entityClass);
 		if (event == null) {
-			event = new CascadingEvent<>(createEntityEvent());
-			if (entityClass != Entity.class) {	// entities cannot inherit callbacks from anything
+			event = new CascadingEvent<>(createEntityEvent());    // decorator around the base Event
+			if (entityClass != Entity.class) {    // entities cannot inherit callbacks from anything
 				Class<?> superclass = entityClass.getSuperclass();
-				getOrCreateEntityEvent(superclass).registerCascading((Event) event);	// recursive call to init every parent
+				getOrCreateEntityEvent(superclass).registerDescendant((Event) event);    // recursive call to init every parent
 			}
 			TICK_EVENTS.put(entityClass, event);
 		}
@@ -34,9 +40,10 @@ public class EntityEventInternals {
 	}
 
 	/**
-	 * Create an {@code Event} for entity tick callbacks.
+	 * Creates an {@code Event} for entity tick callbacks.
 	 *
-	 * This method has no side effects.
+	 * <p> This method has no side effects.
+	 *
 	 * @return a new Event to which {@code EntityTickCallback} can be registered
 	 */
 	@SuppressWarnings("unchecked")
