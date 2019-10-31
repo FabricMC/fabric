3af5e0e6e1ad410d7b943a55f38ec2a96e09df5b
Meredith Espinosa <goclonefilms@gmail.com>
2019-10-30 21:04:27 -0700

    fix some checkstyle violations

--- fabric-mining-levels-v0/src/main/java/net/fabricmc/fabric/api/tag/FabricItemTags.java
+++ fabric-mining-levels-v0/src/main/java/net/fabricmc/fabric/api/tag/FabricItemTags.java
@@ -19,7 +19,6 @@ package net.fabricmc.fabric.api.tag;
 import net.fabricmc.fabric.api.tools.v1.FabricToolTags;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
-import net.minecraft.util.Identifier;
 
 /**
  * Item tags provided by Fabric.
@@ -34,7 +33,5 @@ public class FabricItemTags {
 	public static final Tag<Item> SHOVELS = FabricToolTags.SHOVELS;
 	public static final Tag<Item> SWORDS = FabricToolTags.SWORDS;
 
-	private FabricItemTags() {
-
-	}
+	private FabricItemTags() { }
 }
--- fabric-mining-levels-v0/src/main/java/net/fabricmc/fabric/api/tools/FabricToolTags.java
+++ fabric-mining-levels-v0/src/main/java/net/fabricmc/fabric/api/tools/FabricToolTags.java
@@ -19,18 +19,14 @@ package net.fabricmc.fabric.api.tools;
 import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 
-import net.fabricmc.fabric.api.tag.TagRegistry;
-
 /**
  * @deprecated Use the v1 class, at {@link net.fabricmc.fabric.api.tools.v1.FabricToolTags}
  */
 @Deprecated
 public class FabricToolTags {
-
 	public static final Tag<Item> AXES = net.fabricmc.fabric.api.tools.v1.FabricToolTags.AXES;
 	public static final Tag<Item> HOES = net.fabricmc.fabric.api.tools.v1.FabricToolTags.HOES;
 	public static final Tag<Item> PICKAXES = net.fabricmc.fabric.api.tools.v1.FabricToolTags.PICKAXES;
 	public static final Tag<Item> SHOVELS = net.fabricmc.fabric.api.tools.v1.FabricToolTags.SHOVELS;
 	public static final Tag<Item> SWORDS = net.fabricmc.fabric.api.tools.v1.FabricToolTags.SWORDS;
-
 }
