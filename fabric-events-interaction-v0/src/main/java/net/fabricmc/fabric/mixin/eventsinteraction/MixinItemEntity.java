8e1ca31c6610bbef0b93477a1882e092aa2696e0
Draylar <samuelhimes@gmail.com>
2019-11-09 10:15:54 -0600

    fix unused mixin shadowed method & fix formatting

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/eventsinteraction/MixinItemEntity.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/eventsinteraction/MixinItemEntity.java
@@ -3,23 +3,18 @@ package net.fabricmc.fabric.mixin.eventsinteraction;
 import net.fabricmc.fabric.api.event.player.PlayerPickupItemCallback;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public abstract class MixinItemEntity
-{
-	@Shadow public abstract ItemStack getStack();
-
+public abstract class MixinItemEntity {
 	@Inject(at = @At("HEAD"), method = "onPlayerCollision", cancellable = true)
 	private void pickupItem(final PlayerEntity playerEntity, final CallbackInfo info) {
 		ActionResult result = PlayerPickupItemCallback.EVENT.invoker().interact(playerEntity, (ItemEntity) (Object) this);
-		if(result == ActionResult.FAIL) {
+		if (result == ActionResult.FAIL) {
 			info.cancel();
 		}
 	}
