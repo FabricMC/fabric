414cd17c4376ea7c4d9928284ba0df936c0449a5
Draylar <samuelhimes@gmail.com>
2019-11-10 10:57:33 -0600

    add license headers & make a few checkstyle fixes

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlayerPickupItemCallback.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlayerPickupItemCallback.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.api.event.player;
 
 import net.fabricmc.fabric.api.event.Event;
@@ -9,24 +25,25 @@ import net.minecraft.util.ActionResult;
 /**
  * Callback for the player picking up an item on the ground.
  * Is hooked in before the item is picked up.
- * <p>
- * Upon return:
- * - SUCCESS cancels further processing and picks up the item.
- * - PASS falls back to further processing. If all listeners return PASS, the item is picked up.
- * - FAIL cancels further processing and does not pick up the item.
+ *
+ * <p>Upon return:
+ * <ul><li>SUCCESS cancels further processing and picks up the item.
+ * <li>PASS falls back to further processing. If all listeners return PASS, the item is picked up.
+ * <li>FAIL cancels further processing and does not pick up the item.</ul>
  */
 public interface PlayerPickupItemCallback {
-	public static final Event<PlayerPickupItemCallback> EVENT = EventFactory.createArrayBacked(PlayerPickupItemCallback.class,
-		(listeners) -> (player, entity) -> {
-			for (PlayerPickupItemCallback event : listeners) {
-				ActionResult result = event.interact(player, entity);
-				if (result != ActionResult.PASS) {
-					return result;
-				}
-			}
+	Event<PlayerPickupItemCallback> EVENT = EventFactory.createArrayBacked(PlayerPickupItemCallback.class,
+			(listeners) -> (player, entity) -> {
+				for (PlayerPickupItemCallback event : listeners) {
+					ActionResult result = event.interact(player, entity);
 
-			return ActionResult.PASS;
-		}
+					if (result != ActionResult.PASS) {
+						return result;
+					}
+				}
+
+				return ActionResult.PASS;
+			}
 	);
 
 	ActionResult interact(PlayerEntity player, ItemEntity pickupEntity);
