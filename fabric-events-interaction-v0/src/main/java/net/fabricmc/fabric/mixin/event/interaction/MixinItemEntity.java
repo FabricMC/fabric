c13d3e2f2c436fcd28ab85059b99ab9d08739160
Draylar <samuelhimes@gmail.com>
2019-11-10 10:43:10 -0600

    move mixin to new events interaction folder

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemEntity.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinItemEntity.java
@@ -1,38 +1,19 @@
-/*
- * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.fabricmc.fabric.mixin.event.interaction;
 
+import net.fabricmc.fabric.api.event.player.PlayerPickupItemCallback;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResult;
-
-import net.fabricmc.fabric.api.event.player.PlayerPickupItemCallback;
 
 @Mixin(ItemEntity.class)
 public abstract class MixinItemEntity {
 	@Inject(at = @At("HEAD"), method = "onPlayerCollision", cancellable = true)
 	private void pickupItem(final PlayerEntity playerEntity, final CallbackInfo info) {
 		ActionResult result = PlayerPickupItemCallback.EVENT.invoker().interact(playerEntity, (ItemEntity) (Object) this);
-
 		if (result == ActionResult.FAIL) {
 			info.cancel();
 		}
