1d14e4df5496dd38bd00eb91683bfe3b2e9429eb
i509vcb <git@i509.me>
2020-02-25 17:35:55 -0600

    Now the mod actually loads and more javadoc

--- fabric-events-player-v1/src/main/java/net/fabricmc/fabric/api/event/player/v1/PlayerRespawnCallback.java
+++ fabric-events-player-v1/src/main/java/net/fabricmc/fabric/api/event/player/v1/PlayerRespawnCallback.java
@@ -1,6 +1,8 @@
-package net.fabricmc.api.event.player.v1;
+package net.fabricmc.fabric.api.event.player.v1;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.dimension.DimensionType;
 
 import net.fabricmc.fabric.api.event.Event;
@@ -11,7 +13,7 @@ import net.fabricmc.fabric.api.event.EventFactory;
  *
  * <p>This occurs when a player dies, or changes dimensions, such as returning to the overworld from the end.
  *
- * <p>Note the player can be repositioned.
+ * @apiNote This callback may be used to reposition the respawning player, or copy modded data between the old and the new.
  */
 public interface PlayerRespawnCallback {
 	Event<PlayerRespawnCallback> EVENT = EventFactory.createArrayBacked(PlayerRespawnCallback.class, (callbacks) -> (newPlayer, oldPlayer, newDimension, alive) -> {
@@ -25,10 +27,19 @@ public interface PlayerRespawnCallback {
 	 *
 	 * <p>Note the player can be repositioned.
 	 *
+	 * <p>When this method is called, {@code newPlayer} will be at its spawn position.
+	 *
+	 * <p>The player may be repositioned by callbacks to change where it respawns.
+	 * The {@code oldPlayer}'s coordinates and world will be the same as before the respawn,
+	 * By default player's {@linkplain Entity#dimension dimension} will have been set to {@link DimensionType#OVERWORLD}.
+	 *
+	 * <p>Player's data has been copied from the {@code oldPlayer} to {@code newPlayer}.
+	 * The actual copied data depends on various factors such as the value of {@code isAlive} or of the {@link GameRules#KEEP_INVENTORY keepInventory} GameRule.
+	 *
 	 * @param newPlayer The new {@link ServerPlayerEntity} that will be spawned.
 	 * @param oldPlayer The old {@link ServerPlayerEntity} that is being removed.
 	 * @param dimension The dimension this player is being respawned in.
-	 * @param oldPlayerIsAlive Whether the old player is still alive.
+	 * @param isAlive Whether the old player is still alive.
 	 */
-	void onRespawn(ServerPlayerEntity newPlayer, ServerPlayerEntity oldPlayer, DimensionType dimension, boolean oldPlayerIsAlive);
+	void onRespawn(ServerPlayerEntity newPlayer, ServerPlayerEntity oldPlayer, DimensionType dimension, boolean isAlive);
 }
---
+++ fabric-events-player-v1/src/main/java/net/fabricmc/fabric/mixin/event/player/MixinPlayerManager.java
@@ -0,0 +1,37 @@
+package net.fabricmc.fabric.mixin.event.player;
+
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.dimension.DimensionType;
+
+import net.fabricmc.fabric.api.event.player.v1.PlayerRespawnCallback;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(PlayerManager.class)
+public abstract class MixinPlayerManager {
+	/**
+	 * The this injection has been chosen to allow repositioning of the player by dimension and location before respawn packets are sent to the client.
+	 */
+	@Inject(method = "respawnPlayer(Lnet/minecraft/server/network/ServerPlayerEntity;Lnet/minecraft/world/dimension/DimensionType;Z)Lnet/minecraft/server/network/ServerPlayerEntity;",
+		slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;doesNotCollide(Lnet/minecraft/entity/Entity;)Z")),
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/World;getLevelProperties()Lnet/minecraft/world/level/LevelProperties;"
+		),
+		locals = LocalCapture.CAPTURE_FAILHARD
+	)
+	private void fabric_onPlayerRespawnFireEvent(ServerPlayerEntity oldPlayer, DimensionType newDimension, boolean oldPlayerIsAlive, CallbackInfoReturnable<ServerPlayerEntity> cir, BlockPos spawnPos, boolean forcedSpawn,
+		ServerPlayerInteractionManager manager, ServerPlayerEntity clone) {
+		PlayerRespawnCallback.EVENT.invoker().onRespawn(clone, oldPlayer, newDimension, oldPlayerIsAlive);
+	}
+}
