264ce5dd3a511f8bc0d40ac1e4794b7e31b49ac6
Juuxel <kasperi.kauppi@gmail.com>
2020-02-25 10:41:00 +0200

    Improve comment

--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/MixinWorld.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/MixinWorld.java
@@ -10,11 +10,16 @@ import net.minecraft.world.dimension.DimensionType;
 
 @Mixin(World.class)
 public abstract class MixinWorld {
-	// This redirector un-hardcodes the overworld checks in World.isDay() and World.isNight()
+	/* World.isDay() and World.isNight() enable the day-night cycle as well as some entity behavior
+	   (such as bees). In vanilla, these methods are hardcoded to only work in the overworld. This
+	   redirector pretends that all dimensions with a visible sky are DimensionType.OVERWORLD, which
+	   makes the time checks for modded dimensions work.
+
+	   Dimension.hasVisibleSky() is true for the overworld, false for the nether and the end, and
+	   customizable for modded dimensions. It is already used for time checking in other places
+	   such as clocks. */
 	@Redirect(method = {"isDay", "isNight"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/dimension/Dimension;getType()Lnet/minecraft/world/dimension/DimensionType;"))
 	private DimensionType replaceCustomDimensionsWithOverworld(Dimension dimension) {
-		// Replaces the dimension type with overworld if the dimension has a visible sky.
-		// hasVisibleSky() is also used for time checking in other places like clocks, for example.
 		return dimension.hasVisibleSky() ? DimensionType.OVERWORLD : dimension.getType();
 	}
 }
