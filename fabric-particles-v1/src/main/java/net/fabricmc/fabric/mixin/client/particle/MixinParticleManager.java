e98b24339fecabca2c13a4c05e9a47f6be409b94
Erlend Åmdal <erlend@aamdal.com>
2020-03-18 18:12:13 +0100

    Simplify fabric-particles-v1 and resolve sprites not being added to the particle sprite atlas

--- fabric-particles-v1/src/main/java/net/fabricmc/fabric/mixin/client/particle/MixinParticleManager.java
+++ fabric-particles-v1/src/main/java/net/fabricmc/fabric/mixin/client/particle/MixinParticleManager.java
@@ -16,48 +16,19 @@
 
 package net.fabricmc.fabric.mixin.client.particle;
 
-import java.util.List;
-import java.util.Map;
-
-import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.client.particle.ParticleFactory;
 import net.minecraft.client.particle.ParticleManager;
-import net.minecraft.client.texture.SpriteAtlasTexture;
-import net.minecraft.resource.ResourceManager;
-import net.minecraft.util.Identifier;
 
-import net.fabricmc.fabric.impl.client.particle.FabricParticleManager;
-import net.fabricmc.fabric.impl.client.particle.VanillaParticleManager;
+import net.fabricmc.fabric.impl.client.particle.ParticleFactoryRegistryImpl;
 
 @Mixin(ParticleManager.class)
-public abstract class MixinParticleManager implements VanillaParticleManager {
-	private final FabricParticleManager fabricParticleManager = new FabricParticleManager(this);
-
-	@Override
-	@Accessor("particleAtlasTexture")
-	public abstract SpriteAtlasTexture getAtlas();
-
-	@Override
-	@Accessor("factories")
-	public abstract Int2ObjectMap<ParticleFactory<?>> getFactories();
-
+public abstract class MixinParticleManager {
 	@Inject(method = "registerDefaultFactories()V", at = @At("RETURN"))
 	private void onRegisterDefaultFactories(CallbackInfo info) {
-		fabricParticleManager.injectValues();
-	}
-
-	@Inject(method = "loadTextureList(Lnet/minecraft/resource/ResourceManager;Lnet/minecraft/util/Identifier;Ljava/util/Map;)V",
-			at = @At("HEAD"),
-			cancellable = true)
-	private void onLoadTextureList(ResourceManager manager, Identifier id, Map<Identifier, List<Identifier>> output, CallbackInfo info) {
-		if (fabricParticleManager.loadParticle(manager, id)) {
-			info.cancel();
-		}
+		ParticleFactoryRegistryImpl.INSTANCE.initialize((ParticleManager) (Object) this);
 	}
 }
