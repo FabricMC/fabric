c5d183a1b79e352852e7f83201e3538a403d3ec7
shedaniel <daniel@shedaniel.me>
2020-04-22 23:13:47 +0800

    choose an item that is the closest

--- fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/ToolHandlers.java
+++ fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/ToolHandlers.java
@@ -1,19 +1,27 @@
 package net.fabricmc.fabric.impl.tool.attribute;
 
+import com.google.common.collect.ImmutableList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ShearsItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Lazy;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.tool.attribute.v1.DynamicAttributeTool;
@@ -30,12 +38,79 @@ public class ToolHandlers implements ModInitializer {
 		ToolManager.general().register(new ModdedToolsModdedBlocksToolHandler());
 		ToolManager.general().register(new VanillaToolsModdedBlocksToolHandler());
 		ToolManager.general().register(new VanillaToolsVanillaBlocksToolHandler());
-		ToolManager.tag(FabricToolTags.PICKAXES).register(new ModdedMiningToolsVanillaBlocksToolHandler(Items.WOODEN_PICKAXE));
-		ToolManager.tag(FabricToolTags.AXES).register(new ModdedMiningToolsVanillaBlocksToolHandler(Items.WOODEN_AXE));
-		ToolManager.tag(FabricToolTags.SHOVELS).register(new ModdedMiningToolsVanillaBlocksToolHandler(Items.WOODEN_SHOVEL));
-		ToolManager.tag(FabricToolTags.HOES).register(new ModdedMiningToolsVanillaBlocksToolHandler(Items.WOODEN_HOE));
-		ToolManager.tag(FabricToolTags.SWORDS).register(new ModdedMiningToolsVanillaBlocksToolHandler(Items.WOODEN_SWORD));
+		ToolManager.tag(FabricToolTags.PICKAXES).register(new ModdedMiningToolsVanillaBlocksToolHandler(
+				ImmutableList.of(
+						new Lazy<>(() -> createPickaxe(ToolMaterials.WOOD)),
+						new Lazy<>(() -> createPickaxe(ToolMaterials.STONE)),
+						new Lazy<>(() -> createPickaxe(ToolMaterials.IRON)),
+						new Lazy<>(() -> createPickaxe(ToolMaterials.DIAMOND))
+				)
+		));
+		ToolManager.tag(FabricToolTags.AXES).register(new ModdedMiningToolsVanillaBlocksToolHandler(
+				ImmutableList.of(
+						new Lazy<>(() -> createAxe(ToolMaterials.WOOD)),
+						new Lazy<>(() -> createAxe(ToolMaterials.STONE)),
+						new Lazy<>(() -> createAxe(ToolMaterials.IRON)),
+						new Lazy<>(() -> createAxe(ToolMaterials.DIAMOND))
+				)
+		));
+		ToolManager.tag(FabricToolTags.SHOVELS).register(new ModdedMiningToolsVanillaBlocksToolHandler(
+				ImmutableList.of(
+						new Lazy<>(() -> createShovel(ToolMaterials.WOOD)),
+						new Lazy<>(() -> createShovel(ToolMaterials.STONE)),
+						new Lazy<>(() -> createShovel(ToolMaterials.IRON)),
+						new Lazy<>(() -> createShovel(ToolMaterials.DIAMOND))
+				)
+		));
+		ToolManager.tag(FabricToolTags.HOES).register(new ModdedMiningToolsVanillaBlocksToolHandler(
+				ImmutableList.of(
+						new Lazy<>(() -> createHoe(ToolMaterials.WOOD)),
+						new Lazy<>(() -> createHoe(ToolMaterials.STONE)),
+						new Lazy<>(() -> createHoe(ToolMaterials.IRON)),
+						new Lazy<>(() -> createHoe(ToolMaterials.DIAMOND))
+				)
+		));
+		ToolManager.tag(FabricToolTags.SWORDS).register(new ModdedMiningToolsVanillaBlocksToolHandler(
+				ImmutableList.of(
+						new Lazy<>(() -> createSword(ToolMaterials.WOOD)),
+						new Lazy<>(() -> createSword(ToolMaterials.STONE)),
+						new Lazy<>(() -> createSword(ToolMaterials.IRON)),
+						new Lazy<>(() -> createSword(ToolMaterials.DIAMOND))
+				)
+		));
 		ToolManager.tag(FabricToolTags.SHEARS).register(new ShearsVanillaBlocksToolHandler());
+	}
+
+	private Item createPickaxe(ToolMaterial material) {
+		return new DummyPickaxeItem(material, 1, -2.8F, new Item.Settings());
+	}
+
+	private Item createAxe(ToolMaterial material) {
+		return new DummyAxeItem(material, 6.0F, -3.2F, new Item.Settings());
+	}
+
+	private Item createShovel(ToolMaterial material) {
+		return new ShovelItem(material, 1.5F, -3.0F, new Item.Settings());
+	}
+
+	private Item createHoe(ToolMaterial material) {
+		return new HoeItem(material, -2.0F, new Item.Settings());
+	}
+
+	private Item createSword(ToolMaterial material) {
+		return new SwordItem(material, 3, -2.4F, new Item.Settings());
+	}
+
+	private static class DummyPickaxeItem extends PickaxeItem {
+		private DummyPickaxeItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+			super(material, attackDamage, attackSpeed, settings);
+		}
+	}
+
+	private static class DummyAxeItem extends AxeItem {
+		public DummyAxeItem(ToolMaterial material, float attackDamage, float attackSpeed, Settings settings) {
+			super(material, attackDamage, attackSpeed, settings);
+		}
 	}
 
 	/**
@@ -124,18 +199,25 @@ public class ToolHandlers implements ModInitializer {
 	 */
 	private static class ModdedMiningToolsVanillaBlocksToolHandler implements ToolManager.ToolHandler {
 		private final FakeAdaptableToolMaterial fakeMaterial = new FakeAdaptableToolMaterial();
-		private final Item vanillaItem;
+		private final ImmutableList<Lazy<Item>> vanillaItems;
 
-		private ModdedMiningToolsVanillaBlocksToolHandler(Item vanillaItem) {
-			this.vanillaItem = vanillaItem;
+		private ModdedMiningToolsVanillaBlocksToolHandler(ImmutableList<Lazy<Item>> vanillaItems) {
+			this.vanillaItems = vanillaItems;
+		}
+
+		private Item getVanillaItem(int miningLevel) {
+			if (miningLevel < 0) return vanillaItems.get(0).get();
+			if (miningLevel >= vanillaItems.size()) return vanillaItems.get(vanillaItems.size() - 1).get();
+			return vanillaItems.get(miningLevel).get();
 		}
 
 		@Override
 		public ActionResult isEffectiveOn(Tag<Item> tag, ItemStack stack, LivingEntity user, BlockState state) {
 			if (stack.getItem() instanceof DynamicAttributeTool) {
-				ToolMaterial tempMaterial = ((ToolItem) vanillaItem).getMaterial();
 				int miningLevel = ((DynamicAttributeTool) stack.getItem()).getMiningLevel(tag, state, stack, user);
 				if (miningLevel < 0) return ActionResult.PASS;
+				Item vanillaItem = getVanillaItem(miningLevel);
+				ToolMaterial tempMaterial = ((ToolItem) vanillaItem).getMaterial();
 				fakeMaterial.miningLevel = miningLevel;
 				((ToolItemAccessor) vanillaItem).setMaterial(fakeMaterial);
 				boolean effective = vanillaItem.isEffectiveOn(state);
@@ -149,6 +231,9 @@ public class ToolHandlers implements ModInitializer {
 		@Override
 		public Float getMiningSpeedMultiplier(Tag<Item> tag, ItemStack stack, LivingEntity user, BlockState state) {
 			if (stack.getItem() instanceof DynamicAttributeTool) {
+				int miningLevel = ((DynamicAttributeTool) stack.getItem()).getMiningLevel(tag, state, stack, user);
+				if (miningLevel < 0) return null;
+				Item vanillaItem = getVanillaItem(miningLevel);
 				float tempMiningSpeed = ((MiningToolItemAccessor) vanillaItem).getMiningSpeed();
 				((MiningToolItemAccessor) vanillaItem).setMiningSpeed(((DynamicAttributeTool) stack.getItem()).getMiningSpeedMultiplier(tag, state, stack, user));
 				float miningSpeed = vanillaItem.getMiningSpeed(stack, state);
