e40501d4d58db30b1127f0e6facddbab02c3981e
Player <player@player.to>
2020-04-22 10:52:20 +0200

    Fix import order to make checkstyle happy

--- fabric-climbable-api-v1/src/main/java/net/fabricmc/fabric/mixin/climbable/MixinLivingEntity.java
+++ fabric-climbable-api-v1/src/main/java/net/fabricmc/fabric/mixin/climbable/MixinLivingEntity.java
@@ -16,17 +16,19 @@
 
 package net.fabricmc.fabric.mixin.climbable;
 
-import net.fabricmc.fabric.impl.climbable.FabricClimbableImpl;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
+
+import net.fabricmc.fabric.impl.climbable.FabricClimbableImpl;
 
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity extends Entity {
