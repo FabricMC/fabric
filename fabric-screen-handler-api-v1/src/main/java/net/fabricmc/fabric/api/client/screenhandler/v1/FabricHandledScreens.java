17a7fa3eb24e5b364922c2f4633304fc574b99a5
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-04-25 10:43:18 +0300

    Replace the content of links in FabricHandledScreens for readability

--- fabric-screen-handler-api-v1/src/main/java/net/fabricmc/fabric/api/client/screenhandler/v1/FabricHandledScreens.java
+++ fabric-screen-handler-api-v1/src/main/java/net/fabricmc/fabric/api/client/screenhandler/v1/FabricHandledScreens.java
@@ -29,7 +29,7 @@ import net.fabricmc.api.Environment;
 
 /**
  * An API for registering screens that represent screen handlers on the client.
- * Exposes vanilla's private {@link HandledScreens#register} to modders as {@link #register}.
+ * Exposes vanilla's private {@link HandledScreens#register HandledScreens.register()} to modders as {@link #register FabricHandlerScreens.register()}.
  *
  * <h2>Example</h2>
  * <pre>
