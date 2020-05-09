ec530c6485e42ca8b5c140246cad1da603e1850c
liach <liach@users.noreply.github.com>
2020-05-09 18:05:16 -0500

    Custom advancement criterion registry
    Fixes #20
    
    Signed-off-by: liach <liach@users.noreply.github.com>

---
+++ fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/api/object/builder/v1/advancement/FabricCriterionRegistry.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.object.builder.v1.advancement;
+
+import net.minecraft.advancement.criterion.Criterion;
+import net.minecraft.util.Identifier;
+
+import net.fabricmc.fabric.mixin.object.builder.CriteriaAccessor;
+
+/**
+ * Allows registering advancement criterions for triggers.
+ *
+ * <p>A registered criteria can be retrieved through
+ * {@link net.minecraft.advancement.criterion.Criteria#getById(Identifier)}.</p>
+ *
+ * @see net.minecraft.advancement.criterion.Criteria
+ */
+public final class FabricCriterionRegistry {
+	/**
+	 * Registers a criterion for a trigger for advancements.
+	 *
+	 * @param criterion the criterion registered
+	 * @throws IllegalArgumentException if a criterion with the same {@link
+	 *                                  Criterion#getId() id} exists
+	 */
+	public static void register(Criterion<?> criterion) {
+		CriteriaAccessor.callRegister(criterion);
+	}
+}
