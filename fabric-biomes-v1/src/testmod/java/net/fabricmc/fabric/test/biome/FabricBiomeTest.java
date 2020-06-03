4996a2c8dcc7821f487c0c1e9f3a5094e8ba2175
Hugman <contact.hugman@gmail.com>
2020-06-03 22:16:53 +0200

    Made TestCrimsonForestBiome an inner class in FabricBiomeTest

--- fabric-biomes-v1/src/testmod/java/net/fabricmc/fabric/test/biome/FabricBiomeTest.java
+++ fabric-biomes-v1/src/testmod/java/net/fabricmc/fabric/test/biome/FabricBiomeTest.java
@@ -19,10 +19,10 @@ package net.fabricmc.fabric.test.biome;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.CrimsonForestBiome;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biomes.v1.NetherBiomes;
-import net.fabricmc.fabric.test.biome.biome.TestCrimsonForestBiome;
 
 public class FabricBiomeTest implements ModInitializer {
 	public static final String MOD_ID = "fabric-biome-api-v1-testmod";
@@ -31,5 +31,8 @@ public class FabricBiomeTest implements ModInitializer {
 		TestCrimsonForestBiome biome = Registry.register(Registry.BIOME, new Identifier(MOD_ID, "test_crimson_forest"), new TestCrimsonForestBiome());
 		NetherBiomes.addNetherBiome(Biomes.BEACH);
 		NetherBiomes.addNetherBiome(biome);
+	}
+
+	public class TestCrimsonForestBiome extends CrimsonForestBiome {
 	}
 }
