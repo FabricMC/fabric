3d52d1e569bf9bce398219525623cc0ac6b7bf90
Meredith Espinosa <Boundarybreaker@users.noreply.github.com>
2020-06-05 03:34:38 -0700

    Make Fabric mod data packs automatically enable on world creation, allow mod data packs to load on dedicated servers (#663)
    * make mod data packs load on dedi servers, auto-enable
    
    * license headers

--- fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/MixinClass_5359.java
+++ fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/MixinClass_5359.java
@@ -16,8 +16,8 @@
 
 package net.fabricmc.fabric.mixin.resource.loader;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,23 +27,35 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.resource.ResourcePackProfile;
-import net.minecraft.resource.ResourcePackProvider;
-import net.minecraft.resource.ResourceType;
+import net.minecraft.class_5359;
 
-import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 
-@Mixin(ResourcePackManager.class)
-public class MixinResourcePackManager<T extends ResourcePackProfile> {
+@Mixin(class_5359.class)
+public class MixinClass_5359 {
 	@Shadow
 	@Final
 	@Mutable
-	private Set<ResourcePackProvider> providers;
+	private List<String> field_25395;
 
+	/*
+	This injection takes all instances of this class with an enabled list that only have the vanilla pack enabled,
+	and forcibly enables all mod resource packs. This is probably not the best option, but it's the only one that I can
+	think of that will work on both existing and new worlds. Is there a better option?
+	 */
 	@Inject(method = "<init>", at = @At("RETURN"))
-	public void construct(ResourcePackProfile.class_5351<T> arg, ResourcePackProvider[] resourcePackProviders, CallbackInfo info) {
-		providers = new HashSet<>(providers);
-		providers.add(new ModResourcePackCreator(ResourceType.SERVER_DATA));
+	private void init(List<String> enabled, List<String> disabled, CallbackInfo info) {
+		if (enabled.size() == 1 && enabled.get(0).equals("vanilla")) {
+			List<String> newEnabled = new ArrayList<>(enabled);
+
+			for (ModContainer container : FabricLoader.getInstance().getAllMods()) {
+				if (!container.getMetadata().getType().equals("builtin")) {
+					newEnabled.add("fabric/" + container.getMetadata().getId());
+				}
+			}
+
+			this.field_25395 = newEnabled;
+		}
 	}
 }
