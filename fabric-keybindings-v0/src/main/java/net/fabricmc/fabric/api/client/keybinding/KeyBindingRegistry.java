3fa9f7c5f885cb3bfdd40ee1819e16418bbdd8c5
shedaniel <daniel@shedaniel.me>
2020-06-12 11:18:17 +0100

    migrate to fabric-keybindings-v1 and remove builder (#615)
    * Edited Clone of #244
    - Fixed checkstyle issues
    - Migrated to fabric-keybindings-v1
    - Removed sticky keybindings from #244 as it sounds useless and you can just around it by simply adding that functionality yourself, I might add it back if someone can tell me the advantages of sticky keys except bloat
    - Added a test mod
    - Added FabricKeyBinding#getBoundKeyOf for getting vanilla bound keys with ease
    - Renamed `registered` to `automaticallyRegister` as that is more of a better name
    - Added a couple Objects.requireNonNull validations
    
    * Add back StickyFabricKeyBinding as it is in vanilla, did not notice.
    
    * Remove extra "key."
    
    * Bump to 1.0.0
    
    * build().register()
    
    * Remove `register()`
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Fix test
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Rename module
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Fix checkstyle violation
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * major refactor
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * revert some stuff
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * fix build
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * major stuff
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * fix license, of course
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Add resource loader v0
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Let's not break the api.
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Rename to buildAndRegister
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * resolve reviews
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Use GLFW
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Dump the builder entirely
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>
    
    * Rename to Key Binding
    
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- fabric-keybindings-v0/src/main/java/net/fabricmc/fabric/api/client/keybinding/KeyBindingRegistry.java
+++ fabric-keybindings-v0/src/main/java/net/fabricmc/fabric/api/client/keybinding/KeyBindingRegistry.java
@@ -18,15 +18,28 @@ package net.fabricmc.fabric.api.client.keybinding;
 
 import net.minecraft.client.options.KeyBinding;
 
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
 
 /**
  * Interface for registering key bindings.
  *
  * @see KeyBinding
+ * @deprecated Please migrate to v1. Please use {@link KeyBindingHelper} instead.
  */
+@Deprecated
 public interface KeyBindingRegistry {
-	KeyBindingRegistry INSTANCE = KeyBindingRegistryImpl.INSTANCE;
+	KeyBindingRegistry INSTANCE = new KeyBindingRegistry() {
+		@Override
+		public boolean addCategory(String categoryName) {
+			return KeyBindingRegistryImpl.addCategory(categoryName);
+		}
+
+		@Override
+		public boolean register(FabricKeyBinding binding) {
+			return KeyBindingRegistryImpl.registerKeyBinding(binding) != null;
+		}
+	};
 
 	/**
 	 * Add a new key binding category.
