a6b45075a2aebe65783b3d54eb166b4ce0f23ae0
Will Toll <will@jtoll.com>
2020-06-12 16:09:32 -0500

    Minor modifications
    - Made FabricStructure protected instead of public
    - Rewrote the default getName implementation to be a smarter use of resources
    - Removed the redundant method to define generation steps
    - Removed the unnecessary appendStructure method as end users ought to never need it and if they do, they are doing something horribly cursed
    - Replaced the access widener with an accessor mixin
    - ran the updateLicenses task
    - Renamed the module following the naming standards
    - Added a newline at the bottom of the module's build.gradle
    - Renamed FabricStructures to FabricStructureHelper
    - Added a call to RegistryEntryAddedCallback in the testmod

--- fabric-structures-v1/src/main/java/net/fabricmc/fabric/api/structures/v1/FabricStructureHelper.java
+++ fabric-structures-v1/src/main/java/net/fabricmc/fabric/api/structures/v1/FabricStructureHelper.java
@@ -1,16 +1,32 @@
-package net.fabricmc.fabric.api.structures.v0;
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package net.fabricmc.fabric.api.structures.v1;
+
+import net.fabricmc.fabric.mixin.structures.StructureFeatureMixin;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.FeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
 
 /**
  * API that hooks into the internal structure generation step logic
  */
-public final class FabricStructures {
-	private FabricStructures() { }
+public final class FabricStructureHelper {
+	private FabricStructureHelper() { }
 
 	/**
 	 * Adds a structure to the given generation step
@@ -19,18 +35,7 @@ public final class FabricStructures {
 	 */
 	public static void setGenerationStep(FabricStructure structure) {
 		// Put if absent was used to avoid vanilla overrides
-		StructureFeature.STRUCTURE_TO_GENERATION_STEP.putIfAbsent(structure, structure.generationStep());
-	}
-
-	/**
-	 * Appends a structure to the internal structure hashmap for serialization
-	 *
-	 * Now if users want to change the name of their structure
-	 *
-	 * @param structure The structure to be appended
-	 */
-	public static void appendStructure(FabricStructure structure) {
-		StructureFeature.STRUCTURES.put(structure.getName(), structure);
+		StructureFeatureMixin.getGenerationStepMap().putIfAbsent(structure, structure.method_28663());
 	}
 
 	/**
@@ -41,7 +46,7 @@ public final class FabricStructures {
 	 * @return
 	 */
 	public static <T extends FeatureConfig> StructureFeature<T> register(Identifier id, FabricStructure<T> structure) {
-		appendStructure(structure);
+		StructureFeature.STRUCTURES.put(structure.getName(), structure);
 		setGenerationStep(structure);
 		return Registry.register(Registry.STRUCTURE_FEATURE, id, structure);
 	}
