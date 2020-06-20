dd4dd52f4a4ee095e7c2568ca38c0bc8e42f1564
RedstoneParadox <spinjitzu00@gmail.com>
2020-06-20 16:51:19 -0700

    Complete API for adding biomes to the end

--- fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/EndBiomes.java
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/EndBiomes.java
@@ -1,4 +1,10 @@
 package net.fabricmc.fabric.api.biomes.v1;
 
+import net.fabricmc.fabric.impl.biome.InternalBiomeData;
+import net.minecraft.world.biome.Biome;
+
 public final class EndBiomes {
+	public static void addBiome(Biome biome, EndRegion region, double weight) {
+		InternalBiomeData.addEndBiome(biome, region, weight);
+	}
 }
---
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/EndRegion.java
@@ -0,0 +1,9 @@
+package net.fabricmc.fabric.api.biomes.v1;
+
+public enum EndRegion {
+	MAIN,
+	HIGHLANDS,
+	MIDLANDS,
+	BARRENS,
+	SMALL_ISLANDS
+}
