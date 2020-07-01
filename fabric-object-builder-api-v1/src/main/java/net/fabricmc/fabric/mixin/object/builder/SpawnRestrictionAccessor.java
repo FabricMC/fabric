0d27a3c6b0c06f53b4320e48c8d04f91fb4a1606
Yanis48 <doublecraft.official@gmail.com>
2020-07-01 15:51:02 +0200

    checkstyle²

--- fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/mixin/object/builder/SpawnRestrictionAccessor.java
+++ fabric-object-builder-api-v1/src/main/java/net/fabricmc/fabric/mixin/object/builder/SpawnRestrictionAccessor.java
@@ -16,12 +16,13 @@
 
 package net.fabricmc.fabric.mixin.object.builder;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnRestriction;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.world.Heightmap;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(SpawnRestriction.class)
 public interface SpawnRestrictionAccessor {
