d532d74b8374fef45e8bb5451f49302ea6015e0d
i509VCB <git@i509.me>
2020-07-24 10:25:09 -0500

    Create Game Rule API (#641)
    * Create GameRule API
    
    * Some tweaks
    
    * gamerule -> game-rule
    
    * Fix translation keys and update to 20w22a
    
    * Command results and remove unnessecary factory method
    
    * Update to 1.16-pre1
    
    * Update fabric-game-rule-api-v1/src/main/java/net/fabricmc/fabric/api/gamerule/v1/FabricRuleTypeConsumer.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * I like final things
    
    * Update to 1.16-pre3, change enum button style
    
    * checkstyle
    
    * use right min values
    
    * Document ValidatableRule
    
    * Document LiteralRule, clarify generics
    
    * Update to 20w27a
    
    * Rename some parts to compensate for future yarn renames.
    
    Flatten some logic related to EnumRules
    
    * forgot one
    
    * javadoc galore
    
    * finish javadoc
    
    * Start things
    
    * Update to 20w29a, drop float rule
    
    * Make cycle naming more accurate
    
    * Convert colors to hex
    
    Co-authored-by: Erlend Åmdal <erlend@aamdal.com>
    
    * Update to 20w30a
    
    * imports again
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    Co-authored-by: Prospector <6166773+Prospector@users.noreply.github.com>
    Co-authored-by: Erlend Åmdal <erlend@aamdal.com>

---
+++ fabric-game-rule-api-v1/src/main/java/net/fabricmc/fabric/api/gamerule/v1/rule/EnumRule.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.gamerule.v1.rule;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import com.mojang.brigadier.context.CommandContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.world.GameRules;
+
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+
+public final class EnumRule<E extends Enum<E>> extends GameRules.Rule<EnumRule<E>> {
+	private static final Logger LOGGER = LogManager.getLogger(GameRuleRegistry.class);
+
+	private final Class<E> classType;
+	private final List<E> supportedValues;
+	private E value;
+
+	/**
+	 * @deprecated You should not be calling this constructor!
+	 */
+	@Deprecated
+	public EnumRule(GameRules.Type<EnumRule<E>> type, E value, E[] supportedValues) {
+		this(type, value, Arrays.asList(supportedValues));
+	}
+
+	/**
+	 * You should not be calling this constructor!
+	 */
+	@Deprecated
+	public EnumRule(GameRules.Type<EnumRule<E>> type, E value, Collection<E> supportedValues) {
+		super(type);
+		this.classType = value.getDeclaringClass();
+		this.value = value;
+		this.supportedValues = new ArrayList<>(supportedValues);
+
+		if (!this.supports(value)) {
+			throw new IllegalArgumentException("Cannot set default value");
+		}
+	}
+
+	@Override
+	protected void setFromArgument(CommandContext<ServerCommandSource> context, String name) {
+		// Do nothing. We use a different system for application with literals
+	}
+
+	@Override
+	protected void deserialize(String value) {
+		try {
+			final E deserialized = Enum.valueOf(this.classType, value);
+
+			if (!this.supports(deserialized)) {
+				LOGGER.warn("Failed to parse rule of value {} for rule of type {}. Since the value {}, is unsupported.", value, this.classType, value);
+			}
+
+			this.set(deserialized, null);
+		} catch (IllegalArgumentException e) {
+			LOGGER.warn("Failed to parse rule of value {} for rule of type {}", value, this.classType);
+		}
+	}
+
+	@Override
+	public String serialize() {
+		return this.value.name();
+	}
+
+	@Override
+	public int getCommandResult() {
+		// For now we are gonna use the ordinal as the command result. Could be changed or set to relate to something else entirely.
+		return this.value.ordinal();
+	}
+
+	@Override
+	protected EnumRule<E> getThis() {
+		return this;
+	}
+
+	public Class<E> getEnumClass() {
+		return this.classType;
+	}
+
+	@Override
+	public String toString() {
+		return this.value.toString();
+	}
+
+	@Override
+	protected EnumRule<E> copy() {
+		return new EnumRule<>(this.type, this.value, this.supportedValues);
+	}
+
+	@Override
+	public void setValue(EnumRule<E> rule, MinecraftServer minecraftServer) {
+		if (!this.supports(rule.value)) {
+			throw new IllegalArgumentException(String.format("Rule does not support value: %s", rule.value));
+		}
+
+		this.value = rule.value;
+		this.changed(minecraftServer);
+	}
+
+	public E get() {
+		return this.value;
+	}
+
+	/**
+	 * Cycles the value of this enum rule to the next supported value.
+	 */
+	public void cycle() {
+		int index = this.supportedValues.indexOf(this.value);
+
+		if (index < 0) {
+			throw new IllegalArgumentException(String.format("Invalid value: %s", this.value));
+		}
+
+		this.set(this.supportedValues.get((index + 1) % this.supportedValues.size()), null);
+	}
+
+	public boolean supports(E value) {
+		return this.supportedValues.contains(value);
+	}
+
+	public void set(E value, /* @Nullable */ MinecraftServer server) throws IllegalArgumentException {
+		checkNotNull(value);
+
+		if (!this.supports(value)) {
+			throw new IllegalArgumentException("Tried to set an unsupported value: " + value.toString());
+		}
+
+		this.value = value;
+		this.changed(server);
+	}
+}
---
+++ fabric-game-rule-api-v1/src/main/java/net/fabricmc/fabric/mixin/gamerule/GameRuleCommandVisitorMixin.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.gamerule;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.world.GameRules;
+
+import net.fabricmc.fabric.impl.gamerule.EnumRuleCommand;
+import net.fabricmc.fabric.impl.gamerule.EnumRuleType;
+
+@Mixin(targets = "net/minecraft/server/command/GameRuleCommand$1")
+public abstract class GameRuleCommandVisitorMixin {
+	@Shadow
+	private LiteralArgumentBuilder<ServerCommandSource> field_19419;
+
+	@Inject(at = @At("HEAD"), method = "visit(Lnet/minecraft/world/GameRules$Key;Lnet/minecraft/world/GameRules$Type;)V", cancellable = true)
+	private <T extends GameRules.Rule<T>> void onRegisterCommand(GameRules.Key<T> key, GameRules.Type<T> type, CallbackInfo ci) {
+		// Check if our type is a EnumRuleType
+		if (type instanceof EnumRuleType) {
+			//noinspection rawtypes,unchecked
+			EnumRuleCommand.register(this.field_19419, (GameRules.Key) key, (EnumRuleType) type);
+			ci.cancel();
+		}
+	}
+}
