e8b75a5dd30a4409e997680e5e971abecf8a8779
Joe <burtonjae@hotmail.co.uk>
2020-07-26 17:03:45 +0100

    Documentation and null assertions

--- fabric-structure-api-v1/src/main/java/net/fabricmc/fabric/api/structure/v1/FabricStructureBuilder.java
+++ fabric-structure-api-v1/src/main/java/net/fabricmc/fabric/api/structure/v1/FabricStructureBuilder.java
@@ -16,6 +16,8 @@
 
 package net.fabricmc.fabric.api.structure.v1;
 
+import java.util.Objects;
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
@@ -33,6 +35,24 @@ import net.fabricmc.fabric.mixin.structure.FlatChunkGeneratorConfigAccessor;
 import net.fabricmc.fabric.mixin.structure.StructureFeatureAccessor;
 import net.fabricmc.fabric.mixin.structure.StructuresConfigAccessor;
 
+/**
+ * A builder for registering custom structures.
+ *
+ * <p>Example usage:
+ * <pre>{@code
+ * StructureFeature structure = new MyStructure(DefaultFeatureConfig.CODEC);
+ * ConfiguredStructureFeature<DefaultFeatureConfig, ? extends StructureFeature<DefaultFeatureConfig>> configuredStructure
+ *     = structure.configure(new DefaultFeatureConfig());
+ * FabricStructureBuilder.create()
+ *     .step(GenerationStep.Feature.SURFACE_STRUCTURES) // required
+ *     .defaultConfig(32, 8, 12345) // required
+ *     .superflatFeature(configuredStructure)
+ *     .register();}
+ * </pre></p>
+ *
+ * <p>This class does <i>not</i> add structures to biomes for you, you have to do that yourself. You may also need to
+ * register custom structure pieces yourself.</p>
+ */
 public class FabricStructureBuilder<FC extends FeatureConfig, S extends StructureFeature<FC>> {
 	private final Identifier id;
 	private final S structure;
@@ -46,46 +66,104 @@ public class FabricStructureBuilder<FC extends FeatureConfig, S extends Structur
 		this.structure = structure;
 	}
 
+	/**
+	 * Creates a new {@code FabricStructureBuilder} for registering a structure.
+	 *
+	 * @param id The structure ID.
+	 * @param structure The {@linkplain StructureFeature} you want to register.
+	 */
 	public static <FC extends FeatureConfig, S extends StructureFeature<FC>> FabricStructureBuilder<FC, S> create(Identifier id, S structure) {
+		Objects.requireNonNull(id, "id must not be null");
+		Objects.requireNonNull(structure, "structure must not be null");
 		return new FabricStructureBuilder<>(id, structure);
 	}
 
+	/**
+	 * Sets the generation step of this structure. The generation step specifies when the structure is generated, to
+	 * ensure they are generated in the correct order to reduce the amount of floating blocks.
+	 *
+	 * <p>The most commonly used values for structures are {@linkplain GenerationStep.Feature#SURFACE_STRUCTURES} and
+	 * {@linkplain GenerationStep.Feature#UNDERGROUND_STRUCTURES}, however technically any value in the
+	 * {@linkplain GenerationStep.Feature} enum may be used.</p>
+	 *
+	 * <p>This is a required option.</p>
+	 */
 	public FabricStructureBuilder<FC, S> step(GenerationStep.Feature step) {
+		Objects.requireNonNull(step, "step must not be null");
 		this.step = step;
 		return this;
 	}
 
+	/**
+	 * Sets the default {@linkplain StructureConfig} for this structure. See the alternative
+	 * {@linkplain #defaultConfig(int, int, int)} for details.
+	 *
+	 * <p>This is a required option.</p>
+	 */
 	public FabricStructureBuilder<FC, S> defaultConfig(StructureConfig config) {
+		Objects.requireNonNull(config, "config must not be null");
 		this.defaultConfig = config;
 		return this;
 	}
 
+	/**
+	 * Sets the default {@linkplain StructureConfig} for this structure. This sets the default configuration of where in
+	 * the world to place structures.
+	 *
+	 * <p>Note: the {@code spacing} and {@code separation} options are subject to other checks for whether the structure
+	 * can spawn, such as biome. If these checks always pass and the structure can spawn in every biome, then the
+	 * description of these values below would be exactly correct.</p>
+	 *
+	 * <p>This is a required option. Vanilla needs it to function.</p>
+	 *
+	 * @param spacing The minimum distance between 2 structures of this type.
+	 * @param separation The average distance between 2 structures of this type along the X and Z axes.
+	 * @param salt The random salt of the structure. This does not affect how common the structure is, but every
+	 *                structure must have an unique {@code salt} in order to spawn in different places.
+	 *
+	 * @see #defaultConfig(StructureConfig)
+	 */
 	public FabricStructureBuilder<FC, S> defaultConfig(int spacing, int separation, int salt) {
 		return defaultConfig(new StructureConfig(spacing, separation, salt));
 	}
 
+	/**
+	 * Sets the structure configuration which spawns in superflat worlds. If unset, this structure will not spawn in
+	 * superflat worlds.
+	 *
+	 * @see #superflatFeature(FeatureConfig)
+	 */
 	public FabricStructureBuilder<FC, S> superflatFeature(ConfiguredStructureFeature<FC, ? extends StructureFeature<FC>> superflatFeature) {
+		Objects.requireNonNull(superflatFeature, "superflatFeature must not be null");
 		this.superflatFeature = superflatFeature;
 		return this;
 	}
 
+	/**
+	 * Sets the structure configuration which spawns in superflat worlds. If unset, this structure will not spawn in
+	 * superflat worlds.
+	 *
+	 * @see #superflatFeature(ConfiguredStructureFeature)
+	 */
 	public FabricStructureBuilder<FC, S> superflatFeature(FC config) {
 		return superflatFeature(structure.configure(config));
 	}
 
+	/**
+	 * Causes structure pieces of this structure to adjust the surface of the world to fit them, so that they don't
+	 * stick out of or into the ground.
+	 */
 	public FabricStructureBuilder<FC, S> adjustsSurface() {
 		this.adjustsSurface = true;
 		return this;
 	}
 
+	/**
+	 * Registers this structure and applies the other changes from the {@linkplain FabricStructureBuilder}.
+	 */
 	public S register() {
-		if (step == null) {
-			throw new IllegalStateException("Structure \"" + id + "\" is missing a generation step");
-		}
-
-		if (defaultConfig == null) {
-			throw new IllegalStateException("Structure \"" + id + "\" is missing a default config");
-		}
+		Objects.requireNonNull(step, "Structure \"" + id + "\" is missing a generation step");
+		Objects.requireNonNull(defaultConfig, "Structure \"" + id + "\" is missing a default config");
 
 		// Ensure StructuresConfig class is initialized, so the assertion in its static {} block doesn't fail
 		StructuresConfig.DEFAULT_STRUCTURES.size();
