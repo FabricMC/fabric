8b5bf5f86fd5415e5a582f2944ae67bc5ba6795b
Hugman <contact.hugman@gmail.com>
2020-07-28 17:55:15 +0200

    Updated v0 for backwards compatibility

--- fabric-object-builders-v0/src/main/java/net/fabricmc/fabric/api/entity/FabricEntityTypeBuilder.java
+++ fabric-object-builders-v0/src/main/java/net/fabricmc/fabric/api/entity/FabricEntityTypeBuilder.java
@@ -81,12 +81,12 @@ public class FabricEntityTypeBuilder<T extends Entity> {
 	}
 
 	public FabricEntityTypeBuilder<T> trackable(int trackingDistanceBlocks, int updateIntervalTicks) {
-		this.delegate.trackable(trackingDistanceBlocks, updateIntervalTicks);
+		this.delegate.trackBlockDistance(trackingDistanceBlocks).updateIntervalTicks(updateIntervalTicks).alwaysUpdateVelocity(true);
 		return this;
 	}
 
 	public FabricEntityTypeBuilder<T> trackable(int trackingDistanceBlocks, int updateIntervalTicks, boolean alwaysUpdateVelocity) {
-		this.delegate.trackable(trackingDistanceBlocks, updateIntervalTicks, alwaysUpdateVelocity);
+		this.delegate.trackBlockDistance(trackingDistanceBlocks).updateIntervalTicks(updateIntervalTicks).alwaysUpdateVelocity(alwaysUpdateVelocity);
 		return this;
 	}
 
