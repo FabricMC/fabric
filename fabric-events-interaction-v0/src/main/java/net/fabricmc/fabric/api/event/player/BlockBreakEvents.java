2d81e83a379aa66281bca815a9c7493c363f7baa
Jai A <jai.a@tuta.io>
2020-08-03 11:07:36 -0700

    Move JavaDoc + Make success have same function as pass

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
@@ -34,8 +34,8 @@ public final class BlockBreakEvents {
 	 * Only called on the server, however updates are synced with the client.
 	 *
 	 * <p>Upon return:
-	 * <ul><li>SUCCESS/CONSUME continues the default code for breaking the block
-	 * <li>PASS passes on the callback to the next listener
+	 * <ul><li>SUCCESS/PASS passes on the callback to the next listener
+	 * <li>CONSUME continues the default code for breaking the block and ignores all other listeners
 	 * <li>FAIL cancels the block breaking action</ul>
 	 */
 	public static final Event<Before> BEFORE = EventFactory.createArrayBacked(Before.class,
@@ -76,47 +76,47 @@ public final class BlockBreakEvents {
 			}
 	);
 
-	/* Called before a block is broken
-	 *
-	 * <p>Fields:
-	 * <ul><li> world - The world at which the block is being broken
-	 * <li> player - The player who is breaking the block
-	 * <li> pos - The position at which the block is being broken
-	 * <li> state - The block state from BEFORE the block is broken
-	 * <li> entity - The block entity from BEFORE the block is broken (can be null)
-	 */
 	@FunctionalInterface
 	public interface Before {
+		/* Called before a block is broken
+		 *
+		 * <p>Fields:
+		 * <ul><li> world - The world at which the block is being broken
+		 * <li> player - The player who is breaking the block
+		 * <li> pos - The position at which the block is being broken
+		 * <li> state - The block state from BEFORE the block is broken
+		 * <li> entity - The block entity from BEFORE the block is broken (can be null)
+		 */
 		ActionResult beforeBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, /* Nullable */ BlockEntity entity);
 	}
 
-	/* Called after a block is broken
-	 *
-	 * <p>Fields:
-	 * <ul><li> world - The world where the block was broken
-	 * <li> player - The player who broke the block
-	 * <li> pos - The position where the block was broken
-	 * <li> state - The block state from AFTER the block was broken
-	 * <li> entity - The block entity of the broken block (can be null)
-	 * <li> block - The block instance of the block that was broken</ul>
-	 */
 	@FunctionalInterface
 	public interface After {
+		/* Called after a block is broken
+		 *
+		 * <p>Fields:
+		 * <ul><li> world - The world where the block was broken
+		 * <li> player - The player who broke the block
+		 * <li> pos - The position where the block was broken
+		 * <li> state - The block state from AFTER the block was broken
+		 * <li> entity - The block entity of the broken block (can be null)
+		 * <li> block - The block instance of the block that was broken</ul>
+		 */
 		void afterBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, /* Nullable */ BlockEntity entity);
 	}
 
-	/* Called when a block break has been canceled
-	 *
-	 * <p>Fields:
-	 * <ul><li> world - The world where the block was going to be broken
-	 * <li> player - The player was going to break the block
-	 * <li> pos - The position where the block was going to be broken
-	 * <li> state - The block state of the block that was going to be broken
-	 * <li> entity - The block entity of the block that was going to be broken (can be null)
-	 * <li> block - The block instance of the block that was going to be broken</ul>
-	 */
 	@FunctionalInterface
 	public interface Cancel {
+		/* Called when a block break has been canceled
+		 *
+		 * <p>Fields:
+		 * <ul><li> world - The world where the block was going to be broken
+		 * <li> player - The player was going to break the block
+		 * <li> pos - The position where the block was going to be broken
+		 * <li> state - The block state of the block that was going to be broken
+		 * <li> entity - The block entity of the block that was going to be broken (can be null)
+		 * <li> block - The block instance of the block that was going to be broken</ul>
+		 */
 		void onBlockBreakCancel(World world, PlayerEntity player, BlockPos pos, BlockState state, /* Nullable */ BlockEntity entity);
 	}
 }
