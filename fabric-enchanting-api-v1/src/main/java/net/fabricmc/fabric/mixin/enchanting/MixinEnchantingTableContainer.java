9006a0844c7c91b3626fe8d5664462a52bfe00e4
Siphalor <info@siphalor.de>
2020-08-06 22:18:14 +0200

    Improve mixin usage

--- fabric-enchanting-api-v1/src/main/java/net/fabricmc/fabric/mixin/enchanting/MixinEnchantingTableContainer.java
+++ fabric-enchanting-api-v1/src/main/java/net/fabricmc/fabric/mixin/enchanting/MixinEnchantingTableContainer.java
@@ -19,13 +19,10 @@ package net.fabricmc.fabric.mixin.enchanting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.container.EnchantingTableContainer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
@@ -34,34 +31,22 @@ import net.fabricmc.fabric.api.enchanting.v1.EnchantingPowerProvider;
 
 @Mixin(EnchantingTableContainer.class)
 public abstract class MixinEnchantingTableContainer {
-	@Unique
-	private World world;
-	@Unique
-	private BlockPos blockPos;
-
-	@SuppressWarnings("UnresolvedMixinReference")
-	@Inject(method = "method_17411(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V", at = @At("HEAD"))
-	private void onEnchantmentCalculation(ItemStack itemStack, World world, BlockPos blockPos, CallbackInfo callbackInfo) {
-		this.world = world;
-		this.blockPos = blockPos;
-	}
-
 	@SuppressWarnings("UnresolvedMixinReference")
 	@ModifyVariable(method = "method_17411(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;)V", at = @At(value = "INVOKE", target = "Ljava/util/Random;setSeed(J)V", shift = At.Shift.BEFORE), ordinal = 0)
-	private int changeEnchantingPower(int power) {
+	private int changeEnchantingPower(int power, World world, BlockPos blockPos) {
 		BlockPos.Mutable position = new BlockPos.Mutable();
 
 		for (int offsetZ = -1; offsetZ <= 1; ++offsetZ) {
 			for (int offsetX = -1; offsetX <= 1; ++offsetX) {
 				if ((offsetZ != 0 || offsetX != 0) && world.isAir(position.set(blockPos).setOffset(offsetX, 0, offsetZ)) && world.isAir(position.setOffset(Direction.UP))) {
-					power += getEnchantingPower(position.set(blockPos).setOffset(offsetX * 2, 0, offsetZ * 2));
-					power += getEnchantingPower(position.setOffset(Direction.UP));
+					power += getEnchantingPower(world, position.set(blockPos).setOffset(offsetX * 2, 0, offsetZ * 2));
+					power += getEnchantingPower(world, position.setOffset(Direction.UP));
 
 					if (offsetX != 0 && offsetZ != 0) {
-						power += getEnchantingPower(position.set(blockPos).setOffset(offsetX * 2, 0, offsetZ));
-						power += getEnchantingPower(position.setOffset(Direction.UP));
-						power += getEnchantingPower(position.set(blockPos).setOffset(offsetX, 0, offsetZ * 2));
-						power += getEnchantingPower(position.setOffset(Direction.UP));
+						power += getEnchantingPower(world, position.set(blockPos).setOffset(offsetX * 2, 0, offsetZ));
+						power += getEnchantingPower(world, position.setOffset(Direction.UP));
+						power += getEnchantingPower(world, position.set(blockPos).setOffset(offsetX, 0, offsetZ * 2));
+						power += getEnchantingPower(world, position.setOffset(Direction.UP));
 					}
 				}
 			}
@@ -71,7 +56,7 @@ public abstract class MixinEnchantingTableContainer {
 	}
 
 	@Unique
-	private int getEnchantingPower(BlockPos blockPos) {
+	private int getEnchantingPower(World world, BlockPos blockPos) {
 		BlockState blockState = world.getBlockState(blockPos);
 
 		if (blockState.getBlock() instanceof EnchantingPowerProvider) {
