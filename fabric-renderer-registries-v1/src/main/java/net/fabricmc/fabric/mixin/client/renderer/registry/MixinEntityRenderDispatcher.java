e2862de602a3145a1b76ef5f82f046d15db0f48b
i509VCB <git@i509.me>
2020-08-07 13:49:39 -0500

    Add callback to allow registration of feature renderers. (#873)
    * Add callback to allow registration of feature renderers.
    
    * Laymans terms for those who don't map yarn for a living.
    
    * Be a little less generic
    
    * Dispatch events in a better spot, play with generics again, move to renderer-registries.
    
    Also move this to render registries
    
    * Handle players as well
    
    * Tweak generics
    
    * Hide acceptor impl
    
    * Add actual testmod, fix where player events are fired, Simplify to returning a list.
    
    * the old style worked fine
    
    * registerRenderers
    
    * Rename the event interface
    
    * Javadoc fixes

--- fabric-renderer-registries-v1/src/main/java/net/fabricmc/fabric/mixin/client/renderer/registry/MixinEntityRenderDispatcher.java
+++ fabric-renderer-registries-v1/src/main/java/net/fabricmc/fabric/mixin/client/renderer/registry/MixinEntityRenderDispatcher.java
@@ -18,6 +18,7 @@ package net.fabricmc.fabric.mixin.client.renderer.registry;
 
 import java.util.Map;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -26,20 +27,51 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.LivingEntityRenderer;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.resource.ReloadableResourceManager;
 
 import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendereregistry.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.fabricmc.fabric.impl.client.renderer.registry.RegistrationHelperImpl;
 
 @Mixin(EntityRenderDispatcher.class)
-public class MixinEntityRenderDispatcher {
+public abstract class MixinEntityRenderDispatcher {
 	@Shadow
-	Map<EntityType<?>, EntityRenderer<?>> renderers;
+	@Final
+	private Map<EntityType<?>, EntityRenderer<?>> renderers;
 
-	@Inject(method = "registerRenderers", at = @At(value = "RETURN"), require = 1)
-	public void on_method_23167(ItemRenderer itemRenderer, ReloadableResourceManager manager, CallbackInfo info) {
+	@Shadow
+	@Final
+	private Map<String, PlayerEntityRenderer> modelRenderers;
+
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	@Inject(method = "registerRenderers", at = @At(value = "TAIL"))
+	public void onRegisterRenderers(ItemRenderer itemRenderer, ReloadableResourceManager manager, CallbackInfo info) {
 		final EntityRenderDispatcher me = (EntityRenderDispatcher) (Object) this;
 		EntityRendererRegistry.INSTANCE.initialize(me, me.textureManager, manager, itemRenderer, renderers);
+
+		// Dispatch events to register feature renderers.
+		for (Map.Entry<EntityType<?>, EntityRenderer<?>> entry : this.renderers.entrySet()) {
+			if (entry.getValue() instanceof LivingEntityRenderer) { // Must be living for features
+				LivingEntityRendererAccessor accessor = (LivingEntityRendererAccessor) entry.getValue();
+
+				LivingEntityFeatureRendererRegistrationCallback.EVENT.invoker().registerRenderers((EntityType<? extends LivingEntity>) entry.getKey(), (LivingEntityRenderer) entry.getValue(), new RegistrationHelperImpl(accessor::callAddFeature));
+			}
+		}
+	}
+
+	@SuppressWarnings({"unchecked", "rawtypes"})
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void afterRegisterPlayerModels(CallbackInfo ci) {
+		// Players are a fun case, we need to do these separately and per model type
+		for (Map.Entry<String, PlayerEntityRenderer> entry : this.modelRenderers.entrySet()) {
+			LivingEntityRendererAccessor accessor = (LivingEntityRendererAccessor) entry.getValue();
+
+			LivingEntityFeatureRendererRegistrationCallback.EVENT.invoker().registerRenderers(EntityType.PLAYER, entry.getValue(), new RegistrationHelperImpl(accessor::callAddFeature));
+		}
 	}
 }
