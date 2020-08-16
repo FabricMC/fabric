96b20b860bb9643502a60b38108fb3c58efb7c0c
i509VCB <git@i509.me>
2020-08-15 19:02:11 -0500

    Move inner class out, fix formatting issue in loot-tables with linux oses

---
+++ fabric-rendering-v1/src/main/java/net/fabricmc/fabric/api/client/rendering/v1/DynamicItemRenderer.java
@@ -0,0 +1,34 @@
+package net.fabricmc.fabric.api.client.rendering.v1;
+
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.model.json.ModelTransformation;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+/**
+ * Dynamic item renderers render items with custom code.
+ * They allow using non-model rendering, such as BERs, for items.
+ *
+ * <p>An item with a dynamic renderer must have a model extending {@code minecraft:builtin/entity}.
+ * The renderers are registered with {@link BuiltinItemRendererRegistry#register(ItemConvertible, DynamicItemRenderer)}.
+ */
+@FunctionalInterface
+@Environment(EnvType.CLIENT)
+public
+interface DynamicItemRenderer {
+	/**
+	 * Renders an item stack.
+	 *
+	 * @param stack           the rendered item stack
+	 * @param mode            the model transformation mode
+	 * @param matrices        the matrix stack
+	 * @param vertexConsumers the vertex consumer provider
+	 * @param light           packed lightmap coordinates
+	 * @param overlay         the overlay UV passed to {@link net.minecraft.client.render.VertexConsumer#overlay(int)}
+	 */
+	void render(ItemStack stack, ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay);
+}
--- fabric-rendering-v1/src/main/java/net/fabricmc/fabric/impl/client/rendering/BuiltinItemRendererRegistryImpl.java
+++ fabric-rendering-v1/src/main/java/net/fabricmc/fabric/impl/client/rendering/BuiltinItemRendererRegistryImpl.java
@@ -28,6 +28,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.DynamicItemRenderer;
 
 @Environment(EnvType.CLIENT)
 public final class BuiltinItemRendererRegistryImpl implements BuiltinItemRendererRegistry {
