e2470d6b9f74badb90b9d515375549d733c16352
TheBrokenRail <connor24nolan@live.com>
2020-08-17 20:16:30 -0400

    Add BlockBreakEffectsCallback

---
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/BlockBreakEffectsCallback.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2020 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.event;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+/**
+ * Callback for the effects displayed when a block is broken (particles and sounds)
+ *
+ * <p>Upon return:
+ * <ul><li>SUCCESS cancels further processing.
+ * <li>PASS falls back to further processing.
+ * <li>FAIL cancels further processing and prevents the block break effects from being displayed.</ul>
+ */
+public interface BlockBreakEffectsCallback {
+	Event<BlockBreakEffectsCallback> EVENT = EventFactory.createArrayBacked(BlockBreakEffectsCallback.class,
+			(listeners) -> (world, breakingEntity, pos) -> {
+				for (BlockBreakEffectsCallback event : listeners) {
+					ActionResult result = event.run(world, breakingEntity, pos);
+
+					if (result != ActionResult.PASS) {
+						return result;
+					}
+				}
+
+				return ActionResult.PASS;
+			}
+	);
+
+	ActionResult run(World world, Entity breakingEntity, BlockPos pos);
+}
---
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/mixin/event/interaction/MixinWorld.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.event.interaction;
+
+import net.fabricmc.fabric.api.event.BlockBreakEffectsCallback;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(World.class)
+public class MixinWorld {
+	@Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;syncWorldEvent(ILnet/minecraft/util/math/BlockPos;I)V"), method = "breakBlock")
+	public void breakBlock(World world, int eventId, BlockPos pos, int data, BlockPos pos2, boolean drop, Entity breakingEntity, int maxUpdateDepth) {
+		if (BlockBreakEffectsCallback.EVENT.invoker().run(world, breakingEntity, pos) != ActionResult.PASS) {
+			world.syncWorldEvent(eventId, pos, data);
+		}
+	}
+}
