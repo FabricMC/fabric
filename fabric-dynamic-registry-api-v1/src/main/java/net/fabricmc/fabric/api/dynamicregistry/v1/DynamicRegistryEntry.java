01d667cfc4213b3e61f2f771dad86d2a79738e4d
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-08-19 17:04:47 -0400

    Create the fabric-dynamic-registry-api-v1 module

---
+++ fabric-dynamic-registry-api-v1/src/main/java/net/fabricmc/fabric/api/dynamicregistry/v1/DynamicRegistryEntry.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.dynamicregistry.v1;
+
+import java.util.function.Supplier;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.Lifecycle;
+
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.util.registry.SimpleRegistry;
+
+public class DynamicRegistryEntry<T> {
+	private final SimpleRegistry<T> registry;
+	private final Supplier<T> defaultValueSupplier;
+	private final Codec<T> codec;
+
+	public DynamicRegistryEntry(SimpleRegistry<T> registry, Supplier<T> defaultValueSupplier, Codec<T> codec) {
+		this.registry = registry;
+		this.defaultValueSupplier = defaultValueSupplier;
+		this.codec = codec;
+	}
+
+	public SimpleRegistry<T> getRegistry() {
+		return this.registry;
+	}
+
+	public RegistryKey<? extends Registry<T>> getRegistryRef() {
+		return this.registry.getKey();
+	}
+
+	public Lifecycle getLifecycle() {
+		return this.registry.method_31138();
+	}
+
+	public Supplier<T> getDefaultValueSupplier() {
+		return this.defaultValueSupplier;
+	}
+
+	public DynamicRegistryManager.Info<?> getInfo() {
+		return new DynamicRegistryManager.Info<>(this.getRegistryRef(), this.codec, null);
+	}
+}
---
+++ fabric-dynamic-registry-api-v1/src/main/java/net/fabricmc/fabric/mixin/dynamicregistry/DynamicRegistryManagerMixin.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.dynamicregistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.ImmutableMap;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.DynamicRegistryManager;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+
+import net.fabricmc.fabric.api.dynamicregistry.v1.DynamicRegistryEntry;
+import net.fabricmc.fabric.api.dynamicregistry.v1.DynamicRegistryProvider;
+import net.fabricmc.loader.api.FabricLoader;
+
+@Mixin(DynamicRegistryManager.class)
+public class DynamicRegistryManagerMixin {
+	@Inject(method = "method_30531", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/registry/DynamicRegistryManager;register(Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/util/registry/RegistryKey;Lcom/mojang/serialization/Codec;)V", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
+	private static void registerCustomDynamicRegistries(CallbackInfoReturnable<ImmutableMap<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>>> ci, ImmutableMap.Builder<RegistryKey<? extends Registry<?>>, DynamicRegistryManager.Info<?>> builder) {
+		List<DynamicRegistryEntry<?>> entries = new ArrayList<>();
+
+		List<DynamicRegistryProvider> providers = FabricLoader.getInstance().getEntrypoints("dynamic-registry-provider", DynamicRegistryProvider.class);
+
+		for (DynamicRegistryProvider provider : providers) {
+			provider.getDynamicRegistries(entries);
+		}
+
+		for (DynamicRegistryEntry<?> entry : entries) {
+			addRegistry(entry);
+			builder.put(entry.getRegistryRef(), entry.getInfo());
+		}
+	}
+
+	@Unique
+	private static <T> void addRegistry(DynamicRegistryEntry<T> entry) {
+		BuiltinRegistries.addRegistry(entry.getRegistryRef(), entry.getRegistry(), entry.getDefaultValueSupplier(), entry.getLifecycle());
+	}
+}
