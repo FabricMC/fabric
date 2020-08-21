e2e6cdad60f5a43c3f4c226eab80eb96c1743f17
Geometrically <18202329+Geometrically@users.noreply.github.com>
2020-08-21 09:22:11 -0700

    Block break event (#980)
    * Block break event
    
    * License headers + after event
    
    * Before and after events, testmod
    
    * Fix checkstyle
    
    * Version bump, fix client method call
    
    * Move to one event class
    
    * Expand event parameters + javadoc
    
    * Add cancelation event and move javadocs
    
    * Move JavaDoc + Make success have same function as pass
    
    * Fix success bug
    
    * Fix documentation again, change approach
    
    * Fix checkstyle
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/BlockBreakEvents.java
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    
    * Fix checkstyle
    
    * Rename stuff
    
    * fixes
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlayerBlockBreakEvents.java
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlayerBlockBreakEvents.java
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/impl/event/interaction/InteractionEventsRouter.java
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Update fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/PlayerBlockBreakEvents.java
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Rename Canceled Event Method Name
    
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>
    Co-authored-by: i509VCB <i509vcb@gmail.com>

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/impl/event/interaction/InteractionEventsRouter.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/impl/event/interaction/InteractionEventsRouter.java
@@ -16,9 +16,13 @@
 
 package net.fabricmc.fabric.impl.event.interaction;
 
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.BlockState;
+import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.block.BlockAttackInteractionAware;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
@@ -41,5 +45,22 @@ public class InteractionEventsRouter implements ModInitializer {
 
 			return ActionResult.PASS;
 		});
+
+		/*
+		* This code is for telling the client that the block wasn't actually broken.
+		* This covers a 3x3 area due to how vanilla redstone handles updates, as it considers
+		* important functions like quasi-connectivity and redstone dust logic
+		 */
+		PlayerBlockBreakEvents.CANCELED.register(((world, player, pos, state, blockEntity) -> {
+			BlockPos cornerPos = pos.add(-1, -1, -1);
+
+			for (int x = 0; x < 3; x++) {
+				for (int y = 0; y < 3; y++) {
+					for (int z = 0; z < 3; z++) {
+						((ServerPlayerEntity) player).networkHandler.sendPacket(new BlockUpdateS2CPacket(world, cornerPos.add(x, y, z)));
+					}
+				}
+			}
+		}));
 	}
 }
