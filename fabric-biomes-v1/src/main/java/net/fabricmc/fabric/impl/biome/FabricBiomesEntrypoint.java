6de14ed75dc5fa8b550dd93dee01a9d31c0a5af1
CheaterCodes <cheater.codes@outlook.com>
2020-08-27 15:33:03 +0200

    rebased, adjusted entrypoint accordingly, and added some overloads to the builder

--- fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biome/FabricBiomesEntrypoint.java
+++ fabric-biomes-v1/src/main/java/net/fabricmc/fabric/impl/biome/FabricBiomesEntrypoint.java
@@ -2,6 +2,8 @@ package net.fabricmc.fabric.impl.biome;
 
 import com.mojang.serialization.Lifecycle;
 
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.DynamicRegistryManager;
 import net.minecraft.util.registry.MutableRegistry;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -10,26 +12,34 @@ import net.minecraft.world.biome.Biome;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biomes.v1.FabricBiomeBuilder;
 import net.fabricmc.fabric.api.biomes.v1.event.BiomeLoadingCallback;
-import net.fabricmc.fabric.api.event.registry.DynamicRegistryEntryAddedCallback;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 
-public class FabricBiomesEntrypoint implements ModInitializer, DynamicRegistryEntryAddedCallback<Biome> {
+public class FabricBiomesEntrypoint implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		DynamicRegistryEntryAddedCallback.event(Registry.BIOME_KEY).register(this);
+		DynamicRegistrySetupCallback.EVENT.register(registryManager -> {
+			RegistryEntryAddedCallback.event(registryManager.get(Registry.BIOME_KEY)).register((rawId, id, biome) -> {
+				onEntryAdded(rawId, id, biome, registryManager);
+			});
+		});
 	}
 
-	@Override
 	@SuppressWarnings("ConstantConditions")
-	public void onEntryAdded(int rawId, RegistryKey<Biome> key, Biome oldBiome, MutableRegistry<Biome> registry) {
+	public void onEntryAdded(int rawId, Identifier id, Biome oldBiome, DynamicRegistryManager registryManager) {
 		if (!((HasBeenProcessedProvider) (Object) oldBiome).hasBeenProcessed()) {
+			// Obtain registry and key
+			MutableRegistry<Biome> biomeRegistry = registryManager.get(Registry.BIOME_KEY);
+			RegistryKey<Biome> key = RegistryKey.of(Registry.BIOME_KEY, id);
+
 			// Create builder, pass to event and rebuild biome
-			FabricBiomeBuilder builder = FabricBiomeBuilder.of(oldBiome);
+			FabricBiomeBuilder builder = FabricBiomeBuilder.of(oldBiome, registryManager);
 			BiomeLoadingCallback.EVENT.invoker().onBiomeLoading(key, builder);
 			Biome newBiome = builder.build();
 
 			// Mark as processed to prevent re-triggering
 			((HasBeenProcessedProvider) (Object) newBiome).setProcessed();
-			registry.set(rawId, key, newBiome, Lifecycle.stable());
+			biomeRegistry.set(rawId, key, newBiome, Lifecycle.stable());
 		}
 	}
 }
