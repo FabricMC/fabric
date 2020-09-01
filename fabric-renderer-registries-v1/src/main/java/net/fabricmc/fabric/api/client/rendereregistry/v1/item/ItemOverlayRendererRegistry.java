1acbb44998057d113d37c293f7de136f0355d626
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-09-01 21:48:44 +0300

    NO nulls are allowed in my CHRISTIAN REGISTRY

--- fabric-renderer-registries-v1/src/main/java/net/fabricmc/fabric/api/client/rendereregistry/v1/item/ItemOverlayRendererRegistry.java
+++ fabric-renderer-registries-v1/src/main/java/net/fabricmc/fabric/api/client/rendereregistry/v1/item/ItemOverlayRendererRegistry.java
@@ -16,6 +16,8 @@
 
 package net.fabricmc.fabric.api.client.rendereregistry.v1.item;
 
+import java.util.Objects;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 
@@ -25,18 +27,24 @@ public final class ItemOverlayRendererRegistry {
 	private ItemOverlayRendererRegistry() { }
 
 	public static CountLabelProperties getCountLabelProperties(Item item) {
+		Objects.requireNonNull(item);
 		return ((ItemOverlayExtensions) item).fabric_getCountLabelProperties();
 	}
 
 	public static void setCountLabelProperties(Item item, CountLabelProperties properties) {
+		Objects.requireNonNull(item);
+		Objects.requireNonNull(properties);
 		((ItemOverlayExtensions) item).fabric_setCountLabelProperties(properties);
 	}
 
 	public static DurabilityBarProperties getDurabilityBarProperties(Item item) {
+		Objects.requireNonNull(item);
 		return ((ItemOverlayExtensions) item).fabric_getDurabilityBarProperties();
 	}
 
 	public static void setDurabilityBarProperties(Item item, DurabilityBarProperties properties) {
+		Objects.requireNonNull(item);
+		Objects.requireNonNull(properties);
 		((ItemOverlayExtensions) item).fabric_setDurabilityBarProperties(properties);
 	}
 
@@ -45,122 +53,80 @@ public final class ItemOverlayRendererRegistry {
 	}
 
 	public static void setCooldownOverlayProperties(Item item, CooldownOverlayProperties properties) {
+		Objects.requireNonNull(item);
+		Objects.requireNonNull(properties);
 		((ItemOverlayExtensions) item).fabric_setCooldownOverlayProperties(properties);
 	}
 
 	public static PreItemOverlayRenderer getPreRenderer(Item item) {
+		Objects.requireNonNull(item);
 		return ((ItemOverlayExtensions) item).fabric_getPreItemOverlayRenderer();
 	}
 
 	public static void setPreRenderer(Item item, PreItemOverlayRenderer renderer) {
+		Objects.requireNonNull(item);
+		Objects.requireNonNull(renderer);
 		((ItemOverlayExtensions) item).fabric_setPreOverlayRenderer(renderer);
 	}
 
 	public static PostItemOverlayRenderer getPostRenderer(Item item) {
+		Objects.requireNonNull(item);
 		return ((ItemOverlayExtensions) item).fabric_getPostItemOverlayRenderer();
 	}
 
 	public static void setPostRenderer(Item item, PostItemOverlayRenderer renderer) {
+		Objects.requireNonNull(item);
+		Objects.requireNonNull(renderer);
 		((ItemOverlayExtensions) item).fabric_setPostOverlayRenderer(renderer);
 	}
 
 	public static CountLabelProperties getCountLabelProperties(ItemConvertible itemConvertible) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return null;
-		}
-
-		return getCountLabelProperties(item);
+		Objects.requireNonNull(itemConvertible);
+		return getCountLabelProperties(itemConvertible.asItem());
 	}
 
 	public static void setCountLabelProperties(ItemConvertible itemConvertible, CountLabelProperties properties) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return;
-		}
-
-		setCountLabelProperties(item, properties);
+		Objects.requireNonNull(itemConvertible);
+		setCountLabelProperties(itemConvertible.asItem(), properties);
 	}
 
 	public static DurabilityBarProperties getDurabilityBarProperties(ItemConvertible itemConvertible) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return null;
-		}
-
-		return getDurabilityBarProperties(item);
+		Objects.requireNonNull(itemConvertible);
+		return getDurabilityBarProperties(itemConvertible.asItem());
 	}
 
 	public static void setDurabilityBarProperties(ItemConvertible itemConvertible, DurabilityBarProperties properties) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return;
-		}
-
-		setDurabilityBarProperties(item, properties);
+		Objects.requireNonNull(itemConvertible);
+		setDurabilityBarProperties(itemConvertible.asItem(), properties);
 	}
 
 	public static CooldownOverlayProperties getCooldownOverlayProperties(ItemConvertible itemConvertible) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return null;
-		}
-
-		return getCooldownOverlayProperties(item);
+		Objects.requireNonNull(itemConvertible);
+		return getCooldownOverlayProperties(itemConvertible.asItem());
 	}
 
 	public static void setCooldownOverlayProperties(ItemConvertible itemConvertible, CooldownOverlayProperties properties) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return;
-		}
-
-		setCooldownOverlayProperties(item, properties);
+		Objects.requireNonNull(itemConvertible);
+		setCooldownOverlayProperties(itemConvertible.asItem(), properties);
 	}
 
 	public static PreItemOverlayRenderer getPreRenderer(ItemConvertible itemConvertible) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return null;
-		}
-
-		return getPreRenderer(item);
+		Objects.requireNonNull(itemConvertible);
+		return getPreRenderer(itemConvertible.asItem());
 	}
 
 	public static void setPreRenderer(ItemConvertible itemConvertible, PreItemOverlayRenderer renderer) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return;
-		}
-
-		setPreRenderer(item, renderer);
+		Objects.requireNonNull(itemConvertible);
+		setPreRenderer(itemConvertible.asItem(), renderer);
 	}
 
 	public static PostItemOverlayRenderer getPostRenderer(ItemConvertible itemConvertible) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return null;
-		}
-
-		return getPostRenderer(item);
+		Objects.requireNonNull(itemConvertible);
+		return getPostRenderer(itemConvertible.asItem());
 	}
 
 	public static void setPostRenderer(ItemConvertible itemConvertible, PostItemOverlayRenderer renderer) {
-		Item item = itemConvertible.asItem();
-
-		if (item == null) {
-			return;
-		}
-
-		setPostRenderer(item, renderer);
+		Objects.requireNonNull(itemConvertible);
+		setPostRenderer(itemConvertible.asItem(), renderer);
 	}
 }
