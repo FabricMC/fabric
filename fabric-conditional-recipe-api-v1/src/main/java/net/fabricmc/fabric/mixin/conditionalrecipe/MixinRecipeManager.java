512bf45f9b7acd937fb8c26f0e5e17608dad515c
shedaniel <daniel@shedaniel.me>
2020-09-11 22:51:25 +0800

    @unique
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- fabric-conditional-recipe-api-v1/src/main/java/net/fabricmc/fabric/mixin/conditionalrecipe/MixinRecipeManager.java
+++ fabric-conditional-recipe-api-v1/src/main/java/net/fabricmc/fabric/mixin/conditionalrecipe/MixinRecipeManager.java
@@ -23,6 +23,7 @@ import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -45,6 +46,7 @@ public class MixinRecipeManager {
 		map.entrySet().removeIf(this::processConditions);
 	}
 
+	@Unique
 	private boolean processConditions(Map.Entry<Identifier, JsonElement> entry) {
 		JsonElement element = entry.getValue();
 
