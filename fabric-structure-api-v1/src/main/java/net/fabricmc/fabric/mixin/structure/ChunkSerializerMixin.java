dc690beaa9f8dd3252d868cbdbcb0c8327984b5a
i509VCB <git@i509.me>
2020-09-24 13:32:54 -0500

    Licenses...

--- fabric-structure-api-v1/src/main/java/net/fabricmc/fabric/mixin/structure/ChunkSerializerMixin.java
+++ fabric-structure-api-v1/src/main/java/net/fabricmc/fabric/mixin/structure/ChunkSerializerMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.structure;
 
 import java.util.Map;
@@ -22,6 +38,7 @@ import net.minecraft.world.gen.feature.StructureFeature;
 abstract class ChunkSerializerMixin {
 	@Unique
 	private static final ThreadLocal<Unit> CHUNK_NEEDS_SAVING = new ThreadLocal<>();
+
 	/**
 	 * Remove objects keyed by `null` in the map.
 	 * This data is likely bad since multiple missing structures will cause value mapped by `null` to change at least once.
