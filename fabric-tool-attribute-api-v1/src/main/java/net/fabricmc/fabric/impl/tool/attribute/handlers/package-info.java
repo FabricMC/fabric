6170c708156c6193e9bb753c369aeb346979a04c
shedaniel <daniel@shedaniel.me>
2020-10-07 00:37:00 +0800

    Remove line between the javadoc and the package line
    Signed-off-by: shedaniel <daniel@shedaniel.me>

--- fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/handlers/package-info.java
+++ fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/handlers/package-info.java
@@ -17,7 +17,6 @@
 /**
  * Implementation handlers of the fabric tool attribute module.
  */
-
 @ApiStatus.Internal
 package net.fabricmc.fabric.impl.tool.attribute.handlers;
 
--- fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/package-info.java
+++ fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/impl/tool/attribute/package-info.java
@@ -17,7 +17,6 @@
 /**
  * Implementation of the fabric tool attribute module.
  */
-
 @ApiStatus.Internal
 package net.fabricmc.fabric.impl.tool.attribute;
 
