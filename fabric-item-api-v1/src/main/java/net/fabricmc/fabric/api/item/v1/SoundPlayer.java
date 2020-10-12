7e2c7636b5f9f54158a762f27195119afbb0dd87
biom4st3r <biom4st3r@gmail.com>
2020-10-12 17:26:29 -0400

    * Fixed license errors

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/SoundPlayer.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/SoundPlayer.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package net.fabricmc.fabric.api.item.v1;
 
 import net.minecraft.entity.player.PlayerEntity;
