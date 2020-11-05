c097c2c675005cdd5019ec9352a4b225d0ccf6a8
i509VCB <git@i509.me>
2020-11-05 14:02:40 -0600

    Reimplement server entity load event (#1151)

--- fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/mixin/event/lifecycle/ServerWorldEntityLoaderMixin.java
+++ fabric-lifecycle-events-v1/src/main/java/net/fabricmc/fabric/mixin/event/lifecycle/ServerWorldEntityLoaderMixin.java
@@ -16,23 +16,29 @@
 
 package net.fabricmc.fabric.mixin.event.lifecycle;
 
-import java.util.function.BooleanSupplier;
-
-import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 
-@Mixin(ServerWorld.class)
-public abstract class ServerWorldMixin {
-	// Make sure "insideBlockTick" is true before we call the start tick, so inject after it is set
-	@Inject(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/server/world/ServerWorld;inBlockTick:Z", opcode = Opcodes.PUTFIELD, ordinal = 0, shift = At.Shift.AFTER))
-	private void startWorldTick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-		ServerTickEvents.START_WORLD_TICK.invoker().onStartTick((ServerWorld) (Object) this);
+@Mixin(targets = "net/minecraft/server/world/ServerWorld$class_5526")
+abstract class ServerWorldEntityLoaderMixin {
+	// final synthetic Lnet/minecraft/server/world/ServerWorld; field_26936
+	@SuppressWarnings("ShadowTarget")
+	@Shadow
+	@Final
+	private ServerWorld field_26936;
+
+	// onLoadEntity
+	@Inject(method = "method_31798(Lnet/minecraft/entity/Entity;)V", at = @At("TAIL"))
+	private void invokeEntityLoadEvent(Entity entity, CallbackInfo ci) {
+		ServerEntityEvents.ENTITY_LOAD.invoker().onLoad(entity, this.field_26936);
 	}
 }
