ec7e203b3834f9e500e08553032b676133a455ed
modmuss50 <modmuss50@gmail.com>
2020-11-13 19:07:01 +0000

    Revert "End Biomes Api [1.16] (#830)"
    This reverts commit ee897308

--- fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/impl/biome/WeightedBiomePicker.java
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/impl/biome/WeightedBiomePicker.java
@@ -30,7 +30,7 @@ import net.minecraft.world.biome.layer.util.LayerRandomnessSource;
  */
 public final class WeightedBiomePicker {
 	private double currentTotal;
-	private final List<WeightedBiomeEntry> entries;
+	private List<ContinentalBiomeEntry> entries;
 
 	WeightedBiomePicker() {
 		currentTotal = 0;
@@ -40,7 +40,7 @@ public final class WeightedBiomePicker {
 	void addBiome(final RegistryKey<Biome> biome, final double weight) {
 		currentTotal += weight;
 
-		entries.add(new WeightedBiomeEntry(biome, weight, currentTotal));
+		entries.add(new ContinentalBiomeEntry(biome, weight, currentTotal));
 	}
 
 	double getCurrentWeightTotal() {
@@ -53,19 +53,13 @@ public final class WeightedBiomePicker {
 		return search(target).getBiome();
 	}
 
-	public RegistryKey<Biome> pickFromNoise(LayerRandomnessSource source, double x, double y, double z) {
-		double target = Math.abs(source.getNoiseSampler().sample(x, y, z, 0.0, 0.0)) * getCurrentWeightTotal();
-
-		return search(target).getBiome();
-	}
-
 	/**
 	 * Searches with the specified target value.
 	 *
 	 * @param target The target value, must satisfy the constraint 0 <= target <= currentTotal
 	 * @return The result of the search
 	 */
-	WeightedBiomeEntry search(final double target) {
+	ContinentalBiomeEntry search(final double target) {
 		// Sanity checks, fail fast if stuff is going wrong.
 		Preconditions.checkArgument(target <= currentTotal, "The provided target value for biome selection must be less than or equal to the weight total");
 		Preconditions.checkArgument(target >= 0, "The provided target value for biome selection cannot be negative");
