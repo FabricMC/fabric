7271fb40478be9357602af1b389d867243d608b8
RedstoneParadox <spinjitzu00@gmail.com>
2020-11-15 11:21:24 -0800

    End Biomes Api [1.16] Take Two (#1164)
    * Create EndBiomes.java
    
    * Revert "Create EndBiomes.java"
    
    This reverts commit 4d1736fad99e1be0921ef92d9576e8f503e64be4.
    
    * Revert "Revert "Create EndBiomes.java""
    
    This reverts commit 673c508d825a0eb095f94ff9b5cb88d1b77e395c.
    
    * Rename ContinentalBiomeEntry to WeightedBiomeEntry
    
    * Complete API for adding biomes to the end
    
    * Mixin to end biome source
    
    * Revert "Rename ContinentalBiomeEntry to WeightedBiomeEntry"
    
    This reverts commit 849197e15d18c26af2a6f2aa1766b7d73d9a7144.
    
    * Revert "Revert "Rename ContinentalBiomeEntry to WeightedBiomeEntry""
    
    This reverts commit c2aa4ab097eb3daed71e82d4e77142671ed04316.
    
    * Create SimpleLayerRandomnessSource for use with end biome source mixin
    
    * Renamed MAIN region to MAIN_ISLANDS
    
    * Create test mod
    
    * Added pickFromNoise method to WeightedBiomePicker
    
    * Javadoc and licensing info
    
    * Comply with checkstyle
    
    * Internalize EndRegion
    
    * Added stretching when getting the biome for the end
    
    * Rename EndBiomes to TheEndBiomes to be consistent with yarn mappings
    
    * Fix typo
    
    Courtesy of i509VBC
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Add @Unique to private fields in MixinTheEndBiomeSource
    
    * Made end test biome consistent in style with previous test
    
    Courtesy of i509VBC
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    
    * Fix imports
    
    * Didn't know test mods needed to follow the checkstyle
    
    * SimpleLayerRandomnessSource's random is now use to instantiate its noise sampler
    
    * A very important note
    
    * Update javadoc for adding biomes to the Main Island region
    
    * Remove method that is leftover from when EndRegion was part of the API
    
    * Ported 1.16.1 biomes-api-v1 to 1.16.2.
    Public API changes:
    - Removed flagging biomes as suitable for player spawns since that is now handled in the Biome Builder.
    - Changed API over to RegistryKey's, not because it's necessary, but because it is more ergonomic since Vanilla Biomes in BuiltInBiomes are now all exposed as keys, rather than instances.
    
    * Increase memory size to fix build failures.
    
    * Add the ability to add modded dimensions to the regions of the end.
    
    * Hopefully fixed everything that went wrong during the rebase
    
    * Update fabric-biome-api-v1.mixins.json
    
    * Finally got custom end biomes to generate
    
    * Thanks for nothing, GitHub Desktop
    
    * Bad GitHub Desktop
    
    * Bad newline no cookie
    
    * Got everything to follow the checkstyle
    
    * Bring branch up-to-date with main repo (#11)
    
    * Revert "Bring branch up-to-date with main repo (#11)"
    
    This reverts commit dc471062410ec2de0899a7520584b16f5ddb88f1.
    
    * Remove some stuff that's not supposed to be there
    
    * Fix git silliness
    
    * Update FabricBiomeTest.java
    
    * TheEndBiomes is now Deprecated to match OverworldBiomes and NetherBiomes
    
    * Update some checkNotNull messages
    
    * Change the noise scale when replacing end biomes
    
    * Bad space no cookie
    
    * Remove unnecessary code
    
    * Remove unused imports
    
    * Set up InternalBiomeData to treat End Midlands and End Barrens as border biomes
    
    * Changed the API to reflect midlands and barrens biomes being considered border biomes
    
    * Start work on getting the new system fully working
    
    * Finally got everything working and cleaned up the Javadoc
    
    * Fixed checkstyle violations
    
    * Fix checkstyle violations again
    
    * Drop fabric_ prefix
    
    * Calling SimpleLayerRandomnessSource#nextInt() now throws an exception
    
    * If the midlands or barrens biome picker is null, the replacement key defaults to the vanilla one
    
    * Fix usage of vanilla identifier
    
    * Update MixinTheEndBiomeSource.java
    
    * Fix checkstyle violations
    
    Co-authored-by: i509VCB <i509vcb@gmail.com>
    Co-authored-by: Sebastian Hartte <sebastian@hartte.de>

--- fabric-biome-api-v1/src/testmod/java/net/fabricmc/fabric/test/biome/FabricBiomeTest.java
+++ fabric-biome-api-v1/src/testmod/java/net/fabricmc/fabric/test/biome/FabricBiomeTest.java
@@ -16,14 +16,28 @@
 
 package net.fabricmc.fabric.test.biome;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.sound.BiomeMoodSound;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeEffects;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.DefaultBiomeCreator;
+import net.minecraft.world.biome.GenerationSettings;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.ConfiguredFeatures;
+import net.minecraft.world.gen.feature.ConfiguredStructureFeatures;
+import net.minecraft.world.gen.feature.DefaultBiomeFeatures;
 import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilders;
+import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.SurfaceConfig;
+import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
@@ -32,6 +46,7 @@ import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
 import net.fabricmc.fabric.api.biome.v1.NetherBiomes;
 import net.fabricmc.fabric.api.biome.v1.OverworldBiomes;
 import net.fabricmc.fabric.api.biome.v1.OverworldClimate;
+import net.fabricmc.fabric.api.biome.v1.TheEndBiomes;
 
 /**
  * <b>NOTES FOR TESTING:</b>
@@ -47,8 +62,13 @@ public class FabricBiomeTest implements ModInitializer {
 	public static final String MOD_ID = "fabric-biome-api-v1-testmod";
 
 	private static final RegistryKey<Biome> TEST_CRIMSON_FOREST = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "test_crimson_forest"));
-
 	private static final RegistryKey<Biome> CUSTOM_PLAINS = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "custom_plains"));
+	private static final RegistryKey<Biome> TEST_END_HIGHLANDS = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "test_end_highlands"));
+	private static final RegistryKey<Biome> TEST_END_MIDLANDS = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "test_end_midlands"));
+	private static final RegistryKey<Biome> TEST_END_BARRRENS = RegistryKey.of(Registry.BIOME_KEY, new Identifier(MOD_ID, "test_end_barrens"));
+
+	private static BlockState STONE = Blocks.STONE.getDefaultState();
+	private static ConfiguredSurfaceBuilder<TernarySurfaceConfig> TEST_END_SURFACE_BUILDER = registerTestSurfaceBuilder(new Identifier(MOD_ID, "end"), SurfaceBuilder.DEFAULT.withConfig(new TernarySurfaceConfig(STONE, STONE, STONE)));
 
 	@Override
 	public void onInitialize() {
@@ -59,6 +79,15 @@ public class FabricBiomeTest implements ModInitializer {
 
 		Registry.register(BuiltinRegistries.BIOME, CUSTOM_PLAINS.getValue(), DefaultBiomeCreator.createPlains(false));
 		OverworldBiomes.addBiomeVariant(BiomeKeys.PLAINS, CUSTOM_PLAINS, 1);
+
+		Registry.register(BuiltinRegistries.BIOME, TEST_END_HIGHLANDS.getValue(), createEndHighlands());
+		Registry.register(BuiltinRegistries.BIOME, TEST_END_MIDLANDS.getValue(), createEndMidlands());
+		Registry.register(BuiltinRegistries.BIOME, TEST_END_BARRRENS.getValue(), createEndBarrens());
+		// TESTING HINT: to get to the end:
+		// /execute in minecraft:the_end run tp @s 0 90 0
+		TheEndBiomes.addHighlandsBiome(TEST_END_HIGHLANDS, 5.0);
+		TheEndBiomes.addMidlandsBiome(TEST_END_HIGHLANDS, TEST_END_MIDLANDS, 1.0);
+		TheEndBiomes.addBarrensBiome(TEST_END_HIGHLANDS, TEST_END_BARRRENS, 1.0);
 
 		OverworldBiomes.addEdgeBiome(BiomeKeys.PLAINS, BiomeKeys.END_BARRENS, 0.9);
 
@@ -79,5 +108,31 @@ public class FabricBiomeTest implements ModInitializer {
 						context -> {
 							context.getGenerationSettings().setBuiltInSurfaceBuilder(ConfiguredSurfaceBuilders.CRIMSON_FOREST);
 						});
+	}
+
+	// These are used for testing the spacing of custom end biomes.
+	private static Biome createEndHighlands() {
+		GenerationSettings.Builder builder = (new GenerationSettings.Builder()).surfaceBuilder(TEST_END_SURFACE_BUILDER).structureFeature(ConfiguredStructureFeatures.END_CITY).feature(GenerationStep.Feature.SURFACE_STRUCTURES, ConfiguredFeatures.END_GATEWAY).feature(GenerationStep.Feature.VEGETAL_DECORATION, ConfiguredFeatures.CHORUS_PLANT);
+		return composeEndSpawnSettings(builder);
+	}
+
+	public static Biome createEndMidlands() {
+		GenerationSettings.Builder builder = (new GenerationSettings.Builder()).surfaceBuilder(TEST_END_SURFACE_BUILDER).structureFeature(ConfiguredStructureFeatures.END_CITY);
+		return composeEndSpawnSettings(builder);
+	}
+
+	public static Biome createEndBarrens() {
+		GenerationSettings.Builder builder = (new GenerationSettings.Builder()).surfaceBuilder(TEST_END_SURFACE_BUILDER);
+		return composeEndSpawnSettings(builder);
+	}
+
+	private static Biome composeEndSpawnSettings(GenerationSettings.Builder builder) {
+		SpawnSettings.Builder builder2 = new SpawnSettings.Builder();
+		DefaultBiomeFeatures.addEndMobs(builder2);
+		return (new Biome.Builder()).precipitation(Biome.Precipitation.NONE).category(Biome.Category.THEEND).depth(0.1F).scale(0.2F).temperature(0.5F).downfall(0.5F).effects((new BiomeEffects.Builder()).waterColor(4159204).waterFogColor(329011).fogColor(10518688).skyColor(0).moodSound(BiomeMoodSound.CAVE).build()).spawnSettings(builder2.build()).generationSettings(builder.build()).build();
+	}
+
+	private static <SC extends SurfaceConfig> ConfiguredSurfaceBuilder<SC> registerTestSurfaceBuilder(Identifier id, ConfiguredSurfaceBuilder<SC> configuredSurfaceBuilder) {
+		return BuiltinRegistries.add(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, id, configuredSurfaceBuilder);
 	}
 }
