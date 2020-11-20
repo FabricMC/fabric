4188f856f4f474d22a5b28b72ee604101640f8fb
Waterpicker <Waterpickerenternity@gmail.com>
2020-11-19 21:15:41 -0600

    Move Nullable import.

--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/api/dimension/v1/FabricDimensions.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/api/dimension/v1/FabricDimensions.java
@@ -17,13 +17,14 @@
 package net.fabricmc.fabric.api.dimension.v1;
 
 import com.google.common.base.Preconditions;
+import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.TeleportTarget;
 
 import net.fabricmc.fabric.impl.dimension.FabricDimensionInternals;
-import org.jetbrains.annotations.Nullable;
+
 
 /**
  * This class consists exclusively of static methods that operate on world dimensions.
