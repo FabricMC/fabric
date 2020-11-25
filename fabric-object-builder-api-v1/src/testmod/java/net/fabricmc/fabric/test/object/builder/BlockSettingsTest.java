2deb6581040c07acf000aa798091918d3b8b8baa
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-25 17:13:15 +0530

    Testmod checkstyle

--- fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/BlockSettingsTest.java
+++ fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/BlockSettingsTest.java
@@ -23,12 +23,10 @@ import net.minecraft.util.registry.Registry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
-import static net.fabricmc.fabric.test.object.builder.ObjectBuilderTestConstants.id;
-
 public class BlockSettingsTest implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This block should not cause a crash on a dedicated server
-		Registry.register(Registry.BLOCK, id("cutout_block"), new Block(FabricBlockSettings.of(Material.STONE).nonOpaque().cutout()));
+		Registry.register(Registry.BLOCK, ObjectBuilderTestConstants.id("cutout_block"), new Block(FabricBlockSettings.of(Material.STONE).nonOpaque().cutout()));
 	}
 }
