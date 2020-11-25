2f197687761c06ff1ce13d5457db83e296dac28a
SD <harrypotter.starwars.shrish@gmail.com>
2020-11-25 15:30:13 +0530

    Test mod

---
+++ fabric-object-builder-api-v1/src/testmod/java/net/fabricmc/fabric/test/object/builder/BlockSettingsTest.java
@@ -0,0 +1,18 @@
+package net.fabricmc.fabric.test.object.builder;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.util.registry.Registry;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+
+import static net.fabricmc.fabric.test.object.builder.ObjectBuilderTestConstants.id;
+
+public class BlockSettingsTest implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		// This block should not cause a crash on a dedicated server
+		Registry.register(Registry.BLOCK, id("cutout_block"), new Block(FabricBlockSettings.of(Material.STONE).cutout()));
+	}
+}
