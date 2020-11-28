fddcc0d88fd8133e3af54f6c0ebe7dc9e70a00ac
Technici4n <bruno.ploumhans@gmail.com>
2020-11-28 20:46:56 +0100

    Cache the fuel time map in the fuel registry (#1108)
    * Cache the fuel time map in the fuel registry
    
    * Use a faster map for the fuel times
    
    * Rebuild the fuel times map on the client side too
    
    * Use more compatible mixins
    
    * Replace a function overwrite by a TAIL inject
    
    * Mixin formatting
    
    * Indentation
    
    * Make cache thread-safe, switch to boxed collection (IHM) since most
    uses yield boxed results, refactor cache clearing
    
    * fix import formatting
    
    Co-authored-by: Player <player@player.to>

---
+++ fabric-content-registries-v0/src/main/java/net/fabricmc/fabric/mixin/content/registry/client/MixinClientPlayNetworkHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.content.registry.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.s2c.play.SynchronizeTagsS2CPacket;
+
+import net.fabricmc.fabric.impl.content.registry.FuelRegistryImpl;
+
+@Mixin(ClientPlayNetworkHandler.class)
+public abstract class MixinClientPlayNetworkHandler {
+	@Inject(at = @At("TAIL"), method = "onSynchronizeTags")
+	private void onSynchronizeTagsHook(SynchronizeTagsS2CPacket packet, CallbackInfo info) {
+		FuelRegistryImpl.INSTANCE.resetCache();
+	}
+}
