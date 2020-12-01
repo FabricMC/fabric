bc15a32ba98e5122b0a24bc0085905367cdc8ea4
AqH47i <75282944+AqH47i@users.noreply.github.com>
2020-12-01 11:52:02 +0800

    Update ClientCommandManager.java

--- fabric-command-api-v1/src/main/java/net/fabricmc/fabric/api/command/v1/ClientCommandManager.java
+++ fabric-command-api-v1/src/main/java/net/fabricmc/fabric/api/command/v1/ClientCommandManager.java
@@ -22,33 +22,35 @@ import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import net.fabricmc.fabric.impl.command.v1.ClientCommandManagerImpl;
+
 import net.minecraft.command.CommandSource;
 
+import net.fabricmc.fabric.impl.command.v1.ClientCommandManagerImpl;
+
 public interface ClientCommandManager {
-    ClientCommandManager INSTANCE = new ClientCommandManagerImpl();
+	ClientCommandManager INSTANCE = new ClientCommandManagerImpl();
 
-    static LiteralArgumentBuilder<CommandSource> literal(String name) {
-        return LiteralArgumentBuilder.literal(name);
-    }
+	static LiteralArgumentBuilder<CommandSource> literal(String name) {
+		return LiteralArgumentBuilder.literal(name);
+	}
 
-    static <T> RequiredArgumentBuilder<CommandSource, T> argument(String name, ArgumentType<T> argumentType) {
-        return RequiredArgumentBuilder.argument(name, argumentType);
-    }
+	static <T> RequiredArgumentBuilder<CommandSource, T> argument(String name, ArgumentType<T> argumentType) {
+		return RequiredArgumentBuilder.argument(name, argumentType);
+	}
 
-    CommandDispatcher<CommandSource> getDispatcher();
+	CommandDispatcher<CommandSource> getDispatcher();
 
-    default <T> int execute(CommandDispatcher<T> dispatcher, String command, T source) {
-        return execute(dispatcher, new StringReader(command), source);
-    }
+	default <T> int execute(CommandDispatcher<T> dispatcher, String command, T source) {
+		return execute(dispatcher, new StringReader(command), source);
+	}
 
-    default <T> int execute(CommandDispatcher<T> dispatcher, StringReader command, T source) {
-        if (command.canRead(1) && command.peek() == '/') {
-            command.skip();
-        }
+	default <T> int execute(CommandDispatcher<T> dispatcher, StringReader command, T source) {
+		if (command.canRead(1) && command.peek() == '/') {
+			command.skip();
+		}
 
-        return execute(dispatcher, dispatcher.parse(command, source));
-    }
+		return execute(dispatcher, dispatcher.parse(command, source));
+	}
 
-    <T> int execute(CommandDispatcher<T> dispatcher, ParseResults<T> parseResults);
+	<T> int execute(CommandDispatcher<T> dispatcher, ParseResults<T> parseResults);
 }
