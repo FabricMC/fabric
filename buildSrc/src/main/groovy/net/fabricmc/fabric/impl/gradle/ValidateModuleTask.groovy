e76fcfde794886e7b6c359ec6995f2ad8fe87c6c
i509VCB <git@i509.me>
2021-01-01 20:42:55 -0600

    Apply task to each project and dont cross projects

--- buildSrc/src/main/groovy/net/fabricmc/fabric/impl/gradle/ValidateModuleTask.groovy
+++ buildSrc/src/main/groovy/net/fabricmc/fabric/impl/gradle/ValidateModuleTask.groovy
@@ -8,26 +8,24 @@ import org.gradle.api.tasks.TaskAction
 class ValidateModuleTask extends DefaultTask {
     @TaskAction
     void validate() {
-        project.subprojects { subProject ->
-            def json = new JsonSlurper().parse(subProject.file("src/main/resources/fabric.mod.json") as File) as Map<String, Map<String, String>>;
+        def json = new JsonSlurper().parse(project.file("src/main/resources/fabric.mod.json") as File) as Map<String, Map<String, String>>;
 
-            if (json.custom == null) {
-                throw new GradleException("Subproject ${subProject} does not have a custom value containing module lifecycle!")
-            }
+        if (json.custom == null) {
+            throw new GradleException("Module ${project} does not have a custom value containing module lifecycle!")
+        }
 
-            if (json.custom.get("fabric-api:module-lifecycle") == null) {
-                throw new GradleException("Subproject ${subProject} does not have module lifecycle in custom values!")
-            }
+        if (json.custom.get("fabric-api:module-lifecycle") == null) {
+            throw new GradleException("Module ${project} does not have module lifecycle in custom values!")
+        }
 
-            // Validate the lifecycle value
-            switch (json.custom.get("fabric-api:module-lifecycle")) {
-                case "stable":
-                case "experimental":
-                case "deprecated":
-                    break;
-                default:
-                    throw new GradleException("Subproject ${subProject} has an invalid module lifecycle ${json.custom.get('fabric-api:module-lifecycle')}");
-            }
+        // Validate the lifecycle value
+        switch (json.custom.get("fabric-api:module-lifecycle")) {
+            case "stable":
+            case "experimental":
+            case "deprecated":
+                break;
+            default:
+                throw new GradleException("Module ${project} has an invalid module lifecycle ${json.custom.get('fabric-api:module-lifecycle')}");
         }
     }
 }
