85570837cd5ee74e7b3c77e0664281e66a3223ae
IMS <31803019+IMS212@users.noreply.github.com>
2021-01-11 20:13:46 -0800

    Added different method of resource bundles, and headless mode works thanks to @AlexIIL
    Signed-off-by: IMS <31803019+IMS212@users.noreply.github.com>

--- src/main/java/net/fabricmc/fabric/impl/launch/APIWarning.java
+++ src/main/java/net/fabricmc/fabric/impl/launch/APIWarning.java
@@ -17,7 +17,12 @@
 package net.fabricmc.fabric.impl.launch;
 
 import java.awt.GraphicsEnvironment;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 
 import javax.swing.JOptionPane;
@@ -26,8 +31,27 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public final class APIWarning {
 	public static void main(String[] args) {
-		Locale defaultLocale = Locale.getDefault();
-		String message = ResourceBundle.getBundle("lang/WarningAPI", defaultLocale).getString("api.warning");
+		final ResourceBundle WarningAPI = ResourceBundle.getBundle("lang/WarningAPI", Locale.getDefault(), new ResourceBundle.Control() {
+			@Override
+			public ResourceBundle newBundle(String baseName, Locale locale, String format, ClassLoader loader, boolean reload) throws IllegalAccessException, InstantiationException, IOException {
+				final String bundleName = toBundleName(baseName, locale);
+				final String resourceName = toResourceName(bundleName, "properties");
+
+				try (InputStream stream = loader.getResourceAsStream(resourceName)) {
+					if (stream != null) {
+						try (InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8)) {
+							return new PropertyResourceBundle(reader);
+						}
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+					return super.newBundle(baseName, locale, format, loader, reload);
+			}
+		});
+		String message = WarningAPI.getString("api.warning");
+
 		if (GraphicsEnvironment.isHeadless()) {
 			System.err.println(message);
 		} else {
@@ -36,6 +60,7 @@ public final class APIWarning {
 			} catch (ReflectiveOperationException | UnsupportedLookAndFeelException ignored) {
 				// Ignored
 			}
+
 			JOptionPane.showMessageDialog(null, message);
 		}
 	}
