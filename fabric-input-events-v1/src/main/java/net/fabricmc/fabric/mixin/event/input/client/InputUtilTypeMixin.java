b1628e0492182920919be49c3da6785f05e6e8ac
yyny <6576327+yyny@users.noreply.github.com>
2021-01-18 19:59:51 +0100

    Clean InputUtilTypeMixin

--- fabric-input-events-v1/src/main/java/net/fabricmc/fabric/mixin/event/input/client/InputUtilTypeMixin.java
+++ fabric-input-events-v1/src/main/java/net/fabricmc/fabric/mixin/event/input/client/InputUtilTypeMixin.java
@@ -18,13 +18,13 @@ package net.fabricmc.fabric.mixin.event.input.client;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
-
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.InputUtil.Key;
 
 @Mixin(InputUtil.Type.class)
 public interface InputUtilTypeMixin {
 	@Accessor("map")
-	public Int2ObjectMap<Key> getMap();
+	Int2ObjectMap<Key> getMap();
 }
