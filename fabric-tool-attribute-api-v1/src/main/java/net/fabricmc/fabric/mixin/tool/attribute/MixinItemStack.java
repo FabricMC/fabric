0f378db5d652fe406a69d8ef694709ef6a4b160e
Draylar <samuelhimes@gmail.com>
2021-04-22 17:15:40 -0500

    remove unused shadow of getItem()

--- fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/mixin/tool/attribute/MixinItemStack.java
+++ fabric-tool-attribute-api-v1/src/main/java/net/fabricmc/fabric/mixin/tool/attribute/MixinItemStack.java
@@ -22,7 +22,6 @@ import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Multimap;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -36,21 +35,17 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.tool.attribute.v1.DynamicAttributeTool;
-import net.fabricmc.fabric.impl.tool.attribute.ItemStackContext;
 import net.fabricmc.fabric.api.tool.attribute.v1.ToolManager;
+import net.fabricmc.fabric.impl.tool.attribute.ItemStackContext;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack implements ItemStackContext {
-	@Shadow
-	public abstract Item getItem();
-
 	@Unique
 	@Nullable
 	private LivingEntity contextEntity = null;
