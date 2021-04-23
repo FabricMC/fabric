6f7fdcd0c7e7604684fbc6cd140b248810271900
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-04-23 23:46:16 +0200

    Remove wildcard import

--- fabric-api-base/src/main/java/net/fabricmc/fabric/api/event/AutoInvokingEvent.java
+++ fabric-api-base/src/main/java/net/fabricmc/fabric/api/event/AutoInvokingEvent.java
@@ -1,6 +1,10 @@
 package net.fabricmc.fabric.api.event;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Indicates that this {@link Event} is auto-invoking:
