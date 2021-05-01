4b24d382c11d79452c109349c7f0674c498ebc50
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-05-01 23:06:27 +0200

    Change event behavior for one listener, introduce `AutoInvokingEvent`, update `ArrayBackedEvent` implementation (#1369)
    * Cleanup Events
    
    * Add the AutoInvokingEvent annotation.
    
    Co-authored-by: Player <sfPlayer1@users.noreply.github.com>
    Co-authored-by: Technici4n <13494793+Technici4n@users.noreply.github.com>

--- fabric-api-base/src/main/java/net/fabricmc/fabric/impl/base/event/ArrayBackedEvent.java
+++ fabric-api-base/src/main/java/net/fabricmc/fabric/impl/base/event/ArrayBackedEvent.java
@@ -18,6 +18,7 @@ package net.fabricmc.fabric.impl.base.event;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Function;
@@ -25,52 +26,30 @@ import java.util.function.Function;
 import net.fabricmc.fabric.api.event.Event;
 
 class ArrayBackedEvent<T> extends Event<T> {
-	private final Class<? super T> type;
 	private final Function<T[], T> invokerFactory;
-	private final T dummyInvoker;
 	private final Lock lock = new ReentrantLock();
 	private T[] handlers;
 
-	ArrayBackedEvent(Class<? super T> type, T dummyInvoker, Function<T[], T> invokerFactory) {
-		this.type = type;
-		this.dummyInvoker = dummyInvoker;
+	@SuppressWarnings("unchecked")
+	ArrayBackedEvent(Class<? super T> type, Function<T[], T> invokerFactory) {
 		this.invokerFactory = invokerFactory;
+		this.handlers = (T[]) Array.newInstance(type, 0);
 		update();
 	}
 
-	@SuppressWarnings("unchecked")
 	void update() {
-		if (handlers == null) {
-			if (dummyInvoker != null) {
-				invoker = dummyInvoker;
-			} else {
-				invoker = invokerFactory.apply((T[]) Array.newInstance(type, 0));
-			}
-		} else if (handlers.length == 1) {
-			invoker = handlers[0];
-		} else {
-			invoker = invokerFactory.apply(handlers);
-		}
+		this.invoker = invokerFactory.apply(handlers);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public void register(T listener) {
-		if (listener == null) {
-			throw new NullPointerException("Tried to register a null listener!");
-		}
+		Objects.requireNonNull(listener, "Tried to register a null listener!");
 
 		lock.lock();
 
 		try {
-			if (handlers == null) {
-				handlers = (T[]) Array.newInstance(type, 1);
-				handlers[0] = listener;
-			} else {
-				handlers = Arrays.copyOf(handlers, handlers.length + 1);
-				handlers[handlers.length - 1] = listener;
-			}
-
+			handlers = Arrays.copyOf(handlers, handlers.length + 1);
+			handlers[handlers.length - 1] = listener;
 			update();
 		} finally {
 			lock.unlock();
