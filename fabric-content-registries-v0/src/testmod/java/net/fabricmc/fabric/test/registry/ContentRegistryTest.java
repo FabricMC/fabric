7d48215e5005f1b9cbe4d12036761bbda69e443f
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-06-12 11:35:17 -0400

    Create a test mod for the fabric-content-registries-v0 module

---
+++ fabric-content-registries-v0/src/testmod/java/net/fabricmc/fabric/test/registry/ContentRegistryTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.registry;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Items;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+
+public final class ContentRegistryTest implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		CompostingChanceRegistry.INSTANCE.add(Items.OBSIDIAN, 0.5F);
+		FlammableBlockRegistry.getDefaultInstance().add(Blocks.OBSIDIAN, 2, 3);
+		FuelRegistry.INSTANCE.add(Items.OBSIDIAN, 20 * 3);
+	}
+}
