643acdec3c1940b7437f9ea741943f151efebc90
deirn <foreirn@gmail.com>
2021-07-12 10:03:17 +0700

    Document mixin Return ImmutableSet instead

--- fabric-tag-extensions-v0/src/main/java/net/fabricmc/fabric/mixin/tag/extension/MixinRequiredTagListRegistry.java
+++ fabric-tag-extensions-v0/src/main/java/net/fabricmc/fabric/mixin/tag/extension/MixinRequiredTagListRegistry.java
@@ -17,11 +17,11 @@
 package net.fabricmc.fabric.mixin.tag.extension;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -33,6 +33,11 @@ import net.minecraft.tag.RequiredTagListRegistry;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 
+/**
+ * This mixin adds support for custom {@link RequiredTagList} registration via {@link RequiredTagListRegistry#register(RegistryKey, String)}.
+ * <p>
+ * Call {@link RequiredTagList#add(String)} to create a new tag.
+ */
 @Mixin(RequiredTagListRegistry.class)
 public class MixinRequiredTagListRegistry {
 	@Unique
@@ -40,20 +45,25 @@ public class MixinRequiredTagListRegistry {
 
 	@Inject(method = "register", at = @At("HEAD"), cancellable = true)
 	private static void beforeRegister(RegistryKey<? extends Registry<?>> registryKey, String dataType, CallbackInfoReturnable<RequiredTagList<?>> cir) {
+		// Return already registered tag list for the registry if it has the same dataType, in case multiple mods tried to do it.
 		if (TAG_LISTS.containsKey(registryKey)) {
 			RequiredTagList<?> tagList = TAG_LISTS.get(registryKey);
-			Preconditions.checkArgument(tagList.getDataType().equals(dataType), "Tag list for registry %s is already exist with data type %s", registryKey.getValue(), tagList.getDataType());
+			// Throw an exception if the tagList has different dataType.
+			Preconditions.checkArgument(tagList.getDataType().equals(dataType), "Tag list for registry %s is already existed with data type %s", registryKey.getValue(), tagList.getDataType());
 			cir.setReturnValue(tagList);
 		}
 	}
 
 	@Inject(method = "register", at = @At("TAIL"))
 	private static void afterRegister(RegistryKey<? extends Registry<?>> registryKey, String dataType, CallbackInfoReturnable<RequiredTagList<?>> cir) {
+		// Put newly created tag list to the map.
 		TAG_LISTS.put(registryKey, cir.getReturnValue());
 	}
 
 	@Inject(method = "getBuiltinTags", at = @At("TAIL"), cancellable = true)
 	private static void getBuiltinTags(CallbackInfoReturnable<Set<RequiredTagList<?>>> cir) {
-		cir.setReturnValue(new HashSet<>(TAG_LISTS.values()));
+		// Return all registered tag lists instead of hardcoded vanilla ones.
+		// Needs @At("TAIL") because vanilla tag lists are first initialized here.
+		cir.setReturnValue(ImmutableSet.copyOf(TAG_LISTS.values()));
 	}
 }
