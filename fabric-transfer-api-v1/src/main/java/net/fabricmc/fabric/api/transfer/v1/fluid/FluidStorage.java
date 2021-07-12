c09be4c48ab924842a65b1c72ae0cc59b97d94ba
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-07-12 19:28:33 +0200

    Fabric Transfer API: "fluid only" edition (#1356)
    * Original fluid API design
    
    * Rework the transaction system
    
    * First javadoc pass
    
    * Add a testmod, a base implementation for fluid storages and fix some little bugs
    
    * Fix checkstyle
    
    * Make Movement#move extract from the view and not the whole Storage
    
    * Document and update FluidPreconditions
    
    * Use for-each in CombinedStorage and document a little
    
    * Remove useless overrides in Insertion/ExtractionOnlyStorage
    
    * Move SnapshotParticipant#snapshots to the top of the class, and make updateSnapshots public
    
    * Fix garbage collection of unused CauldronWrappers
    
    * Use ArrayList directly
    
    * Remove locking, reorganize transaction implementation, and add outer close callback
    
    * Add more javadoc
    
    * Rework Storage#forEach into Storage#iterator
    
    * Add a few missing `transaction.addCloseCallback(iterator)`
    
    * Add anyView(), exactView(), capacity() and isEmpty()
    
    * Add Storage#iterable to make iteration friendlier to for loops
    
    * Storages may now have multiple open iterators
    
    Co-authored-by: Devan-Kerman <dev.sel20@gmail.com>
    
    * Make CombinedStorage#supportsInsertion/Extraction iterate through the parts
    
    * Block updates should be used when the supportsInsertion/Extraction status changes
    
    * Fluid -> FluidKey
    
    * Remove all references to ItemKey inside FluidKey, and other minor tweaks
    
    * Cache FluidKeys with a null tag inside Fluid directly
    
    * Fluid unit convention
    
    * Add FluidKeyRendering and RenderHandler
    
    * Bump version for more testing (also published to my maven)
    
    * Add SingleViewIterator, massively reduce code duplication!
    
    * Make API experimental, and add README
    
    * Bump version
    
    * Apparently Fluids.EMPTY is flowing
    
    * Add package info
    
    * Minor adjustements
    
    * 1.17 port, cauldron support, add ResourceKey
    
    * Checkstyle, gas rendering, use record for ResourceAmount
    
    * Add a few helpers, rename some stuff
    
    * Remove anyView, allow nullable in StorageUtil#find*, fix missing try block
    
    * Slight findStoredResource cleanup
    
    * Slightly improve implementation
    
    * Bump version
    
    * Fix wrong transaction
    
    * I wrote in a comment that this could happen...
    
    * Fix SingleFluidStorage bugs, add tests in the testmod, add testmod assets
    
    * Add extract stick
    
    * Rename a few things
    
    * `ResourceKey<T>` -> `TransferKey<O>`
    * `ResourceKey#getResource()` -> `TransferKey#getObject()` as resource is already widely used through the API for the keys themselves.
    * `tag` -> `nbt`
    * Add `get` prefixes to `StorageView` functions
    
    * Bump version
    
    * FluidKey -> FluidVariant
    
    * Bump version
    
    * Expand getVersion() documentation, make it thread-safe and use long.
    
    Co-authored-by: Player <player@player.to>
    
    * empty resource -> blank resource, and update SingleFluidStorage
    
    Co-authored-by: Player <player@player.to>
    
    * Make CauldronFluidContent a final class instead of a record.
    
    Co-authored-by: Player <player@player.to>
    
    * Get rid of CauldronFluidContent#minLevel (was always 1)
    
    * Fix nested commits. (Thanks @warjort!)
    
    * Separate Transaction and TransactionContext
    
    Co-authored-by: Devan-Kerman <dev.sel20@gmail.com>
    Co-authored-by: Player <player@player.to>
    
    * Change WorldLocation into a private record
    
    * Bump version
    
    * Guard against exceptions thrown in close callbacks
    
    * Make sure blank fluid variants don't have a tag
    
    * Add documentation, make CauldronStorage clearer
    
    Co-authored-by: frqnny <45723631+frqnny@users.noreply.github.com>
    
    * Allow null storages in StorageUtil#move, and clarify sidedness of FluidStorage
    
    * Add explicit hashCode and equals for transfer variants
    
    * Remove ugly equals and hashCode overrides, and add constant time hashcode spec
    
    Co-authored-by: Devan-Kerman <dev.sel20@gmail.com>
    Co-authored-by: liach <liach@users.noreply.github.com>
    Co-authored-by: Player <player@player.to>
    Co-authored-by: frqnny <45723631+frqnny@users.noreply.github.com>

---
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/api/transfer/v1/fluid/FluidStorage.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.transfer.v1.fluid;
+
+import org.jetbrains.annotations.ApiStatus;
+
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
+
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
+
+/**
+ * Access to {@link Storage Storage&lt;FluidVariant&gt;} instances.
+ *
+ * @deprecated Experimental feature, we reserve the right to remove or change it without further notice.
+ * The transfer API is a complex addition, and we want to be able to correct possible design mistakes.
+ */
+@ApiStatus.Experimental
+@Deprecated
+public final class FluidStorage {
+	/**
+	 * Sided block access to fluid variant storages.
+	 * Fluid amounts are always expressed in {@linkplain FluidConstants droplets}.
+	 * The {@code Direction} parameter may never be null.
+	 * Refer to {@link BlockApiLookup} for documentation on how to use this field.
+	 *
+	 * <p>When the operations supported by a storage change,
+	 * that is if the return value of {@link Storage#supportsInsertion} or {@link Storage#supportsExtraction} changes,
+	 * the storage should notify its neighbors with a block update so that they can refresh their connections if necessary.
+	 *
+	 * <p>May only be queried on the logical server thread, never client-side or from another thread!
+	 */
+	public static final BlockApiLookup<Storage<FluidVariant>, Direction> SIDED =
+			BlockApiLookup.get(new Identifier("fabric:sided_fluid_storage"), Storage.asClass(), Direction.class);
+
+	private FluidStorage() {
+	}
+
+	static {
+		// Initialize vanilla cauldron wrappers
+		CauldronFluidContent.getForFluid(Fluids.WATER);
+	}
+}
