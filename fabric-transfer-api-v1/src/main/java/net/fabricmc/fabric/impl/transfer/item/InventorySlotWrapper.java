f16285f5d9fbb841b7bd5d902a913fbdb954ccc1
Technici4n <13494793+technici4n@users.noreply.github.com>
2021-09-09 19:47:06 +0200

    Work around vanilla capturing ItemStack references (#1700)

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/InventorySlotWrapper.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/InventorySlotWrapper.java
@@ -34,6 +34,7 @@ class InventorySlotWrapper extends SingleStackStorage {
 	 */
 	private final InventoryStorageImpl storage;
 	final int slot;
+	private ItemStack lastReleasedSnapshot = null;
 
 	InventorySlotWrapper(InventoryStorageImpl storage, int slot) {
 		this.storage = storage;
@@ -65,5 +66,26 @@ class InventorySlotWrapper extends SingleStackStorage {
 	public void updateSnapshots(TransactionContext transaction) {
 		storage.markDirtyParticipant.updateSnapshots(transaction);
 		super.updateSnapshots(transaction);
+	}
+
+	@Override
+	protected void releaseSnapshot(ItemStack snapshot) {
+		lastReleasedSnapshot = snapshot;
+	}
+
+	@Override
+	protected void onFinalCommit() {
+		// Try to apply the change to the original stack
+		ItemStack original = lastReleasedSnapshot;
+		ItemStack currentStack = getStack();
+
+		if (!original.isEmpty() && !currentStack.isEmpty() && (original.getItem() == currentStack.getItem() && ItemStack.areTagsEqual(original, currentStack))) {
+			// None is empty and the contents match: just update the amount and reuse the original stack.
+			original.setCount(currentStack.getCount());
+			setStack(original);
+		} else {
+			// Otherwise assume everything was taken from original so empty it.
+			original.setCount(0);
+		}
 	}
 }
