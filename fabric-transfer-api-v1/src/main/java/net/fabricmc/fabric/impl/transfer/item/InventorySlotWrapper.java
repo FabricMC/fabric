6d063b955405063a52c1fed99c767f0a77992834
Technici4n <13494793+Technici4n@users.noreply.github.com>
2021-10-05 23:50:28 +0200

    Inventory wrappers: reuse the original stack more aggressively

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/InventorySlotWrapper.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/InventorySlotWrapper.java
@@ -79,9 +79,10 @@ class InventorySlotWrapper extends SingleStackStorage {
 		ItemStack original = lastReleasedSnapshot;
 		ItemStack currentStack = getStack();
 
-		if (!original.isEmpty() && !currentStack.isEmpty() && ItemStack.canCombine(original, currentStack)) {
-			// None is empty and the contents match: just update the amount and reuse the original stack.
+		if (!original.isEmpty() && original.getItem() == currentStack.getItem()) {
+			// None is empty and the items match: just update the amount and NBT, and reuse the original stack.
 			original.setCount(currentStack.getCount());
+			original.setNbt(currentStack.hasNbt() ? currentStack.getNbt().copy() : null);
 			setStack(original);
 		} else {
 			// Otherwise assume everything was taken from original so empty it.
