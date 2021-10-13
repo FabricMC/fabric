7bc6163d8c1f115e2e329f9e7889c059b78baa20
crashniels <NielsWest@live.de>
2021-10-13 19:28:59 +0200

    Fix texture not loading in testmod

--- fabric-rendering-v1/src/main/java/net/fabricmc/fabric/mixin/client/rendering/MixinItemRenderer.java
+++ fabric-rendering-v1/src/main/java/net/fabricmc/fabric/mixin/client/rendering/MixinItemRenderer.java
@@ -155,7 +155,7 @@ public abstract class MixinItemRenderer {
 		ItemDamageBarInfo props = ItemOverlayMaps.DAMAGE_BAR_INFO_MAP.get(stack.getItem());
 
 		if (props == null) {
-			return stack.isDamaged();
+			return stack.isItemBarVisible();
 		}
 
 		return props.isVisible(stack);
