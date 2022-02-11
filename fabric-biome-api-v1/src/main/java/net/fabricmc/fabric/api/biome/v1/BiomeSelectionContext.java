58d7bd3d48265ccee78784f424e7845b002bffaf
modmuss50 <modmuss50@gmail.com>
2022-02-11 18:29:38 +0000

    Update fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/api/biome/v1/BiomeSelectionContext.java
    Co-authored-by: Juuxel <6596629+Juuxel@users.noreply.github.com>

--- fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/api/biome/v1/BiomeSelectionContext.java
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/api/biome/v1/BiomeSelectionContext.java
@@ -146,7 +146,7 @@ public interface BiomeSelectionContext {
 	boolean canGenerateIn(RegistryKey<DimensionOptions> dimensionKey);
 
 	/**
-	 * Returns true if this biome is in the given {@link TagKey}.
+	 * {@return true if this biome is in the given {@link TagKey}}
 	 */
 	boolean hasTag(TagKey<Biome> tag);
 }
