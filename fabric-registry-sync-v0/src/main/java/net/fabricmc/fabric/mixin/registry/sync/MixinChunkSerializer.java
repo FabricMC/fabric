0d9ab3728411bc915bec770d507fac0199a2abd9
modmuss50 <modmuss50@gmail.com>
2022-03-05 18:10:57 +0000

    Fix a vanilla bug causing missing structures to reset chunks when upgrading a world from previous versions. (#2045)

---
+++ fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/mixin/registry/sync/MixinChunkSerializer.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.registry.sync;
+
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.world.ChunkSerializer;
+
+@Mixin(ChunkSerializer.class)
+public class MixinChunkSerializer {
+	@Redirect(method = "readStructureReferences", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"))
+	private static void log(Logger logger, String msg, Object identifier, Object chunkPos) {
+		// Drop to debug log level.
+		logger.debug(msg, identifier, chunkPos);
+	}
+}
