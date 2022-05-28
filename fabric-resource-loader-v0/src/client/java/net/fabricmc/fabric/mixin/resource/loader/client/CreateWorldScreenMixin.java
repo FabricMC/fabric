c47facf215239084a1f3b9be167573049a6ce937
apple502j <33279053+apple502j@users.noreply.github.com>
2022-05-29 00:20:14 +0900

    Also fix this for the dedicated server

--- fabric-resource-loader-v0/src/client/java/net/fabricmc/fabric/mixin/resource/loader/client/CreateWorldScreenMixin.java
+++ fabric-resource-loader-v0/src/client/java/net/fabricmc/fabric/mixin/resource/loader/client/CreateWorldScreenMixin.java
@@ -17,8 +17,6 @@
 package net.fabricmc.fabric.mixin.resource.loader.client;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.JsonOps;
@@ -35,16 +33,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
 import net.minecraft.resource.DataPackSettings;
 import net.minecraft.resource.ResourceManager;
-import net.minecraft.resource.ResourcePack;
 import net.minecraft.resource.ResourcePackManager;
-import net.minecraft.resource.ResourcePackProfile;
 import net.minecraft.resource.ResourceType;
 import net.minecraft.util.dynamic.RegistryOps;
 import net.minecraft.util.registry.DynamicRegistryManager;
 
-import net.fabricmc.fabric.impl.resource.loader.FabricModResourcePack;
-import net.fabricmc.fabric.impl.resource.loader.ModNioResourcePack;
 import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
+import net.fabricmc.fabric.impl.resource.loader.ModResourcePackUtil;
 import net.fabricmc.fabric.mixin.resource.loader.ResourcePackManagerAccessor;
 
 @Mixin(CreateWorldScreen.class)
@@ -65,26 +60,7 @@ public class CreateWorldScreenMixin {
 
 	@Redirect(method = "create(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "FIELD", target = "Lnet/minecraft/resource/DataPackSettings;SAFE_MODE:Lnet/minecraft/resource/DataPackSettings;", ordinal = 0))
 	private static DataPackSettings replaceDefaultSettings() {
-		ModResourcePackCreator modResourcePackCreator = new ModResourcePackCreator(ResourceType.SERVER_DATA);
-		List<ResourcePackProfile> moddedResourcePacks = new ArrayList<>();
-		modResourcePackCreator.register(moddedResourcePacks::add);
-
-		List<String> enabled = new ArrayList<>(DataPackSettings.SAFE_MODE.getEnabled());
-		List<String> disabled = new ArrayList<>(DataPackSettings.SAFE_MODE.getDisabled());
-
-		// This ensures that any built-in registered data packs by mods which needs to be enabled by default are
-		// as the data pack screen automatically put any data pack as disabled except the Default data pack.
-		for (ResourcePackProfile profile : moddedResourcePacks) {
-			try (ResourcePack pack = profile.createResourcePack()) {
-				if (pack instanceof FabricModResourcePack || (pack instanceof ModNioResourcePack && ((ModNioResourcePack) pack).getActivationType().isEnabledByDefault())) {
-					enabled.add(profile.getName());
-				} else {
-					disabled.add(profile.getName());
-				}
-			}
-		}
-
-		return (defaultDataPackSettings = new DataPackSettings(enabled, disabled));
+		return (defaultDataPackSettings = ModResourcePackUtil.createDefaultDataPackSettings());
 	}
 
 	@ModifyArg(method = "create(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/Screen;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/world/CreateWorldScreen;<init>(Lnet/minecraft/client/gui/screen/Screen;Lnet/minecraft/resource/DataPackSettings;Lnet/minecraft/client/gui/screen/world/MoreOptionsDialog;)V"), index = 1)
