e8b09dcb1086909e6be1b1949f401d5cc9963acb
aws404 <aws40404@gmail.com>
2022-06-14 04:39:19 +1000

    Add registries for villager interactions to fabric-content-registries module (#2193)
    * Add VillagerFoodRegistry
    
    * Fix style
    
    * Add VillagerCompostingRegistry and separate CollectablesRegistry from FoodRegistry
    
    * Add VillagerPlantableRegistry
    
    * Add warning when registering a non-compostable item as a villager compostable
    
    * Rename some registries, use block placement sound
    
    * Add VillagerHeroGiftRegistry
    
    * Add javadoc
    
    * Combined all registries into one API class
    
    * Remove now redundant class
    
    * Change registries to a static method
    
    * Combine into VillagerInteractionRegistries class
    
    * Fix typo
    
    * Move ImmutableCollectionUtils to impl
    
    * Add isEmpty check to plantables, prefix mixin methods with fabric_
    
    * VillagerPlantableRegistry Rework
    
    * Remove dangling comment
    
    * notNulls, included vanilla items in plantable registry, checkstyle fix
    
    (cherry picked from commit 6f01bfd847897a5825a80444ac66109285039fc7)

---
+++ fabric-content-registries-v0/src/main/java/net/fabricmc/fabric/mixin/content/registry/FarmerWorkTaskAccessor.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.content.registry;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import net.minecraft.entity.ai.brain.task.FarmerWorkTask;
+import net.minecraft.item.Item;
+
+@Mixin(FarmerWorkTask.class)
+public interface FarmerWorkTaskAccessor {
+	@Mutable
+	@Accessor("COMPOSTABLES")
+	static void fabric_setCompostables(List<Item> items) {
+		throw new AssertionError("Untransformed @Accessor");
+	}
+
+	@Accessor("COMPOSTABLES")
+	static List<Item> fabric_getCompostable() {
+		throw new AssertionError("Untransformed @Accessor");
+	}
+}
