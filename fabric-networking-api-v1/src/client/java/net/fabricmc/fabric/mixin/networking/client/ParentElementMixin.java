b174c6d2d2a85646e2bafcfc80facbc8e72de443
deirn <deirn@bai.lol>
2022-06-18 16:24:17 +0700

    Make disconnected screen reason text scrollable

---
+++ fabric-networking-api-v1/src/client/java/net/fabricmc/fabric/mixin/networking/client/ParentElementMixin.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.networking.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.gui.ParentElement;
+
+import net.fabricmc.fabric.impl.networking.client.DisconnectedScreenHooks;
+
+@Mixin(ParentElement.class)
+public interface ParentElementMixin {
+	@Inject(method = "mouseScrolled", at = @At("HEAD"))
+	default void onMouseScrolled(double mouseX, double mouseY, double amount, CallbackInfoReturnable<Boolean> cir) {
+		if (this instanceof DisconnectedScreenHooks screen) {
+			screen.fabric_mouseScrolled(mouseX, mouseY, amount);
+		}
+	}
+}
