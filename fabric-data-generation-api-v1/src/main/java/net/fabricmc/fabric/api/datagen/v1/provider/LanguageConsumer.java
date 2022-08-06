a053abc703c9fbce27bf4c95afa54c53069450fc
mineblock11 <93472213+mineblock11@users.noreply.github.com>
2022-08-06 23:38:14 +0100

    addLanguage for: EntityType, Enchantment, EntityAttribute, StatType and StatusEffect

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/LanguageConsumer.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/LanguageConsumer.java
@@ -22,8 +22,13 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.stat.StatType;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -68,6 +73,51 @@ public interface LanguageConsumer {
 	 */
 	default void addLanguage(ItemGroup group, String value) {
 		addLanguage("itemGroup." + group.getName(), value);
+	}
+
+	/**
+	 * Adds a language entry for an {@link EntityType}.
+	 * @param entityType The {@link EntityType} to get the language entry key from.
+	 * @param value The value of the entry.
+	 */
+	default void addLanguage(EntityType<?> entityType, String value) {
+		addLanguage(entityType.getTranslationKey(), value);
+	}
+
+	/**
+	 * Adds a language entry for an {@link Enchantment}.
+	 * @param enchantment The {@link Enchantment} to get the language entry key from.
+	 * @param value The value of the entry.
+	 */
+	default void addLanguage(Enchantment enchantment, String value) {
+		addLanguage(enchantment.getTranslationKey(), value);
+	}
+
+	/**
+	 * Adds a language entry for an {@link EntityAttribute}.
+	 * @param entityAttribute The {@link EntityAttribute} to get the language entry key from.
+	 * @param value The value of the entry.
+	 */
+	default void addLanguage(EntityAttribute entityAttribute, String value) {
+		addLanguage(entityAttribute.getTranslationKey(), value);
+	}
+
+	/**
+	 * Adds a language entry for a {@link StatType}.
+	 * @param statType The {@link StatType} to get the language entry key from.
+	 * @param value The value of the entry.
+	 */
+	default void addLanguage(StatType<?> statType, String value) {
+		addLanguage(statType.getTranslationKey(), value);
+	}
+
+	/**
+	 * Adds a language entry for a {@link StatusEffect}.
+	 * @param statusEffect The {@link StatusEffect} to get the language entry key from.
+	 * @param value The value of the entry.
+	 */
+	default void addLanguage(StatusEffect statusEffect, String value) {
+		addLanguage(statusEffect.getTranslationKey(), value);
 	}
 
 	/**
