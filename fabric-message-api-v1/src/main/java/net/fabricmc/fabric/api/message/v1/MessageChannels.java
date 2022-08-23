60ee37132fc3c91dc9bda01e23543cdab36b1742
BlueZeeKing <github@blueish.dev>
2022-08-22 22:07:14 -0400

    Add javadoc and fix naming and styling

--- fabric-message-api-v1/src/main/java/net/fabricmc/fabric/api/message/v1/MessageChannels.java
+++ fabric-message-api-v1/src/main/java/net/fabricmc/fabric/api/message/v1/MessageChannels.java
@@ -1,30 +1,93 @@
-package net.fabricmc.fabric.api.message.v1;
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import net.minecraft.util.Identifier;
+package net.fabricmc.fabric.api.message.v1;
 
 import java.util.HashMap;
 import java.util.UUID;
 
+import net.minecraft.util.Identifier;
+
+/**
+ * A class for controlling message channels. Channels provide a way for mods to control who sees which
+ * messages in chat. This class provides methods to set and get the channel of a user or to check if
+ * two users are in the same channel. The channels are all controlled on the server.
+ *
+ * <p>The channels are identifiers and don't need to be registered anywhere before they can be set to a
+ * player. This easily allows for the dynamic creation of channels. It also means that channels don't
+ * have to be removed when they are no longer being used.
+ *
+ * <p>This API is run completely on the server removing the need for anything on the client. This is
+ * done to allow for mods like <a href="https://github.com/Patbox/StyledChat">StyledChat</a> or other similar server-side mods
+ * to make use of this API.
+ *
+ * <p>An example of putting a player in a channel:
+ *
+ * <pre><code>
+ *     MessageChannels.setChannel(player.getUuid(), new Identifier("example", "channel1"));
+ * </code></pre>
+ *
+ * <p>An example of putting a player back into the global channel:
+ *
+ * <pre><code>
+ *     MessageChannels.setChannel(player.getUuid(), MessageChannels.GLOBAL_CHANNEL);
+ * </code></pre>
+ */
 public class MessageChannels {
-	public static final Identifier GLOBAL_CHANNEL = new Identifier("fabric-message-api-v1", "global");
-
-	private static final HashMap<UUID, Identifier> STORE = new HashMap<>();
-
 	private MessageChannels() {
 	}
 
-	public static void setUser(UUID user, Identifier channel) {
-		STORE.put(user, channel);
+	/**
+	 * The default identifier all players start in.
+	 */
+	public static final Identifier GLOBAL_CHANNEL = new Identifier("fabric-message-api-v1", "global");
+
+	private static final HashMap<UUID, Identifier> playerChannels = new HashMap<>();
+
+	/**
+	 * Set the channel of a player to an identifier. Used to move players into channels. It can be used to put players back into the global channel
+	 *
+	 * @param player The uuid of the player
+	 * @param channel The channel the player should be put into
+	 */
+	public static void setChannel(UUID player, Identifier channel) {
+		playerChannels.put(player, channel);
 	}
 
-	public static Identifier getChannel(UUID user) {
-		if (STORE.containsKey(user)) return STORE.get(user);
+	/**
+	 * Get the channel a player is in. This could be used to perform styling to include the current channel on message.
+	 *
+	 * @param player The uuid of the player to check
+	 * @return The identifier of the channel the player is inm returns the global channel by default
+	 */
+	public static Identifier getChannel(UUID player) {
+		if (playerChannels.containsKey(player)) return playerChannels.get(player);
 
 		return GLOBAL_CHANNEL;
 	}
 
-	public static boolean isInSameChannel(UUID user, UUID sender) {
-		if (STORE.containsKey(user)) return STORE.get(user).equals(getChannel(sender));
+	/**
+	 * Check if two players are in the same channel. Primarily used internally.
+	 *
+	 * @param player The uuid of the player receiving the message (interchangeable with sender)
+	 * @param sender The uuid of the player sending the message
+	 * @return Returns whether the two players are in the same channel
+	 */
+	public static boolean isInSameChannel(UUID player, UUID sender) {
+		if (playerChannels.containsKey(player)) return playerChannels.get(player).equals(getChannel(sender));
 
 		return getChannel(sender).equals(GLOBAL_CHANNEL);
 	}
--- fabric-message-api-v1/src/testmod/java/net/fabricmc/fabric/test/message/ChannelTest.java
+++ fabric-message-api-v1/src/testmod/java/net/fabricmc/fabric/test/message/ChannelTest.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.test.message;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
-
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-
-import net.fabricmc.fabric.api.message.v1.MessageChannels;
 
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.IdentifierArgumentType;
@@ -16,6 +28,9 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.message.v1.MessageChannels;
+
 public class ChannelTest {
 	private ChannelTest() {
 	}
@@ -23,6 +38,7 @@ public class ChannelTest {
 	public static void test() {
 		CommandRegistrationCallback.EVENT.register(ChannelTest::registerCommands);
 	}
+
 	private static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
 		LiteralCommandNode<ServerCommandSource> root = CommandManager
 				.literal("fabric-message-api-v1-test")
@@ -51,11 +67,10 @@ public class ChannelTest {
 			return 0;
 		}
 
-		MessageChannels.setUser(player.getUuid(), identifier);
+		MessageChannels.setChannel(player.getUuid(), identifier);
 
 		source.sendFeedback(Text.of("Successfully set channel"), false);
 
 		return 1;
 	}
-
 }
