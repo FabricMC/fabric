bb014fb7456e4d6da11f9b69d844f2b5a561679a
apple502j <33279053+apple502j@users.noreply.github.com>
2022-09-22 17:31:36 +0900

    Add special license header rule

--- fabric-data-fixer-api-v1/src/main/java/net/fabricmc/fabric/api/datafixer/v1/EmptySchema.java
+++ fabric-data-fixer-api-v1/src/main/java/net/fabricmc/fabric/api/datafixer/v1/EmptySchema.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 QuiltMC
+ * Copyright (c) 2016-2022 FabricMC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file is a modified version of Quilt Standard Libraries,
+ * authored by QuiltMC.
  */
 
 package net.fabricmc.fabric.api.datafixer.v1;
