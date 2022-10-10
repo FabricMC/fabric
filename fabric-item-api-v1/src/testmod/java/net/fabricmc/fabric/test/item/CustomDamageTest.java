7f9742a7a892e631314f77a5a05178acdea2e259
modmuss50 <modmuss50@gmail.com>
2022-10-10 20:06:14 +0100

    Add FurnaceGameTest

--- fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/CustomDamageTest.java
+++ fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/CustomDamageTest.java
@@ -16,6 +16,7 @@
 
 package net.fabricmc.fabric.test.item;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.ToolMaterials;
@@ -27,11 +28,15 @@ import net.minecraft.util.registry.Registry;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class CustomDamageTest implements ModInitializer {
+	public static final Item WEIRD_PICK = new WeirdPick();
+
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.ITEM, new Identifier("fabric-item-api-v1-testmod", "weird_pickaxe"), new WeirdPick());
+		Registry.register(Registry.ITEM, new Identifier("fabric-item-api-v1-testmod", "weird_pickaxe"), WEIRD_PICK);
+		FuelRegistry.INSTANCE.add(WEIRD_PICK, 200);
 	}
 
 	public static final CustomDamageHandler WEIRD_DAMAGE_HANDLER = (stack, amount, entity, breakCallback) -> {
@@ -58,7 +63,7 @@ public class CustomDamageTest implements ModInitializer {
 
 		@Override
 		public ItemStack getRecipeRemainder(ItemStack stack) {
-			if (stack.getDamage() < stack.getMaxDamage()) {
+			if (stack.getDamage() < stack.getMaxDamage() - 1) {
 				ItemStack moreDamaged = stack.copy();
 				moreDamaged.setCount(1);
 				moreDamaged.setDamage(stack.getDamage() + 1);
