1073aabcb123ec8ddcf612622cf0f1129e91af9d
Sebastian Hartte <shartte@users.noreply.github.com>
2022-11-03 13:42:01 +0100

    Fix end biome source.

--- fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/TheEndBiomeSourceMixin.java
+++ fabric-biome-api-v1/src/main/java/net/fabricmc/fabric/mixin/biome/TheEndBiomeSourceMixin.java
@@ -20,7 +20,12 @@ import java.util.Set;
 import java.util.function.Supplier;
 
 import com.google.common.base.Suppliers;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,8 +33,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.class_7871;
+import net.minecraft.util.dynamic.RegistryOps;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.source.TheEndBiomeSource;
 import net.minecraft.world.biome.source.util.MultiNoiseUtil;
 
@@ -37,31 +45,55 @@ import net.fabricmc.fabric.impl.biome.TheEndBiomeData;
 
 @Mixin(TheEndBiomeSource.class)
 public class TheEndBiomeSourceMixin extends BiomeSourceMixin {
+	@Shadow
+	@Mutable
+	@Final
+	static Codec<TheEndBiomeSource> CODEC;
+
 	@Unique
 	private Supplier<TheEndBiomeData.Overrides> overrides;
 
 	@Unique
 	private boolean biomeSetModified = false;
 
+	/**
+	 * Modifies the codec, so it calls the static factory method that gives us access to the
+	 * full biome registry instead of just the pre-defined biomes that vanilla uses.
+	 */
+	@Inject(method = "<clinit>", at = @At("TAIL"))
+	private static void modifyCodec(CallbackInfo ci) {
+		CODEC = RecordCodecBuilder.create((instance) -> {
+			return instance.group(RegistryOps.method_46636(Registry.BIOME_KEY)).apply(instance, instance.stable(TheEndBiomeSource::method_46680));
+		});
+	}
+
+	/**
+	 * Captures the biome registry at the beginning of the static factory method to allow access to it in the
+	 * constructor.
+	 */
 	@Inject(method = "method_46680", at = @At("HEAD"))
 	private static void rememberLookup(class_7871<Biome> biomes, CallbackInfoReturnable<?> ci) {
 		TheEndBiomeData.biomeRegistry.set(biomes);
 	}
 
+	/**
+	 * Frees up the captured biome registry.
+	 */
 	@Inject(method = "method_46680", at = @At("TAIL"))
 	private static void clearLookup(class_7871<Biome> biomes, CallbackInfoReturnable<?> ci) {
 		TheEndBiomeData.biomeRegistry.remove();
 	}
 
+	/**
+	 * Uses the captured biome registry to set up the modded end biomes.
+	 */
 	@Inject(method = "<init>", at = @At("RETURN"))
 	private void init(RegistryEntry<Biome> centerBiome, RegistryEntry<Biome> highlandsBiome, RegistryEntry<Biome> midlandsBiome, RegistryEntry<Biome> smallIslandsBiome, RegistryEntry<Biome> barrensBiome, CallbackInfo ci) {
+		class_7871<Biome> biomes = TheEndBiomeData.biomeRegistry.get();
+		if (biomes == null) {
+			throw new IllegalStateException("Biome registry not set by Mixin");
+		}
 		overrides = Suppliers.memoize(() -> {
-			class_7871<Biome> biomes = TheEndBiomeData.biomeRegistry.get();
-
-			if (biomes == null) {
-				throw new IllegalStateException("Biome registry not set by Mixin");
-			}
-
 			return TheEndBiomeData.createOverrides(biomes);
 		});
 	}
