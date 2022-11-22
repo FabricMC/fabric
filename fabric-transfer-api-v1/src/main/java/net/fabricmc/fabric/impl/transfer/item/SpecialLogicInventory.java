1c393127078d18aebce35056438a0bfa945791e4
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-11-23 00:55:53 +0100

    Add transfer API Chiseled Bookshelf support (#2685)

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/SpecialLogicInventory.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/item/SpecialLogicInventory.java
@@ -18,6 +18,8 @@ package net.fabricmc.fabric.impl.transfer.item;
 
 import net.minecraft.item.ItemStack;
 
+import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
+
 /**
  * Internal class that allows inventory instances to defer special logic until {@link InventorySlotWrapper#onFinalCommit()} is called.
  */
@@ -28,4 +30,10 @@ public interface SpecialLogicInventory {
 	void fabric_setSuppress(boolean suppress);
 
 	void fabric_onFinalCommit(int slot, ItemStack oldStack, ItemStack newStack);
+
+	/**
+	 * Called after a slot has been modified (i.e. insert or extract with result > 0).
+	 */
+	default void fabric_onTransfer(int slot, TransactionContext transaction) {
+	}
 }
