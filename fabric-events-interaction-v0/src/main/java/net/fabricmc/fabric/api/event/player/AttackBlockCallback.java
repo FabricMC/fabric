422b77fbfd85ec96cb5efd9959475aa2cbedf1d3
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-11-29 19:04:08 +0100

    AttackBlockCallback: creative fix and javadoc update (#2691)
    * AttackBlockCallback: creative fix and block break delay
    
    * More return value clarifications
    
    * Revert CONSUME_PARTIAL behavior
    
    * Apply suggestions from code review
    
    Co-authored-by: Juuz <6596629+Juuxel@users.noreply.github.com>
    
    Co-authored-by: Juuz <6596629+Juuxel@users.noreply.github.com>

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/AttackBlockCallback.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/event/player/AttackBlockCallback.java
@@ -30,12 +30,19 @@ import net.fabricmc.fabric.api.event.EventFactory;
  * Callback for left-clicking ("attacking") a block.
  * Is hooked in before the spectator check, so make sure to check for the player's game mode as well!
  *
- * <p>Upon return:
- * <ul><li>SUCCESS cancels further processing and, on the client, sends a packet to the server.
- * <li>PASS falls back to further processing.
- * <li>FAIL cancels further processing and does not send a packet to the server.</ul>
+ * <p>On the logical client, the return values have the following meaning:
+ * <ul>
+ *     <li>SUCCESS cancels further processing, causes a hand swing, and sends a packet to the server.</li>
+ *     <li>CONSUME cancels further processing, and sends a packet to the server. It does NOT cause a hand swing.</li>
+ *     <li>PASS falls back to further processing.</li>
+ *     <li>FAIL cancels further processing and does not send a packet to the server.</li>
+ * </ul>
  *
- * <p>ATTACK_BLOCK does not let you control the packet sending process yet.
+ * <p>On the logical server, the return values have the following meaning:
+ * <ul>
+ *     <li>PASS falls back to further processing.</li>
+ *     <li>Any other value cancels further processing.</li>
+ * </ul>
  */
 public interface AttackBlockCallback {
 	Event<AttackBlockCallback> EVENT = EventFactory.createArrayBacked(AttackBlockCallback.class,
