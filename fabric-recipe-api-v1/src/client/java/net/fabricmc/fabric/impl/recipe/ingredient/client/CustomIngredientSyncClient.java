88f6329dc70c011e1342a354183889c7b0b54ba3
Technici4n <13494793+Technici4n@users.noreply.github.com>
2022-12-18 15:22:35 +0100

    Fix client package change

--- fabric-recipe-api-v1/src/client/java/net/fabricmc/fabric/impl/recipe/ingredient/client/CustomIngredientSyncClient.java
+++ fabric-recipe-api-v1/src/client/java/net/fabricmc/fabric/impl/recipe/ingredient/client/CustomIngredientSyncClient.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package net.fabricmc.fabric.impl.ingredient.client;
+package net.fabricmc.fabric.impl.recipe.ingredient.client;
 
 import java.util.concurrent.CompletableFuture;
 
