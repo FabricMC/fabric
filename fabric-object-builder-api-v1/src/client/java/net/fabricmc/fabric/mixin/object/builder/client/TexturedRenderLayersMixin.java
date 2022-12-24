0a5822231366fb2f3a01b4c4e8a5169c644b0b73
AlphaMode <alphamodeyt@gmail.com>
2022-12-24 13:48:48 -0600

    Make TexturedRenderLayersMixin more direct

--- fabric-object-builder-api-v1/src/client/java/net/fabricmc/fabric/mixin/object/builder/client/TexturedRenderLayersMixin.java
+++ fabric-object-builder-api-v1/src/client/java/net/fabricmc/fabric/mixin/object/builder/client/TexturedRenderLayersMixin.java
@@ -16,21 +16,31 @@
 
 package net.fabricmc.fabric.mixin.object.builder.client;
 
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.SignType;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import net.minecraft.client.render.TexturedRenderLayers;
 
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 @Mixin(TexturedRenderLayers.class)
 public class TexturedRenderLayersMixin {
-	@ModifyArg(method = "createSignTextureId", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;<init>(Ljava/lang/String;)V"))
-	private static String modifyTextureId(String id) {
-		if (id.contains(":")) {
-			String[] split = id.substring("entity/signs/".length()).split(":");
-			return split[0] + ":entity/signs/" + split[1];
-		}
+	@Shadow
+	@Final
+	public static Identifier SIGNS_ATLAS_TEXTURE;
 
-		return id;
+	@Inject(method = "createSignTextureId", at = @At("HEAD"), cancellable = true)
+	private static void modifyTextureId(SignType type, CallbackInfoReturnable<SpriteIdentifier> cir) {
+		if (type.getName().contains(":")) {
+			Identifier location = new Identifier(type.getName());
+			cir.setReturnValue(new SpriteIdentifier(SIGNS_ATLAS_TEXTURE, new Identifier(location.getNamespace(), "entity/signs/" + location.getPath())));
+		}
 	}
 }
