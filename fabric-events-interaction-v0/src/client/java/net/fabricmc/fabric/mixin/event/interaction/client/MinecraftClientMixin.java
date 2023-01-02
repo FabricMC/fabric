3baeb27ac38c627c8f8d4c1cd1d543317bb000b4
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-01-02 14:06:35 +0100

    Interaction events fixes (#2774)
    * Deprecate `BlockAttackInteractionAware`
    
    * Document PickBlock events, fix nullability and edge case
    
    * Fix `UseEntityCallback` usability and documentation. Fixes #1260. Fixes #1870.

--- fabric-events-interaction-v0/src/client/java/net/fabricmc/fabric/mixin/event/interaction/client/MinecraftClientMixin.java
+++ fabric-events-interaction-v0/src/client/java/net/fabricmc/fabric/mixin/event/interaction/client/MinecraftClientMixin.java
@@ -22,19 +22,28 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
 
 import net.fabricmc.fabric.api.event.client.player.ClientPickBlockApplyCallback;
 import net.fabricmc.fabric.api.event.client.player.ClientPickBlockCallback;
 import net.fabricmc.fabric.api.event.client.player.ClientPickBlockGatherCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
@@ -114,6 +123,38 @@ public abstract class MinecraftClientMixin {
 	public void cancelItemPick(CallbackInfo info) {
 		if (fabric_itemPickCancelled) {
 			info.cancel();
+		}
+	}
+
+	@Shadow
+	private ClientPlayerEntity player;
+
+	@Shadow
+	public abstract ClientPlayNetworkHandler getNetworkHandler();
+
+	@Inject(
+			at = @At(
+					value = "INVOKE",
+					target = "net/minecraft/client/network/ClientPlayerInteractionManager.interactEntityAtLocation(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/hit/EntityHitResult;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/ActionResult;"
+			),
+			method = "doItemUse",
+			cancellable = true,
+			locals = LocalCapture.CAPTURE_FAILHARD
+	)
+	private void injectUseEntityCallback(CallbackInfo ci, Hand[] hands, int i1, int i2, Hand hand, ItemStack stack, EntityHitResult hitResult, Entity entity) {
+		ActionResult result = UseEntityCallback.EVENT.invoker().interact(player, player.getEntityWorld(), hand, entity, hitResult);
+
+		if (result != ActionResult.PASS) {
+			if (result.isAccepted()) {
+				Vec3d hitVec = hitResult.getPos().subtract(entity.getX(), entity.getY(), entity.getZ());
+				getNetworkHandler().sendPacket(PlayerInteractEntityC2SPacket.interactAt(entity, player.isSneaking(), hand, hitVec));
+			}
+
+			if (result.shouldSwingHand()) {
+				player.swingHand(hand);
+			}
+
+			ci.cancel();
 		}
 	}
 }
