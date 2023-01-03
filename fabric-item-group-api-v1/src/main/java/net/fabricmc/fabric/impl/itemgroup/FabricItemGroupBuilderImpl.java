0b7a407d7762b04f6a0d5002fe10160c3c0c733d
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-01-03 15:58:13 +0100

    Remove leftover `@ApiStatus.Internal` annotations and add annotation validation

--- fabric-item-group-api-v1/src/main/java/net/fabricmc/fabric/impl/itemgroup/FabricItemGroupBuilderImpl.java
+++ fabric-item-group-api-v1/src/main/java/net/fabricmc/fabric/impl/itemgroup/FabricItemGroupBuilderImpl.java
@@ -18,12 +18,9 @@ package net.fabricmc.fabric.impl.itemgroup;
 
 import java.util.Objects;
 
-import org.jetbrains.annotations.ApiStatus;
-
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 
-@ApiStatus.Internal
 public final class FabricItemGroupBuilderImpl extends ItemGroup.Builder {
 	private final Identifier identifier;
 
