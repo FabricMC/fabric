d6798ee84dc337672e135aa3d35fc615d6622d97
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-01-06 17:27:35 +0100

    Overwrite instead

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/ItemStackMixin.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/ItemStackMixin.java
@@ -20,6 +20,7 @@ import java.util.function.Consumer;
 
 import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -36,6 +37,7 @@ import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemStack;
 import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
@@ -101,15 +103,21 @@ public abstract class ItemStackMixin implements FabricItemStack {
 		return item.isSuitableFor((ItemStack) (Object) this, state);
 	}
 
+	@Final
+	@Shadow
+	private Item item;
+	@Shadow
+	private int count;
+	@Shadow
+	private boolean empty;
+
 	/**
-	 * Fix for <a href="https://bugs.mojang.com/projects/MC/issues/MC-258939">MC-258939</a>.
+	 * Soft-overwrite updateEmptyState to fix <a href="https://bugs.mojang.com/projects/MC/issues/MC-258939">MC-258939</a>.
+	 * Cannot hard-overwrite because Lithium contains a similar but insufficient inject.
 	 */
-	@Inject(at = @At("HEAD"), method = "updateEmptyState", cancellable = true)
-	private void fabric_fixUpdateEmptyState(CallbackInfo ci) {
-		if ((Object) this == ItemStack.EMPTY) {
-			// Note that ItemStack.EMPTY is still null the first time this is called, hence it will be correctly initialised.
-			// Prevent further changes to the cached empty state.
-			ci.cancel();
-		}
+	@Inject(method = "updateEmptyState", at = @At("HEAD"), cancellable = true)
+	private void updateEmptyState(CallbackInfo ci) {
+		this.empty = this.item == null || this.item == Items.AIR || this.count <= 0;
+		ci.cancel();
 	}
 }
