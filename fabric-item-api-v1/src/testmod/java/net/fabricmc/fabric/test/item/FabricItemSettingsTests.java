c12c46b694792825abd0589130b7cb186bf6ddbb
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-02-11 12:33:15 +0200

    Add tests for the getters

--- fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/FabricItemSettingsTests.java
+++ fabric-item-api-v1/src/testmod/java/net/fabricmc/fabric/test/item/FabricItemSettingsTests.java
@@ -16,20 +16,44 @@
 
 package net.fabricmc.fabric.test.item;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
-import net.minecraft.util.Identifier;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.CustomDamageHandler;
+import net.fabricmc.fabric.api.item.v1.EquipmentSlotProvider;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 
 public class FabricItemSettingsTests implements ModInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(FabricItemSettingsTests.class);
+
 	@Override
 	public void onInitialize() {
-		// Registers an item with a custom equipment slot.
-		Item testItem = new Item(new FabricItemSettings().equipmentSlot(stack -> EquipmentSlot.CHEST));
+		EquipmentSlotProvider equipmentSlotProvider = stack -> EquipmentSlot.CHEST;
+		CustomDamageHandler damageHandler = (stack, amount, entity, breakCallback) -> amount;
+
+		// Registers an item with a custom equipment slot and a no-op custom damage handler.
+		Item testItem = new Item(new FabricItemSettings().equipmentSlot(equipmentSlotProvider).customDamage(damageHandler));
 		Registry.register(Registries.ITEM, new Identifier("fabric-item-api-v1-testmod", "test_item"), testItem);
+
+		// Test getters
+		assertIdentical(EquipmentSlotProvider.get(testItem), equipmentSlotProvider, "equipment slot provider");
+		assertIdentical(EquipmentSlotProvider.get(Items.APPLE), null, "equipment slot provider");
+		assertIdentical(CustomDamageHandler.get(testItem), damageHandler, "damage handler");
+		assertIdentical(CustomDamageHandler.get(Items.APPLE), null, "damage handler");
+		LOGGER.info("FabricItemSettings tests passed!");
+	}
+
+	private static void assertIdentical(Object found, Object expected, String message) {
+		if (found != expected) {
+			throw new AssertionError("Expected " + expected + " for " + message + ", but was " + found);
+		}
 	}
 }
