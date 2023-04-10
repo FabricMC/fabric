e7e35deafcc214af5dcdfbb4dc3c27b264e9ed5d
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-04-10 23:17:00 +0200

    Finish first iteration

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/entity/FakePlayer.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/entity/FakePlayer.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.api.entity;
+
+import java.util.Map;
+import java.util.Objects;
 
 import com.google.common.collect.MapMaker;
 import com.mojang.authlib.GameProfile;
-
-import net.fabricmc.fabric.impl.event.interaction.FakePlayerNetworkHandler;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.network.packet.c2s.play.ClientSettingsC2SPacket;
@@ -12,8 +29,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stat;
 import net.minecraft.text.Text;
 
-import java.util.Map;
-import java.util.Objects;
+import net.fabricmc.fabric.impl.event.interaction.FakePlayerNetworkHandler;
 
 /**
  * A "fake player", i.e. a {@link ServerPlayerEntity} that is not a human player.
