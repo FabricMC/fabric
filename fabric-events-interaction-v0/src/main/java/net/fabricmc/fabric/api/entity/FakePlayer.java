c47f42d3d005b9c821adeea09063d0494283b9ce
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-04-10 23:27:51 +0200

    Allow subclassing FakePlayer

--- fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/entity/FakePlayer.java
+++ fabric-events-interaction-v0/src/main/java/net/fabricmc/fabric/api/entity/FakePlayer.java
@@ -35,32 +35,35 @@ import net.fabricmc.fabric.impl.event.interaction.FakePlayerNetworkHandler;
  * A "fake player", i.e. a {@link ServerPlayerEntity} that is not a human player.
  * They are typically used to automatically perform player actions such as placing blocks.
  *
- * <p>Fake players can be obtained with {@link FakePlayer#get(ServerWorld, GameProfile)}.
+ * <p>The easiest way to obtain a fake player is with {@link FakePlayer#get(ServerWorld, GameProfile)}.
+ * It is also possible to create a subclass for more control over the fake player's behavior.
  * For good inter-mod compatibility, fake players should have the UUID of their owning (human) player.
  *
  * <p>Fake players try to behave like regular {@link ServerPlayerEntity} objects to a reasonable extent.
  * In some edge cases, or for gameplay considerations, it might be necessary to check whether a {@link ServerPlayerEntity} is a fake player.
  * This can be done with an {@code instanceof} check: {@code player instanceof FakePlayer}.
  */
-// TODO: to finalize or not to finalize?
-public final class FakePlayer extends ServerPlayerEntity {
+public class FakePlayer extends ServerPlayerEntity {
 	/**
-	 * Retrieve the fake player for the specified world and game profile, or create it if it doesn't exist.
+	 * Retrieve a fake player for the specified world and game profile.
 	 *
-	 * <p>Caution should be exerted when storing the returned value, as strong references to the fake player will keep the world loaded.
+	 * <p>Instances are reused for the same parameters.
+	 *
+	 * <p>Caution should be exerted when storing the returned value,
+	 * as strong references to the fake player will keep the world loaded.
 	 */
 	public static FakePlayer get(ServerWorld world, GameProfile profile) {
 		Objects.requireNonNull(world, "World may not be null.");
 		Objects.requireNonNull(profile, "Game profile may not be null.");
 
-		return FAKE_PLAYER_MAP.computeIfAbsent(new FakePlayerKey(world, profile), FakePlayer::new);
+		return FAKE_PLAYER_MAP.computeIfAbsent(new FakePlayerKey(world, profile), key -> new FakePlayer(key.world, key.profile));
 	}
 
 	private record FakePlayerKey(ServerWorld world, GameProfile profile) { }
 	private static final Map<FakePlayerKey, FakePlayer> FAKE_PLAYER_MAP = new MapMaker().weakValues().makeMap();
 
-	private FakePlayer(FakePlayerKey key) {
-		super(key.world().getServer(), key.world(), key.profile());
+	protected FakePlayer(ServerWorld world, GameProfile profile) {
+		super(world.getServer(), world, profile);
 
 		this.networkHandler = new FakePlayerNetworkHandler(this);
 	}
