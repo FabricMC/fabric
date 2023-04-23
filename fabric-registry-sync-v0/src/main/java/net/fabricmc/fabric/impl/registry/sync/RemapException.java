670e8ac6c505e824855cb44af4f37df718b60b74
modmuss50 <modmuss50@gmail.com>
2023-04-23 13:21:39 +0100

    Improve missing local registry entries error message. (#3004)

--- fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/impl/registry/sync/RemapException.java
+++ fabric-registry-sync-v0/src/main/java/net/fabricmc/fabric/impl/registry/sync/RemapException.java
@@ -16,12 +16,26 @@
 
 package net.fabricmc.fabric.impl.registry.sync;
 
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.text.Text;
+
 public class RemapException extends Exception {
+	@Nullable
+	private final Text text;
+
 	public RemapException(String message) {
 		super(message);
+		this.text = null;
 	}
 
-	public RemapException(String message, Throwable t) {
-		super(message, t);
+	public RemapException(Text text) {
+		super(text.getString());
+		this.text = text;
+	}
+
+	@Nullable
+	public Text getText() {
+		return text;
 	}
 }
