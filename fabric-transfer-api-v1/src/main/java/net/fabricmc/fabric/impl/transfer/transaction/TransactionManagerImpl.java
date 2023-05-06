28974cb205125e493aa618469d20c545a1e34ce3
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-05-06 16:40:34 +0300

    Add owning thread to TransactionImpl.toString

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/transaction/TransactionManagerImpl.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/impl/transfer/transaction/TransactionManagerImpl.java
@@ -238,7 +238,7 @@ public class TransactionManagerImpl {
 
 		@Override
 		public String toString() {
-			return "Transaction[depth=%d, lifecycle=%s]".formatted(nestingDepth, lifecycle.name());
+			return "Transaction[depth=%d, lifecycle=%s, thread=%s]".formatted(nestingDepth, lifecycle.name(), thread);
 		}
 	}
 }
