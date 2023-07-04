db122b81f140a2472f27f2c5ba76d2d892155126
modmuss50 <modmuss50@gmail.com>
2023-07-04 20:28:03 +0100

    I give up for now

--- .github/workflows/client_test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-tmux new-session -d -s TestRecorder 'ffmpeg -f x11grab -s 854x480 -i :0.0 -codec:v libx264 -r 5 run/screenshots/test_recording.mp4'
-./gradlew runProductionAutoTestClient --stacktrace --warning-mode=fail
-tmux send-keys -t TestRecorder q
