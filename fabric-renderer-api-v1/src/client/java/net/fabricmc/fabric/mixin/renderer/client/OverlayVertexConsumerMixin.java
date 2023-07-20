616f0fd66f5fef3a5176bacc334e9c4efdc2b16f
PepperCode1 <44146161+PepperCode1@users.noreply.github.com>
2023-07-20 12:42:21 -0600

    Fix checkstyle

--- fabric-renderer-api-v1/src/client/java/net/fabricmc/fabric/mixin/renderer/client/OverlayVertexConsumerMixin.java
+++ fabric-renderer-api-v1/src/client/java/net/fabricmc/fabric/mixin/renderer/client/OverlayVertexConsumerMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.renderer.client;
 
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +37,7 @@ public class OverlayVertexConsumerMixin {
 
 		for (Direction direction : DIRECTIONS) {
 			float dot = x * direction.getOffsetX() + y * direction.getOffsetY() + z * direction.getOffsetZ();
+
 			if (dot > maxDot + MathConstants.EPSILON) {
 				maxDot = dot;
 				closestDir = direction;
