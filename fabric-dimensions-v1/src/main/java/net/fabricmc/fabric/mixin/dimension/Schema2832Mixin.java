0597beaa02e9099fff935d1eca94ec40041ca426
qouteall <qouteall@163.com>
2023-07-22 16:08:25 +0800

    Fix license. Rename mixin.

--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/Schema2832Mixin.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/Schema2832Mixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.dimension;
 
 import java.util.Map;
@@ -16,7 +32,7 @@ import net.minecraft.datafixer.schema.Schema2832;
 import net.fabricmc.fabric.impl.dimension.TaggedChoiceExtension;
 
 @Mixin(value = Schema2832.class, remap = false)
-public class MixinSchema2832 {
+public class Schema2832Mixin {
 	/**
 	 * Make the DSL.taggedChoiceLazy to ignore mod custom generator types and not cause deserialization failure.
 	 */
--- fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/TaggedChoiceTypeMixin.java
+++ fabric-dimensions-v1/src/main/java/net/fabricmc/fabric/mixin/dimension/TaggedChoiceTypeMixin.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.dimension;
 
 import com.mojang.datafixers.types.Type;
@@ -18,7 +34,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import net.fabricmc.fabric.impl.dimension.TaggedChoiceTypeExtension;
 
 @Mixin(value = TaggedChoice.TaggedChoiceType.class, remap = false)
-public class MixinTaggedChoiceType<K> implements TaggedChoiceTypeExtension {
+public class TaggedChoiceTypeMixin<K> implements TaggedChoiceTypeExtension {
 	@Unique
 	private static final Logger LOGGER = LoggerFactory.getLogger("TaggedChoiceType_DimDataFix");
 
