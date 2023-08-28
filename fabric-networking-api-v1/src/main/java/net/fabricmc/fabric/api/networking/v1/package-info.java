c44e2bb37832c982278d9dd77af8836186008c84
apple502j <33279053+apple502j@users.noreply.github.com>
2023-08-28 15:30:00 +0900

    Expand package-info

--- fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/package-info.java
+++ fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/package-info.java
@@ -17,13 +17,35 @@
 /**
  * The Networking API, version 1.
  *
- * <p>For login stage networking see {@link net.fabricmc.fabric.api.networking.v1.ServerLoginNetworking}.
- * For play stage networking see {@link net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking}.
+ * <p>There are three stages of Minecraft networking, all of which are supported in this API:
+ * <dl>
+ *     <dt>LOGIN</dt>
+ *     <dd>This is the initial stage, before the player logs into the world. If using a proxy server,
+ *     the packets in this stage may be intercepted and discarded by the proxy. <strong>Most of the pre-1.20.2
+ *     uses of this event should be replaced with the CONFIGURATION stage.</strong>
+ *     Related events are found at {@link net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents},
+ *     and related methods are found at {@link net.fabricmc.fabric.api.networking.v1.ServerLoginNetworking}.
+ *     </dd>
+ *     <dt>CONFIGURATION</dt>
+ *     <dd>This is the stage after LOGIN. The player is authenticated, but still hasn't joined the
+ *     world at this point. Servers can use this phase to send configurations or verify client's mod
+ *     versions. Note that some server mods allow players in the PLAY stage to re-enter this stage,
+ *     for example when a player chooses a minigame server in a lobby.
+ *     Related events are found at {@link net.fabricmc.fabric.api.networking.v1.S2CConfigurationChannelEvents}
+ *     {@link net.fabricmc.fabric.api.networking.v1.ServerConfigurationConnectionEvents}, and related methods are found at
+ *     {@link net.fabricmc.fabric.api.networking.v1.ServerConfigurationNetworking}.
+ *     </dd>
+ *     <dt>PLAY</dt>
+ *     <dd>This is the stage after CONFIGURATION, where gameplay-related packets are sent and received.
+ *     The player has joined the world and is playing the game. Related events are found at
+ *     {@link net.fabricmc.fabric.api.networking.v1.S2CPlayChannelEvents}
+ *     and {@link net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents}, and related methods are found at
+ *     {@link net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking}.</dd>
+ * </dl>
  *
- * <p>For events related to the connection to a client see {@link net.fabricmc.fabric.api.networking.v1.ServerLoginConnectionEvents} for login stage
- * or {@link net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents} for play stage.
- *
- * <p>For events related to the ability of a client to receive packets on a channel of a specific name see {@link net.fabricmc.fabric.api.networking.v1.S2CPlayChannelEvents}.
+ * <p>In addition, this API includes helpers for {@linkplain
+ * net.fabricmc.fabric.api.networking.v1.PacketByteBufs buffer creations} and {@linkplain
+ * net.fabricmc.fabric.api.networking.v1.PlayerLookup player lookups}.
  */
 
 package net.fabricmc.fabric.api.networking.v1;
