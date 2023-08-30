e4daa5616a8c8358083ec6264e7951dcdb005cfb
deirn <deirn@bai.lol>
2023-08-30 20:31:35 +0700

    add item context test

---
+++ fabric-key-binding-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/client/keybinding/ItemKeyBindingContext.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.test.client.keybinding;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.Hand;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingContext;
+
+public class ItemKeyBindingContext implements KeyBindingContext {
+	private final Item item;
+
+	public ItemKeyBindingContext(Item item) {
+		this.item = item;
+	}
+
+	@Override
+	public boolean isActive(MinecraftClient client) {
+		ClientPlayerEntity player = client.player;
+		return IN_GAME.isActive(client) && player != null && player.getStackInHand(Hand.MAIN_HAND).isOf(item);
+	}
+
+	@Override
+	public boolean conflictsWith(KeyBindingContext other) {
+		return this == other || IN_GAME == other;
+	}
+}
--- fabric-key-binding-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/client/keybinding/KeyBindingsTest.java
+++ fabric-key-binding-api-v1/src/testmodClient/java/net/fabricmc/fabric/test/client/keybinding/KeyBindingsTest.java
@@ -18,9 +18,11 @@ package net.fabricmc.fabric.test.client.keybinding;
 
 import org.lwjgl.glfw.GLFW;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.StickyKeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -36,19 +38,22 @@ public class KeyBindingsTest implements ClientModInitializer {
 		KeyBinding stickyBinding = KeyBindingHelper.registerKeyBinding(new StickyKeyBinding("key.fabric-key-binding-api-v1-testmod.test_keybinding_sticky", GLFW.GLFW_KEY_R, "key.category.first.test", () -> true));
 		KeyBinding duplicateBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.test_keybinding_duplicate", GLFW.GLFW_KEY_RIGHT_SHIFT, "key.category.first.test"));
 
-		KeyBinding inGameBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.in_game_keybinding", GLFW.GLFW_KEY_EQUAL, "key.category.context"), KeyBindingContext.IN_GAME);
-		KeyBinding screenBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.screen_keybinding", GLFW.GLFW_KEY_EQUAL, "key.category.context"), KeyBindingContext.IN_SCREEN);
-		KeyBinding allBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.all_keybinding", GLFW.GLFW_KEY_BACKSLASH, "key.category.context"), KeyBindingContext.ALL);
+		KeyBinding inGameBinding = register("in_game_keybinding", GLFW.GLFW_KEY_EQUAL, "context", KeyBindingContext.IN_GAME);
+		KeyBinding screenBinding = register("screen_keybinding", GLFW.GLFW_KEY_EQUAL, "context", KeyBindingContext.IN_SCREEN);
+		KeyBinding allBinding = register("all_keybinding", GLFW.GLFW_KEY_BACKSLASH, "context", KeyBindingContext.ALL);
 
 		// context1 won't conflict with context2
 		// therefore, one key from context1 and context2 will both be registered as pressed
 		CustomKeyBindingContext context1 = new CustomKeyBindingContext();
-		KeyBinding customCtxBinding1 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.custom_context_1", GLFW.GLFW_KEY_SEMICOLON, "key.category.context"), context1);
-		KeyBinding customCtxBinding2 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.custom_context_2", GLFW.GLFW_KEY_SEMICOLON, "key.category.context"), context1);
+		KeyBinding customCtxBinding1 = register("custom_context_1", GLFW.GLFW_KEY_SEMICOLON, "context", context1);
+		KeyBinding customCtxBinding2 = register("custom_context_2", GLFW.GLFW_KEY_SEMICOLON, "context", context1);
 
 		CustomKeyBindingContext context2 = new CustomKeyBindingContext();
-		KeyBinding customCtxBinding3 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.custom_context_3", GLFW.GLFW_KEY_SEMICOLON, "key.category.context"), context2);
-		KeyBinding customCtxBinding4 = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod.custom_context_4", GLFW.GLFW_KEY_SEMICOLON, "key.category.context"), context2);
+		KeyBinding customCtxBinding3 = register("custom_context_3", GLFW.GLFW_KEY_SEMICOLON, "context", context2);
+		KeyBinding customCtxBinding4 = register("custom_context_4", GLFW.GLFW_KEY_SEMICOLON, "context", context2);
+
+		KeyBinding diamondSwordBinding = register("diamond_sword", GLFW.GLFW_KEY_I, "context", new ItemKeyBindingContext(Items.DIAMOND_SWORD));
+		KeyBinding netheriteSwordBinding = register("netherite_sword", GLFW.GLFW_KEY_I, "context", new ItemKeyBindingContext(Items.NETHERITE_SWORD));
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (binding1.wasPressed()) {
@@ -67,29 +72,28 @@ public class KeyBindingsTest implements ClientModInitializer {
 				client.player.sendMessage(Text.literal("Duplicate Key was pressed!"), false);
 			}
 
-			while (inGameBinding.wasPressed()) {
-				client.player.sendMessage(Text.literal("In-game key was pressed"));
-			}
+			sendMessageWhenPressed(client, inGameBinding, "In-game key was pressed");
+			sendMessageWhenPressed(client, allBinding, "ALL context key was pressed!");
 
-			while (allBinding.wasPressed()) {
-				client.player.sendMessage(Text.literal("ALL context key was pressed!"));
-			}
+			// 1 and 3 should be called at the same time
+			sendMessageWhenPressed(client, customCtxBinding1, "Custom Context Key 1 was pressed!");
+			sendMessageWhenPressed(client, customCtxBinding2, "Custom Context Key 2 was pressed!");
+			sendMessageWhenPressed(client, customCtxBinding3, "Custom Context Key 3 was pressed!");
+			sendMessageWhenPressed(client, customCtxBinding4, "Custom Context Key 4 was pressed!");
 
-			while (customCtxBinding1.wasPressed()) {
-				client.player.sendMessage(Text.literal("Custom Context Key 1 was pressed!"));
-			}
-
-			while (customCtxBinding2.wasPressed()) {
-				client.player.sendMessage(Text.literal("Custom Context Key 2 was pressed!"));
-			}
-
-			while (customCtxBinding3.wasPressed()) {
-				client.player.sendMessage(Text.literal("Custom Context Key 3 was pressed!"));
-			}
-
-			while (customCtxBinding4.wasPressed()) {
-				client.player.sendMessage(Text.literal("Custom Context Key 4 was pressed!"));
-			}
+			sendMessageWhenPressed(client, diamondSwordBinding, "Diamond Sword Key was pressed!");
+			sendMessageWhenPressed(client, netheriteSwordBinding, "Netherite Sword Key was pressed!");
 		});
 	}
+
+	private static KeyBinding register(String key, int code, String category, KeyBindingContext context) {
+		return KeyBindingHelper.registerKeyBinding(new KeyBinding("key.fabric-key-binding-api-v1-testmod." + key, code, "key.category." + category), context);
+	}
+
+	private static void sendMessageWhenPressed(MinecraftClient client, KeyBinding binding, String message) {
+		while (binding.wasPressed()) {
+			client.player.sendMessage(Text.literal(message));
+		}
+	}
+
 }
