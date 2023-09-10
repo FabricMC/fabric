cab27e5bf406ae156fd377dae0e01a61ad83ea67
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-09-10 15:46:51 -0400

    Split out non-clientside warning to its own init

---
+++ fabric-convention-tags-v1/src/client/java/net/fabricmc/fabric/impl/tag/convention/client/ConventionLogWarningsClient.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.impl.tag.convention.client;
+
+import static net.fabricmc.fabric.impl.tag.convention.ConventionLogWarningConfigs.LOG_WARNING_MODES;
+import static net.fabricmc.fabric.impl.tag.convention.ConventionLogWarningConfigs.LOG_UNTRANSLATED_WARNING_MODE;
+
+import java.util.List;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.minecraft.client.resource.language.I18n;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.tag.convention.v1.TagUtil;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class ConventionLogWarningsClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger(ConventionLogWarningsClient.class);
+
+	@Override
+	public void onInitializeClient() {
+		if (LOG_UNTRANSLATED_WARNING_MODE != LOG_WARNING_MODES.SILENCED) setupUntranslatedItemTagWarning();
+	}
+
+	private static void setupUntranslatedItemTagWarning() {
+		// Log missing item tag translations only in development environment and not running dedicated server.
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			boolean isConfigSetToDev =
+					LOG_UNTRANSLATED_WARNING_MODE == LOG_WARNING_MODES.DEV_SHORT
+					|| LOG_UNTRANSLATED_WARNING_MODE == LOG_WARNING_MODES.DEV_VERBOSE;
+
+			if (FabricLoader.getInstance().isDevelopmentEnvironment() == isConfigSetToDev) {
+				Registry<Item> itemRegistry = server.getRegistryManager().get(RegistryKeys.ITEM);
+				List<TagKey<Item>> untranslatedItemTags = new ObjectArrayList<>();
+				itemRegistry.streamTags().forEach(itemTagKey -> {
+					// We do not translate vanilla's tags at this moment.
+					if (itemTagKey.id().getNamespace().equals("minecraft")) {
+						return;
+					}
+
+					String translationKey = TagUtil.getTagTranslationKey(itemTagKey);
+
+					if (!I18n.hasTranslation(translationKey)) {
+						untranslatedItemTags.add(itemTagKey);
+					}
+				});
+
+				if (!untranslatedItemTags.isEmpty()) {
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append("""
+							\n	Dev warning - Untranslated Item Tags detected. Please translate your item tags so other mods such as recipe viewers can properly display your tag's name.
+								You can disable this dev message in Fabric API's properties config file by setting log-untranslated-item-tag-warning to "SILENCED" or see individual tags with "DEV_VERBOSE".
+							""");
+
+					// Print out all untranslated tags when desired.
+					boolean isConfigSetToVerbose =
+							LOG_UNTRANSLATED_WARNING_MODE == LOG_WARNING_MODES.DEV_VERBOSE
+							|| LOG_UNTRANSLATED_WARNING_MODE == LOG_WARNING_MODES.PROD_VERBOSE;
+
+					if (isConfigSetToVerbose) {
+						stringBuilder.append("\nUntranslated item tags:");
+
+						for (TagKey<Item> tagKey : untranslatedItemTags) {
+							stringBuilder.append("\n     ").append(tagKey.id());
+						}
+					}
+
+					LOGGER.warn(stringBuilder.toString());
+				}
+			}
+		});
+	}
+}
---
+++ fabric-convention-tags-v1/src/main/java/net/fabricmc/fabric/impl/tag/convention/ConventionLogWarnings.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.impl.tag.convention;
+
+import static net.fabricmc.fabric.impl.tag.convention.ConventionLogWarningConfigs.LOG_LEGACY_WARNING_MODE;
+import static net.fabricmc.fabric.impl.tag.convention.ConventionLogWarningConfigs.LOG_WARNING_MODES;
+
+import java.util.List;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.tag.TagKey;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class ConventionLogWarnings implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger(ConventionLogWarnings.class);
+
+	@Override
+	public void onInitialize() {
+		if (LOG_LEGACY_WARNING_MODE != LOG_WARNING_MODES.SILENCED) setupLegacyTagWarning();
+	}
+
+	// Remove in 1.22
+	private static void setupLegacyTagWarning() {
+		// Log tags that are still using legacy 'c' namespace
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			boolean isConfigSetToDev =
+					LOG_LEGACY_WARNING_MODE == LOG_WARNING_MODES.DEV_SHORT
+					|| LOG_LEGACY_WARNING_MODE == LOG_WARNING_MODES.DEV_VERBOSE;
+
+			if (FabricLoader.getInstance().isDevelopmentEnvironment() == isConfigSetToDev) {
+				List<TagKey<?>> legacyTags = new ObjectArrayList<>();
+				DynamicRegistryManager.Immutable dynamicRegistries = server.getRegistryManager();
+
+				// We only care about vanilla registries
+				dynamicRegistries.streamAllRegistries().forEach(registryEntry -> {
+					if (registryEntry.key().getValue().getNamespace().equals("minecraft")) {
+						registryEntry.value().streamTags().forEach(tagKey -> {
+							// Grab tags under 'c' namespace
+							if (tagKey.id().getNamespace().equals("c")) {
+								legacyTags.add(tagKey);
+							}
+						});
+					}
+				});
+
+				if (!legacyTags.isEmpty()) {
+					StringBuilder stringBuilder = new StringBuilder();
+					stringBuilder.append("""
+							\n	Dev warning - Legacy Tags detected. Please migrate your 'c' namespace tags to 'common' namespace! See classes under net.fabricmc.fabric.api.tag.convention.v1 package for all tags.
+								NOTE: Many tags have been moved around or renamed. Some new ones were added so please review the new tags. And make sure you follow tag conventions for new tags!
+								You can disable this message in Fabric API's properties config file by setting log-legacy-tag-warnings to "SILENCED" or see individual tags with "DEV_VERBOSE".
+							""");
+
+					// Print out all legacy tags when desired.
+					boolean isConfigSetToVerbose =
+							LOG_LEGACY_WARNING_MODE == LOG_WARNING_MODES.DEV_VERBOSE
+							|| LOG_LEGACY_WARNING_MODE == LOG_WARNING_MODES.PROD_VERBOSE;
+
+					if (isConfigSetToVerbose) {
+						stringBuilder.append("\nLegacy tags:");
+
+						for (TagKey<?> tagKey : legacyTags) {
+							stringBuilder.append("\n     ").append(tagKey);
+						}
+					}
+
+					LOGGER.warn(stringBuilder.toString());
+				}
+			}
+		});
+	}
+}
