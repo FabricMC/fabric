9897c5437686dad011601c24f87dcf10e2b416d7
LLytho <main@lytho.dev>
2023-09-30 20:37:08 +0200

    Update for checkstyle

--- fabric-loot-api-v2/src/testmod/java/net/fabricmc/fabric/test/loot/LootTest.java
+++ fabric-loot-api-v2/src/testmod/java/net/fabricmc/fabric/test/loot/LootTest.java
@@ -95,7 +95,8 @@ public class LootTest implements ModInitializer {
 
 		LootTableEvents.LOADED.register((resourceManager, lootManager) -> {
 			LootTable blackWoolTable = lootManager.getLootTable(Blocks.BLACK_WOOL.getLootTableId());
-			if(blackWoolTable == LootTable.EMPTY) {
+
+			if (blackWoolTable == LootTable.EMPTY) {
 				throw new AssertionError("black wool loot table should not be empty");
 			}
 		});
