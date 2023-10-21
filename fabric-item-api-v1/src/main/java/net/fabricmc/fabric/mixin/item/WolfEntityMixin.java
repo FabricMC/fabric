da1d26bb3760eab42fc2a22e24cd4864d58db5fb
Phoupraw <467934387@qq.com>
2023-10-21 12:54:48 +0800

    vanilla decrement item stack first then get food component

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/WolfEntityMixin.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/mixin/item/WolfEntityMixin.java
@@ -18,21 +18,33 @@ package net.fabricmc.fabric.mixin.item;
 
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.entity.passive.WolfEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 
 @Mixin(WolfEntity.class)
 class WolfEntityMixin {
+	@Unique
+	private ItemStack fabric_interactMob_itemStack = ItemStack.EMPTY;
+
+	@Inject(method = "interactMob", at = @At("HEAD"))
+	private void storeCopy(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+		fabric_interactMob_itemStack = player.getStackInHand(hand).copy();
+	}
+
 	@Redirect(method = "interactMob", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;getFoodComponent()Lnet/minecraft/item/FoodComponent;"))
 	private @Nullable FoodComponent getStackAwareFoodComponent(Item instance, PlayerEntity player, Hand hand) {
-		return player.getStackInHand(hand).getFoodComponent();
+		return fabric_interactMob_itemStack.getFoodComponent();
 	}
 
 	@Redirect(method = "isBreedingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;getFoodComponent()Lnet/minecraft/item/FoodComponent;"))
@@ -40,7 +52,7 @@ class WolfEntityMixin {
 		return stack.getFoodComponent();
 	}
 
-	@Redirect(method = "isBreedingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;isFood()Z",ordinal = 0))
+	@Redirect(method = "isBreedingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item;isFood()Z", ordinal = 0))
 	private boolean isStackAwareFood(Item instance, ItemStack stack) {
 		return stack.isFood();
 	}
