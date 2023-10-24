d94dba26aec1dc94e14e2d723567880095d1d4b3
Technici4n <13494793+Technici4n@users.noreply.github.com>
2023-10-24 16:38:49 +0200

    Remove custom First codec

--- fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/impl/recipe/ingredient/CustomIngredientImpl.java
+++ fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/impl/recipe/ingredient/CustomIngredientImpl.java
@@ -23,10 +23,8 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Stream;
 
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
-import com.mojang.serialization.DynamicOps;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.item.ItemStack;
@@ -133,34 +131,5 @@ public class CustomIngredientImpl extends Ingredient {
 
 	private <T> T coerceIngredient() {
 		return (T) customIngredient;
-	}
-
-	public static <T> Codec<T> first(Codec<T> first, Codec<T> second) {
-		return new First<>(first, second);
-	}
-
-	// Decode/encode the first codec, if that fails return the result of the second.
-	record First<T>(Codec<T> first, Codec<T> second) implements Codec<T> {
-		@Override
-		public <T1> DataResult<Pair<T, T1>> decode(DynamicOps<T1> ops, T1 input) {
-			DataResult<Pair<T, T1>> firstResult = first.decode(ops, input);
-
-			if (firstResult.result().isPresent()) {
-				return firstResult;
-			}
-
-			return second.decode(ops, input);
-		}
-
-		@Override
-		public <T1> DataResult<T1> encode(T input, DynamicOps<T1> ops, T1 prefix) {
-			DataResult<T1> firstResult = first.encode(input, ops, prefix);
-
-			if (firstResult.result().isPresent()) {
-				return firstResult;
-			}
-
-			return second.encode(input, ops, prefix);
-		}
 	}
 }
