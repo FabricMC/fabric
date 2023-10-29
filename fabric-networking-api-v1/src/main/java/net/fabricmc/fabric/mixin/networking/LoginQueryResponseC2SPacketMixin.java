ee439628b53f982c000b87cc19e33e9b0cc51ddc
modmuss50 <modmuss50@gmail.com>
2023-10-29 13:10:17 +0000

    Check payload size

--- fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/mixin/networking/LoginQueryResponseC2SPacketMixin.java
+++ fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/mixin/networking/LoginQueryResponseC2SPacketMixin.java
@@ -16,20 +16,26 @@
 
 package net.fabricmc.fabric.mixin.networking;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.network.PacketByteBuf;
-import net.minecraft.network.packet.c2s.login.LoginQueryResponsePayload;
 import net.minecraft.network.packet.c2s.login.LoginQueryResponseC2SPacket;
+import net.minecraft.network.packet.c2s.login.LoginQueryResponsePayload;
 
 import net.fabricmc.fabric.impl.networking.payload.PacketByteBufLoginQueryResponse;
 import net.fabricmc.fabric.impl.networking.payload.PayloadHelper;
 
 @Mixin(LoginQueryResponseC2SPacket.class)
 public class LoginQueryResponseC2SPacketMixin {
+	@Shadow
+	@Final
+	private static int MAX_PAYLOAD_SIZE;
+
 	@Inject(method = "readPayload", at = @At("HEAD"), cancellable = true)
 	private static void readResponse(int queryId, PacketByteBuf buf, CallbackInfoReturnable<LoginQueryResponsePayload> cir) {
 		boolean hasPayload = buf.readBoolean();
@@ -39,6 +45,6 @@ public class LoginQueryResponseC2SPacketMixin {
 			return;
 		}
 
-		cir.setReturnValue(new PacketByteBufLoginQueryResponse(PayloadHelper.read(buf)));
+		cir.setReturnValue(new PacketByteBufLoginQueryResponse(PayloadHelper.read(buf, MAX_PAYLOAD_SIZE)));
 	}
 }
