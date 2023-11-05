044d4d71cb245b90cfbaad2c81fc4931b9e7ace8
ipz233_vvv <jumpergooog@gmail.com>
2023-11-05 13:47:29 +0200

    added license headers

--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/HandledScreenMixin.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/HandledScreenMixin.java
@@ -1,30 +1,46 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin.client.rendering;
 
 
-import net.fabricmc.fabric.api.client.rendering.v1.TooltipDataCallback;
-import net.fabricmc.fabric.impl.client.rendering.tooltip.MultiTooltipData;
-
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.item.TooltipData;
-import net.minecraft.item.ItemStack;
+import java.util.Optional;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Optional;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.item.TooltipData;
+import net.minecraft.item.ItemStack;
+
+import net.fabricmc.fabric.api.client.rendering.v1.TooltipDataCallback;
+import net.fabricmc.fabric.impl.client.rendering.tooltip.MultiTooltipData;
 
 @Mixin(HandledScreen.class)
 class HandledScreenMixin {
-    @Redirect(method = "drawMouseoverTooltip",at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;getTooltipData()Ljava/util/Optional;"))
-    Optional<TooltipData> addMultiData(ItemStack stack){
-        var original = stack.getTooltipData();
-        var mutlidata = new MultiTooltipData(1);
-        original.ifPresent(mutlidata::add);
-        TooltipDataCallback.EVENT.invoker().getTooltipData(stack,mutlidata);
+	@Redirect(method = "drawMouseoverTooltip",at = @At(value = "INVOKE",target = "Lnet/minecraft/item/ItemStack;getTooltipData()Ljava/util/Optional;"))
+	Optional<TooltipData> addMultiData(ItemStack stack){
+		var original = stack.getTooltipData();
+		var mutlidata = new MultiTooltipData(1);
+		original.ifPresent(mutlidata::add);
+		TooltipDataCallback.EVENT.invoker().getTooltipData(stack,mutlidata);
 		if(mutlidata.size() == 0){
 			return Optional.empty();
 		}
 		return Optional.of(mutlidata);
-    }
+	}
 }
--- fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/tooltip/BundleFullnessTooltipTest.java
+++ fabric-rendering-v1/src/testmodClient/java/net/fabricmc/fabric/test/rendering/client/tooltip/BundleFullnessTooltipTest.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.test.rendering.client.tooltip;
 
-
-import net.fabricmc.fabric.api.client.rendering.v1.TooltipDataCallback;
 
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -11,56 +25,57 @@ import net.minecraft.item.BundleItem;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.TooltipComponentCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.TooltipDataCallback;
 
 public class BundleFullnessTooltipTest implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        TooltipDataCallback.EVENT.register((itemStack, tooltipDataList) -> {
-            if (itemStack.getItem() instanceof BundleItem bundle) {
-                tooltipDataList.add(0,new BundleCustomTooltipData(BundleItem.getAmountFilled(itemStack)));
-            }
-        });
-        TooltipComponentCallback.EVENT.register(data -> {
-            if(data instanceof BundleCustomTooltipData bundleCustomTooltipData)
-                return new BundleFullnessTooltipComponent(bundleCustomTooltipData.fullness);
-            return null;
-        });
-    }
+	@Override
+	public void onInitializeClient() {
+		TooltipDataCallback.EVENT.register((itemStack, tooltipDataList) -> {
+			if (itemStack.getItem() instanceof BundleItem bundle) {
+				tooltipDataList.add(0,new BundleCustomTooltipData(BundleItem.getAmountFilled(itemStack)));
+			}
+		});
+		TooltipComponentCallback.EVENT.register(data -> {
+			if(data instanceof BundleCustomTooltipData bundleCustomTooltipData)
+				return new BundleFullnessTooltipComponent(bundleCustomTooltipData.fullness);
+			return null;
+		});
+	}
 
-    private static class BundleCustomTooltipData implements TooltipData {
-        private final float fullness;
+	private static class BundleCustomTooltipData implements TooltipData {
+		private final float fullness;
 
-        public BundleCustomTooltipData(float fullness) {
-            this.fullness = fullness;
-        }
-    }
+		public BundleCustomTooltipData(float fullness) {
+			this.fullness = fullness;
+		}
+	}
 
-    private static class BundleFullnessTooltipComponent implements TooltipComponent {
-        private static final int BAR_WIDTH = 40;
-        private static final int BAR_HEIGHT = 10;
-        private static final int GAP = 2;
-        private final float fullness;
+	private static class BundleFullnessTooltipComponent implements TooltipComponent {
+		private static final int BAR_WIDTH = 40;
+		private static final int BAR_HEIGHT = 10;
+		private static final int GAP = 2;
+		private final float fullness;
 
-        public BundleFullnessTooltipComponent(float fullness) {
-            this.fullness = fullness;
-        }
+		public BundleFullnessTooltipComponent(float fullness) {
+			this.fullness = fullness;
+		}
 
-        @Override
-        public int getHeight() {
-            return BAR_HEIGHT + GAP;
-        }
+		@Override
+		public int getHeight() {
+			return BAR_HEIGHT + GAP;
+		}
 
-        @Override
-        public int getWidth(TextRenderer textRenderer) {
-            return BAR_WIDTH;
-        }
-        @Override
-        public void drawItems(TextRenderer textRenderer, int x, int y, DrawContext context) {
-            context.getMatrices().push();
-            context.getMatrices().translate(x,y,0);
-            context.fill(0,0,BAR_WIDTH,BAR_HEIGHT,0xFF3F007F);
-            context.fill(0,0, (int) (BAR_WIDTH*fullness),BAR_HEIGHT,0xFF7F00FF);
-            context.getMatrices().pop();
-        }
-    }
+		@Override
+		public int getWidth(TextRenderer textRenderer) {
+			return BAR_WIDTH;
+		}
+		@Override
+		public void drawItems(TextRenderer textRenderer, int x, int y, DrawContext context) {
+			context.getMatrices().push();
+			context.getMatrices().translate(x,y,0);
+			context.fill(0,0,BAR_WIDTH,BAR_HEIGHT,0xFF3F007F);
+			context.fill(0,0, (int) (BAR_WIDTH*fullness),BAR_HEIGHT,0xFF7F00FF);
+			context.getMatrices().pop();
+		}
+	}
 }
