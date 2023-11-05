53c4a12eb5298637212a3dfee173335bb8449217
ipz233_vvv <jumpergooog@gmail.com>
2023-11-05 13:47:29 +0200

    added license headers

--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/TooltipDataCallback.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/TooltipDataCallback.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.api.client.rendering.v1;
 
 import java.util.List;
@@ -10,19 +26,17 @@ import net.fabricmc.fabric.api.event.EventFactory;
 
 @FunctionalInterface
 public interface TooltipDataCallback {
-    /**
-     * Allows registering custom {@link TooltipData} object for item.
-     * This allows you to add your own tooltips to existing items.
-     * Custom {@code TooltipData} should be registered using {@link TooltipComponentCallback},
-     * otherwise game will crash when trying to map {@code TooltipData} to {@code TooltipComponent}
-     */
-    public static final Event<TooltipDataCallback> EVENT = EventFactory.createArrayBacked(TooltipDataCallback.class, callbacks -> (itemStack, tooltipDataList) -> {
-        //MultiTooltipData tooltipData = new MultiTooltipData(callbacks.length);
-        for(TooltipDataCallback callback : callbacks){
-            callback.getTooltipData(itemStack,tooltipDataList);
-        }
-    });
-    void getTooltipData(ItemStack itemStack, List<TooltipData> tooltipDataList);
+	/**
+	 * Allows registering custom {@link TooltipData} object for item.
+	 * This allows you to add your own tooltips to existing items.
+	 * Custom {@code TooltipData} should be registered using {@link TooltipComponentCallback},
+	 * otherwise game will crash when trying to map {@code TooltipData} to {@code TooltipComponent}
+	 */
+	public static final Event<TooltipDataCallback> EVENT = EventFactory.createArrayBacked(TooltipDataCallback.class, callbacks -> (itemStack, tooltipDataList) -> {
+		//MultiTooltipData tooltipData = new MultiTooltipData(callbacks.length);
+		for(TooltipDataCallback callback : callbacks){
+			callback.getTooltipData(itemStack,tooltipDataList);
+		}
+	});
+	void getTooltipData(ItemStack itemStack, List<TooltipData> tooltipDataList);
 }
-
-
