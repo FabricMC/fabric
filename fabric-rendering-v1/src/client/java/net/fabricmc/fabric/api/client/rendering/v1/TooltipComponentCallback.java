f7d84b18d135bda46181875ce81d55193974ad74
JumperOnJava <59884606+JumperOnJava@users.noreply.github.com>
2023-11-06 19:52:32 +0200

    Update fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/TooltipComponentCallback.java
    Co-authored-by: Juuz <6596629+Juuxel@users.noreply.github.com>

--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/TooltipComponentCallback.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/TooltipComponentCallback.java
@@ -28,7 +28,7 @@ import net.fabricmc.fabric.api.event.EventFactory;
 /**
  * Allows registering a mapping from {@link TooltipData} to {@link TooltipComponent}.
  * This allows custom tooltips for items: first, override {@link Item#getTooltipData} and return a custom {@code TooltipData}, or use
- * {@link TooltipDataCallback} to add {@code TooltipData} to an existing item
+ * {@link TooltipDataCallback} to add {@code TooltipData} to an existing item.
  * Second, register a listener to this event and convert the data to your component implementation if it's an instance of your data class.
  *
  * <p>Note that failure to map some data to a component will throw an exception,
