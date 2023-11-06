46b05b47bc7bed166cdc42b63daccc17abf3413e
Phoupraw <50520903+Phoupraw@users.noreply.github.com>
2023-11-07 04:43:46 +0800

    Update fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricItem.java
    Co-authored-by: Juuz <6596629+Juuxel@users.noreply.github.com>

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricItem.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricItem.java
@@ -130,7 +130,7 @@ public interface FabricItem {
 	 * Note that simple food component can also be set via {@link Item.Settings#food(FoodComponent)}.
 	 * If you want to get a food component for a stack, is <b>recommended</b> to use the stack version of this method: {@link FabricItemStack#getFoodComponent()}.
 	 *
-	 * @return this item's {@link FoodComponent}, or {@code null} if none was set.
+	 * @return this item's {@link FoodComponent}, or {@code null} if none was set
 	 */
 	default @Nullable FoodComponent getFoodComponent(ItemStack stack) {
 		return ((Item) this).getFoodComponent();
