248f1f960086a39906a94d6bc5256baea94f8a80
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-22 16:34:01 -0700

    remove injection on ServerPlayerEntity

--- fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerTransferable.java
+++ fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerTransferable.java
@@ -1,19 +1,8 @@
 package net.fabricmc.fabric.api.networking.v1;
 
-import java.net.InetSocketAddress;
-import java.net.URL;
-
 public interface ServerTransferable {
 
     void transferToServer(String host, int port);
-
-    default void transferToServer(URL address) {
-        transferToServer(address.getHost(), address.getPort());
-    }
-
-    default void transferToServer(InetSocketAddress address) {
-        transferToServer(address.getHostName(), address.getPort());
-    }
 
     boolean wasTransferred();
 }
