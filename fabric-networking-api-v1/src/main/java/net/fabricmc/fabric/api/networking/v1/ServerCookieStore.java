d3fe01721c03020664004c53e6707efd19c5dd84
MrNavaStar <Mr.NavaStar@gmail.com>
2024-01-22 18:19:10 -0700

    fix a bug when multiple mods request the same cookie + api comments

--- fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerCookieStore.java
+++ fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerCookieStore.java
@@ -6,7 +6,19 @@ import java.util.concurrent.CompletableFuture;
 
 public interface ServerCookieStore {
 
+	/**
+	 * Sets the cookie data on the client
+	 *
+	 * @param cookieId The id to tag the data with
+	 * @param cookie The data to be set on the client
+	 */
     void setCookie(Identifier cookieId, byte[] cookie);
 
+	/**
+	 * Retrieves cookie data from the client
+	 *
+	 * @param cookieId The id the data was tagged with
+	 * @return The cookie data or an empty byte[] if there was no cookie found with that id
+	 */
     CompletableFuture<byte[]> getCookie(Identifier cookieId);
 }
--- fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerTransferable.java
+++ fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/api/networking/v1/ServerTransferable.java
@@ -2,7 +2,16 @@ package net.fabricmc.fabric.api.networking.v1;
 
 public interface ServerTransferable {
 
+	/**
+	 * Sends the client to a different server. Can also be used on the current server
+	 *
+	 * @param host The hostname or ip address of the server to transfer to
+	 * @param port The port of the server to transfer to
+	 */
     void transferToServer(String host, int port);
 
+	/**
+	 * @return Weather or not this client joined from a server transfer
+	 */
     boolean wasTransferred();
 }
