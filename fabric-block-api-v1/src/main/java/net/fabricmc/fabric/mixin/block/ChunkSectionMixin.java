3e2216cb0320326c08c0d524e5d2ddaf9223d5a3
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-28 08:32:11 -0500

    Make chunk sections only convert vanilla air blocks to AIR (#3535)
    * Make chunk sections only convert vanilla air blocks to AIR
    
    * angry checkstyles calmed
    
    * Comments added for future reference

---
+++ fabric-block-api-v1/src/main/java/net/fabricmc/fabric/mixin/block/ChunkSectionMixin.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.mixin.block;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.world.chunk.ChunkSection;
+
+@Mixin(ChunkSection.class)
+public class ChunkSectionMixin {
+	/**
+	 * Makes Chunk Sections not have isAir = true modded blocks be replaced with AIR against their will.
+	 * Mojang report: https://bugs.mojang.com/browse/MC-232360
+	 */
+	@Redirect(method = "setBlockState(IIILnet/minecraft/block/BlockState;Z)Lnet/minecraft/block/BlockState;",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isAir()Z"))
+	private boolean modifyAirCheck(BlockState blockState) {
+		return blockState.isOf(Blocks.AIR) || blockState.isOf(Blocks.CAVE_AIR) || blockState.isOf(Blocks.VOID_AIR);
+	}
+}
