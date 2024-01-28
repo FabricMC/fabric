5c013344f00a1002dca01a66a8a1926945462159
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2024-01-28 13:56:09 +0100

    Allow `FabricCodecDataProvider` to access dynamic registries (#3522)
    * Add CompletableFuture to FabricCodecDataProvider
    
    * Deprecate the old method and constructor, and fix the style
    
    * Fix the style for real this time
    
    * Add exceptions to the configure methods
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: modmuss <modmuss50@gmail.com>

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricCodecDataProvider.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricCodecDataProvider.java
@@ -19,38 +19,74 @@ package net.fabricmc.fabric.api.datagen.v1.provider;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.BiConsumer;
 
 import com.google.gson.JsonElement;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
 import com.mojang.serialization.JsonOps;
 
 import net.minecraft.data.DataOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.DataWriter;
+import net.minecraft.registry.RegistryOps;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 
 /**
- * Extend this class and implement {@link FabricCodecDataProvider#configure}.
+ * Extend this class and implement {@link FabricCodecDataProvider#configure(BiConsumer, RegistryWrapper.WrapperLookup)}.
  *
  * <p>Register an instance of the class with {@link FabricDataGenerator.Pack#addProvider} in a {@link net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint}.
  */
 public abstract class FabricCodecDataProvider<T> implements DataProvider {
 	private final DataOutput.PathResolver pathResolver;
+	private final CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture;
 	private final Codec<T> codec;
 
+	/**
+	 * @deprecated Please use {@link FabricCodecDataProvider#FabricCodecDataProvider(FabricDataOutput, CompletableFuture, DataOutput.OutputType, String, Codec)}.
+	 */
+	@Deprecated()
 	protected FabricCodecDataProvider(FabricDataOutput dataOutput, DataOutput.OutputType outputType, String directoryName, Codec<T> codec) {
 		this.pathResolver = dataOutput.getResolver(outputType, directoryName);
+		this.registriesFuture = null;
+		this.codec = codec;
+	}
+
+	protected FabricCodecDataProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture, DataOutput.OutputType outputType, String directoryName, Codec<T> codec) {
+		this.pathResolver = dataOutput.getResolver(outputType, directoryName);
+		this.registriesFuture = Objects.requireNonNull(registriesFuture);
 		this.codec = codec;
 	}
 
 	@Override
 	public CompletableFuture<?> run(DataWriter writer) {
+		// TODO: Remove the null check once the deprecated method and constructor are removed.
+		if (this.registriesFuture != null) {
+			return this.registriesFuture.thenCompose(lookup -> {
+				Map<Identifier, JsonElement> entries = new HashMap<>();
+				RegistryOps<JsonElement> ops = RegistryOps.of(JsonOps.INSTANCE, lookup);
+
+				BiConsumer<Identifier, T> provider = (id, value) -> {
+					JsonElement json = this.convert(id, value, ops);
+					JsonElement existingJson = entries.put(id, json);
+
+					if (existingJson != null) {
+						throw new IllegalArgumentException("Duplicate entry " + id);
+					}
+				};
+
+				this.configure(provider, lookup);
+				return this.write(writer, entries);
+			});
+		}
+
 		Map<Identifier, JsonElement> entries = new HashMap<>();
 		BiConsumer<Identifier, T> provider = (id, value) -> {
 			JsonElement json = this.convert(id, value);
@@ -69,11 +105,29 @@ public abstract class FabricCodecDataProvider<T> implements DataProvider {
 	 * Implement this method to register entries to generate.
 	 *
 	 * @param provider A consumer that accepts an {@link Identifier} and a value to register.
+	 * @deprecated Please use {@link FabricCodecDataProvider#configure(BiConsumer, RegistryWrapper.WrapperLookup)}.
 	 */
-	protected abstract void configure(BiConsumer<Identifier, T> provider);
+	@Deprecated()
+	protected void configure(BiConsumer<Identifier, T> provider) {
+		throw new UnsupportedOperationException("Override this method.");
+	}
+
+	/**
+	 * Implement this method to register entries to generate using a {@link RegistryWrapper.WrapperLookup}.
+	 * @param provider A consumer that accepts an {@link Identifier} and a value to register.
+	 * @param lookup A lookup for registries.
+	 */
+	protected void configure(BiConsumer<Identifier, T> provider, RegistryWrapper.WrapperLookup lookup) {
+		// TODO: Make abstract once the deprecated method is removed.
+		throw new UnsupportedOperationException("Override this method.");
+	}
 
 	private JsonElement convert(Identifier id, T value) {
-		DataResult<JsonElement> dataResult = this.codec.encodeStart(JsonOps.INSTANCE, value);
+		return this.convert(id, value, JsonOps.INSTANCE);
+	}
+
+	private JsonElement convert(Identifier id, T value, DynamicOps<JsonElement> ops) {
+		DataResult<JsonElement> dataResult = this.codec.encodeStart(ops, value);
 		return dataResult.get()
 				.mapRight(partial -> "Invalid entry %s: %s".formatted(id, partial.message()))
 				.orThrow();
