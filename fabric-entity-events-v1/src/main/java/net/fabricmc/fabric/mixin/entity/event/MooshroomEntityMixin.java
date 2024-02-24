fb77cfb58618e0f5e617dcf37b4c7b10462af715
maityyy <basebollmonkey@gmail.com>
2024-02-24 13:06:35 +0300

    Fire the MOB_CONVERSION event after shearing the mushroom cow

---
+++ fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/mixin/entity/event/MooshroomEntityMixin.java
@@ -0,0 +1,24 @@
+package net.fabricmc.fabric.mixin.entity.event;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.passive.MooshroomEntity;
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+
+@Mixin(MooshroomEntity.class)
+class MooshroomEntityMixin {
+
+	@ModifyArg(
+			method = "sheared",
+			at = @At(ordinal = 0, value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z")
+	)
+	private Entity afterMooshroomConversion(Entity converted) {
+		ServerLivingEntityEvents.MOB_CONVERSION.invoker().onConversion((MooshroomEntity) (Object) this, (MobEntity) converted, false);
+		return converted;
+	}
+}
