6dad72ad9167c2b4bb2ce1fb87a0b70cd8f4c046
Treetrain1 <treetrain1@gmail.com>
2024-03-11 20:19:15 -0500

    Add data fixer entrypoint

--- fabric-data-fixer-api-v1/src/main/java/net/fabricmc/fabric/api/datafixer/v1/DataFixerEvents.java
+++ fabric-data-fixer-api-v1/src/main/java/net/fabricmc/fabric/api/datafixer/v1/DataFixerEvents.java
@@ -16,6 +16,7 @@
 
 package net.fabricmc.fabric.api.datafixer.v1;
 
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -24,21 +25,34 @@ import com.mojang.datafixers.types.templates.TypeTemplate;
 
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 
 public final class DataFixerEvents {
+	private static final String ENTRYPOINT_KEY = "fabric-data-fixer";
 	/**
-	 * Called when vanilla schema 1904 registers entities.
+	 * Called when vanilla schema 1906 registers block entities.
 	 */
 	public static final Event<RegisterBlockEntities> REGISTER_BLOCK_ENTITIES = EventFactory.createArrayBacked(RegisterBlockEntities.class, callbacks -> (registry, schema) -> {
+		List<EntrypointContainer<DataFixerEntrypoint>> dataFixerEntrypoints = FabricLoader.getInstance().getEntrypointContainers(ENTRYPOINT_KEY, DataFixerEntrypoint.class);
+		List<DataFixerEntrypoint> entrypoints = dataFixerEntrypoints.stream().map(EntrypointContainer::getEntrypoint).toList();
+		for (DataFixerEntrypoint entrypoint : entrypoints) {
+			entrypoint.onRegisterBlockEntities(registry, schema);
+		}
 		for (RegisterBlockEntities callback : callbacks) {
 			callback.onRegisterBlockEntities(registry, schema);
 		}
 	});
 
 	/**
-	 * Called when vanilla schema 1906 registers block entities.
+	 * Called when vanilla schema 1904 registers entities.
 	 */
 	public static final Event<RegisterEntities> REGISTER_ENTITIES = EventFactory.createArrayBacked(RegisterEntities.class, callbacks -> (registry, schema) -> {
+		List<EntrypointContainer<DataFixerEntrypoint>> dataFixerEntrypoints = FabricLoader.getInstance().getEntrypointContainers(ENTRYPOINT_KEY, DataFixerEntrypoint.class);
+		List<DataFixerEntrypoint> entrypoints = dataFixerEntrypoints.stream().map(EntrypointContainer::getEntrypoint).toList();
+		for (DataFixerEntrypoint entrypoint : entrypoints) {
+			entrypoint.onRegisterEntities(registry, schema);
+		}
 		for (RegisterEntities callback : callbacks) {
 			callback.onRegisterEntities(registry, schema);
 		}
