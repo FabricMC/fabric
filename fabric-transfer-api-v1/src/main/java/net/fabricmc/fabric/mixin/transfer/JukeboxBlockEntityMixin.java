7ece0da74f6159d0108021d2b828b9b23644fcae
Syst3ms <arthmm14@gmail.com>
2024-04-09 14:41:08 +0200

    fix mixin

--- fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/mixin/transfer/JukeboxBlockEntityMixin.java
+++ fabric-transfer-api-v1/src/main/java/net/fabricmc/fabric/mixin/transfer/JukeboxBlockEntityMixin.java
@@ -16,6 +16,7 @@
 
 package net.fabricmc.fabric.mixin.transfer;
 
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,18 +25,17 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.block.entity.JukeboxBlockEntity;
+import net.minecraft.inventory.SingleStackInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
 
 import net.fabricmc.fabric.impl.transfer.item.SpecialLogicInventory;
 
 @Mixin(JukeboxBlockEntity.class)
-public abstract class JukeboxBlockEntityMixin implements SpecialLogicInventory {
+abstract class JukeboxBlockEntityMixin implements SpecialLogicInventory, SingleStackInventory {
 	@Shadow
-	private ItemStack recordStack;
-
-	@Shadow
-	public abstract void setStack(ItemStack stack);
-
+	@Final
+	private DefaultedList<ItemStack> inventory;
 	@Unique
 	private boolean fabric_suppressSpecialLogic = false;
 
@@ -45,9 +45,9 @@ public abstract class JukeboxBlockEntityMixin implements SpecialLogicInventory {
 	}
 
 	@Inject(method = "setStack", at = @At("HEAD"), cancellable = true)
-	private void setStackBypass(ItemStack stack, CallbackInfo ci) {
+	private void setStackBypass(int slot, ItemStack stack, CallbackInfo ci) {
 		if (fabric_suppressSpecialLogic) {
-			recordStack = stack;
+			inventory.set(0, stack);
 			ci.cancel();
 		}
 	}
