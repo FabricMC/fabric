255e8be7d693cfc360fc42d750f28214ef75fda1
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-12 13:18:43 -0400

    Added clarification to overworld tag doc for strongholds

--- fabric-convention-tags-v2/src/main/java/net/fabricmc/fabric/api/tag/convention/v2/ConventionalBiomeTags.java
+++ fabric-convention-tags-v2/src/main/java/net/fabricmc/fabric/api/tag/convention/v2/ConventionalBiomeTags.java
@@ -43,7 +43,11 @@ public final class ConventionalBiomeTags {
 	/**
 	 * Biomes that spawn in the Overworld.
 	 * (This is for people who want to tag their biomes as Overworld without getting
-	 * side effects from {@link net.minecraft.registry.tag.BiomeTags.IS_OVERWORLD})
+	 * side effects from {@link net.minecraft.registry.tag.BiomeTags.IS_OVERWORLD}.
+	 * <p>
+	 * NOTE: If you do not add to the vanilla Overworld tag, be sure to add to
+	 * {@link net.minecraft.registry.tag.BiomeTags.STRONGHOLD_HAS_STRUCTURE} so
+	 * some Strongholds do not go missing.)
 	 */
 	public static final TagKey<Biome> IS_OVERWORLD = register("is_overworld");
 
