77e7b2db328a6cb7aa4450f7b01c85ecd5f1ba5c
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-04-26 03:29:14 +0200

    change mixin from ModifyConstant to ModifyArg to be more clear.

--- fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/SelectKnownPacksC2SPacketMixin.java
+++ fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/mixin/resource/loader/SelectKnownPacksC2SPacketMixin.java
@@ -17,8 +17,8 @@
 package net.fabricmc.fabric.mixin.resource.loader;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.network.packet.c2s.config.SelectKnownPacksC2SPacket;
 
@@ -26,7 +26,7 @@ import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
 
 @Mixin(SelectKnownPacksC2SPacket.class)
 public class SelectKnownPacksC2SPacketMixin {
-	@ModifyConstant(method = "<clinit>", constant = @Constant(intValue = 64))
+	@ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/codec/PacketCodecs;toList(I)Lnet/minecraft/network/codec/PacketCodec$ResultFunction;"))
 	private static int setMaxKnownPacks(int constant) {
 		return ModResourcePackCreator.MAX_KNOWN_PACKS;
 	}
