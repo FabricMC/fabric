c0e5481f61aa0c28fc656f99c4da367278029a10
Jochen Jacobs <JochenJacobs33@gmail.com>
2024-05-07 20:03:31 +0200

    Use knownPackInfo on mod data packs to avoid registry syncing (#3726)
    * use knownPack to avoid syncing mod data packs.
    
    * remove empty class
    
    * checkstyle and licenses
    
    * move debug logging from testmod into main module
    
    * fix knownPackInfo not working for build-in data packs.
    
    * make bundled data packs not required for client, and don't auto enable
    
    * improve testmod with added builtin data pack + better error message
    
    * Increase max known packs in C2S packet
    
    * validate size before sending
    * changeable with system property
    
    * change mixin from ModifyConstant to ModifyArg to be more clear.
    
    * Apply suggestions from code review
    
    Co-authored-by: modmuss <modmuss50@gmail.com>
    
    * store list of knownPacks as server start
    
    avoids desync on pack change (since registries aren't reloaded)
    
    * be extra safe: only request knownPacks that were enabled at server start and still are.
    
    Doesn't rely on the fact that registries aren't synced anymore.
    
    ---------
    
    Co-authored-by: modmuss <modmuss50@gmail.com>

--- fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/impl/resource/loader/ResourceManagerHelperImpl.java
+++ fabric-resource-loader-v0/src/main/java/net/fabricmc/fabric/impl/resource/loader/ResourceManagerHelperImpl.java
@@ -25,7 +25,6 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -119,7 +118,7 @@ public class ResourceManagerHelperImpl implements ResourceManagerHelper {
 						entry.getRight().getId(),
 						entry.getLeft(),
 						new BuiltinModResourcePackSource(pack.getFabricModMetadata().getName()),
-						Optional.empty()
+						entry.getRight().getKnownPackInfo()
 				);
 				ResourcePackPosition info2 = new ResourcePackPosition(
 						pack.getActivationType() == ResourcePackActivationType.ALWAYS_ENABLED,
