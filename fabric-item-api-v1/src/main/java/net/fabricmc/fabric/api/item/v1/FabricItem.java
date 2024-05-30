afdfc921b8827f28c09cb934f0a310c88efc4bce
modmuss50 <modmuss50@gmail.com>
2024-05-30 13:01:07 +0100

    Remove FabricItem.getAttributeModifiers.

--- fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricItem.java
+++ fabric-item-api-v1/src/main/java/net/fabricmc/fabric/api/item/v1/FabricItem.java
@@ -16,7 +16,6 @@
 
 package net.fabricmc.fabric.api.item.v1;
 
-import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -62,19 +61,6 @@ public interface FabricItem {
 	 */
 	default boolean allowContinuingBlockBreaking(PlayerEntity player, ItemStack oldStack, ItemStack newStack) {
 		return false;
-	}
-
-	/**
-	 * Return the attribute modifiers to apply when this stack is worn in a living entity equipment slot.
-	 * Stack-aware version of {@link Item#getAttributeModifiers()}.
-	 *
-	 * <p>Note that attribute modifiers are only updated when the stack changes, i.e. when {@code ItemStack.areEqual(old, new)} is false.
-	 *
-	 * @param stack the current stack
-	 * @return the attribute modifiers
-	 */
-	default AttributeModifiersComponent getAttributeModifiers(ItemStack stack) {
-		return ((Item) this).getAttributeModifiers();
 	}
 
 	/**
