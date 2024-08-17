88a80b1002cbe6c37211fd89168d7ddd44833d35
hypherionmc <hypherionmc@gmail.com>
2024-08-17 17:49:18 +0200

    Fix some mixin crashes

--- fabric-content-registries-v0/src/main/java/net/fabricmc/fabric/mixin/content/registry/VillagerEntityMixin.java
+++ fabric-content-registries-v0/src/main/java/net/fabricmc/fabric/mixin/content/registry/VillagerEntityMixin.java
@@ -18,6 +18,10 @@ package net.fabricmc.fabric.mixin.content.registry;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.tag.TagKey;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -29,7 +33,7 @@ import net.fabricmc.fabric.impl.content.registry.VillagerInteractionRegistriesIm
 @Mixin(VillagerEntity.class)
 public class VillagerEntityMixin {
 	@WrapOperation(method = "canGather", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"))
-	private boolean useGatherableItemsSet(ItemStack stack, Operation<Boolean> original) {
-		return VillagerInteractionRegistriesImpl.getCollectableRegistry().contains(stack.getItem()) || original.call(stack);
+	private boolean useGatherableItemsSet(ItemStack stack, TagKey<Item> tag, Operation<Boolean> original) {
+		return VillagerInteractionRegistriesImpl.getCollectableRegistry().contains(stack.getItem()) || original.call(stack, tag);
 	}
 }
--- fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/mixin/recipe/ingredient/ShapelessRecipeMixin.java
+++ fabric-recipe-api-v1/src/main/java/net/fabricmc/fabric/mixin/recipe/ingredient/ShapelessRecipeMixin.java
@@ -42,7 +42,7 @@ import net.fabricmc.fabric.impl.recipe.ingredient.ShapelessMatch;
 public class ShapelessRecipeMixin {
 	@Final
 	@Shadow
-	DefaultedList<Ingredient> ingredients;
+	List<Ingredient> ingredients;
 	@Unique
 	private boolean fabric_requiresTesting = false;
 
