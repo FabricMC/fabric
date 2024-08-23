58fa56522bfd15fa22effd2bab4e18de92efc4c7
TheDeathlyCow <thedeathlycow@gmail.com>
2024-08-22 17:01:03 -1000

    add javadoc

--- fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/ServerLivingEntityEvents.java
+++ fabric-entity-events-v1/src/main/java/net/fabricmc/fabric/api/entity/event/v1/ServerLivingEntityEvents.java
@@ -42,6 +42,16 @@ public final class ServerLivingEntityEvents {
 		return true;
 	});
 
+	/**
+	 * An event that is called after an entity is damaged. This is fired from {@link LivingEntity#damage} after damage
+	 * is applied, or after that damage was blocked by a shield.
+	 * <p>
+	 * Damage dealt is the damage initially applied to the entity. Damage taken is the amount of damage the entity
+	 * actually took, after effects such as shields and extra freezing damage are applied. Damage taken does NOT include
+	 * damage reduction from armor and enchantments.
+	 * <p>
+	 * This event is not fired if the entity was killed by the damage.
+	 */
 	public static final Event<AfterDamage> AFTER_DAMAGE = EventFactory.createArrayBacked(AfterDamage.class, callbacks -> (entity, source, damageDealt, damageTaken, blocked) -> {
 		for (AfterDamage callback : callbacks) {
 			callback.afterDamage(entity, source, damageDealt, damageTaken, blocked);
@@ -113,6 +123,16 @@ public final class ServerLivingEntityEvents {
 	@FunctionalInterface
 	public interface AfterDamage {
 
+		/**
+		 * Called after a living entity took damage, unless they were killed. The damage taken is given as damage taken
+		 * before armor or enchantments are applied.
+		 *
+		 * @param entity the entity that was damaged
+		 * @param source the source of the damage
+		 * @param damageDealt the amount of damage initially dealt
+		 * @param damageTaken the amount of damage actually taken by the entity, before armor and enchantment effects
+		 * @param blocked whether the damage was blocked by a shield
+		 */
 		void afterDamage(LivingEntity entity, DamageSource source, float damageDealt, float damageTaken, boolean blocked);
 	}
 
