5e80e69b9ed73376a15c54059716361858f0f2ac
IThundxr <me@ithundxr.dev>
2024-09-11 20:11:55 -0400

    run formatter

--- fabric-api-dev/src/main/java/net/fabricmc/fabric/mixin/BlockAndItemMixin.java
+++ fabric-api-dev/src/main/java/net/fabricmc/fabric/mixin/BlockAndItemMixin.java
@@ -1,16 +1,30 @@
+/*
+ * Copyright (c) 2024 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-
-import net.fabricmc.fabric.FabricDev;
-
-import net.minecraft.block.Block;
-
-import net.minecraft.item.Item;
-
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+import net.fabricmc.fabric.FabricDev;
 
 @Mixin({Block.class, Item.class})
 public class BlockAndItemMixin {
