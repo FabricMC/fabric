9724bae3894dd04552877e8fb26581d802fac834
Kevinthegreat <92656833+kevinthegreat1@users.noreply.github.com>
2024-09-23 14:37:42 -0400

    Add HudRenderEvents

---
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/api/client/rendering/v1/HudRenderEvents.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.fabric.api.client.rendering.v1;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+/**
+ * Events for rendering elements on the HUD.
+ * <p>
+ * These events will not be called if the HUD is hidden with F1.
+ */
+public class HudRenderEvents {
+	/**
+	 * Called at the start of HUD rendering, right before anything is rendered.
+	 */
+	public static final Event<HudRenderStage> START = createEventForStage();
+
+	/**
+	 * Called after misc overlays (vignette, spyglass, and powder snow) have been rendered, and before the crosshair is rendered.
+	 */
+	public static final Event<HudRenderStage> AFTER_MISC_OVERLAYS = createEventForStage();
+
+	/**
+	 * Called after the hotbar, status bars, and experience bar have been rendered, and before the status effects overlays are rendered.
+	 */
+	public static final Event<HudRenderStage> AFTER_MAIN_HUD = createEventForStage();
+
+	/**
+	 * Called after the debug HUD, scoreboard, overlay message (action bar), and title and subtitle have been rendered, and before the {@link net.minecraft.client.gui.hud.ChatHud} is rendered.
+	 */
+	public static final Event<HudRenderStage> BEFORE_CHAT = createEventForStage();
+
+	/**
+	 * Called after the entire HUD is rendered.
+	 */
+	public static final Event<HudRenderStage> LAST = createEventForStage();
+
+	private static Event<HudRenderStage> createEventForStage() {
+		return EventFactory.createArrayBacked(HudRenderStage.class, listeners -> (context, tickDelta) -> {
+			for (HudRenderStage listener : listeners) {
+				listener.onRender(context, tickDelta);
+			}
+		});
+	}
+
+	@FunctionalInterface
+	public interface HudRenderStage {
+		/**
+		 * Called sometime during a specific HUD render stage.
+		 *
+		 * @param context     The {@link DrawContext} instance
+		 * @param tickCounter The {@link RenderTickCounter} instance
+		 */
+		void onRender(DrawContext context, RenderTickCounter tickCounter);
+	}
+}
--- fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/InGameHudMixin.java
+++ fabric-rendering-v1/src/client/java/net/fabricmc/fabric/mixin/client/rendering/InGameHudMixin.java
@@ -19,16 +19,53 @@ package net.fabricmc.fabric.mixin.client.rendering;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.LayeredDrawer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.render.RenderTickCounter;
 
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderEvents;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
+	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addLayer(Lnet/minecraft/client/gui/LayeredDrawer$Layer;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 0))
+	private LayeredDrawer.Layer fabric$beforeStartAndAfterMiscOverlays(LayeredDrawer.Layer miscOverlaysLayer) {
+		return (context, tickCounter) -> {
+			HudRenderEvents.START.invoker().onRender(context, tickCounter);
+			miscOverlaysLayer.render(context, tickCounter);
+			HudRenderEvents.AFTER_MISC_OVERLAYS.invoker().onRender(context, tickCounter);
+		};
+	}
+
+	@ModifyArg(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addLayer(Lnet/minecraft/client/gui/LayeredDrawer$Layer;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 3))
+	private LayeredDrawer.Layer fabric$afterMainHud(LayeredDrawer.Layer mainHudLayer) {
+		return (context, tickCounter) -> {
+			mainHudLayer.render(context, tickCounter);
+			HudRenderEvents.AFTER_MAIN_HUD.invoker().onRender(context, tickCounter);
+		};
+	}
+
+	@ModifyArg(method = "<init>", slice = @Slice(from = @At(value = "NEW", target = "Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 1)), at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addLayer(Lnet/minecraft/client/gui/LayeredDrawer$Layer;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 5))
+	private LayeredDrawer.Layer fabric$beforeChat(LayeredDrawer.Layer beforeChatLayer) {
+		return (context, tickCounter) -> {
+			HudRenderEvents.BEFORE_CHAT.invoker().onRender(context, tickCounter);
+			beforeChatLayer.render(context, tickCounter);
+		};
+	}
+
+	@ModifyArg(method = "<init>", slice = @Slice(from = @At(value = "NEW", target = "Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 1)), at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/LayeredDrawer;addLayer(Lnet/minecraft/client/gui/LayeredDrawer$Layer;)Lnet/minecraft/client/gui/LayeredDrawer;", ordinal = 7))
+	private LayeredDrawer.Layer fabric$AfterSubtitlesHud(LayeredDrawer.Layer subtitlesHudLayer) {
+		return (context, tickCounter) -> {
+			subtitlesHudLayer.render(context, tickCounter);
+			HudRenderEvents.LAST.invoker().onRender(context, tickCounter);
+		};
+	}
+
 	@Inject(method = "render", at = @At(value = "TAIL"))
 	public void render(DrawContext drawContext, RenderTickCounter tickCounter, CallbackInfo callbackInfo) {
 		HudRenderCallback.EVENT.invoker().onHudRender(drawContext, tickCounter);
