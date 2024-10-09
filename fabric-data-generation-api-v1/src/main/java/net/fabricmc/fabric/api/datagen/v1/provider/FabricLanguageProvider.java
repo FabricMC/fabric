9339092e913d807d7adc95dd1c2cdf0fb8203c65
solonovamax <solonovamax@12oclockpoint.com>
2024-10-09 16:47:40 -0400

    Fix spotless issues

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricLanguageProvider.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricLanguageProvider.java
@@ -207,10 +207,10 @@ public abstract class FabricLanguageProvider implements DataProvider {
 		 * @param value    The value of the entry.
 		 */
 		default void add(StatType<?> statType, String value) {
-		    if (statType.getName() instanceof TranslatableTextContent translatableTextContent) {
-		        add(translatableTextContent.getKey(), value);
-	        } else { // fallback to Util.createTranslationKey formatting (should it error instead?)
-			    add(Util.createTranslationKey("stat_type", Registries.STAT_TYPE.getId(statType)), value);
+			if (statType.getName() instanceof TranslatableTextContent translatableTextContent) {
+				add(translatableTextContent.getKey(), value);
+			} else { // fallback to Util.createTranslationKey formatting (should it error instead?)
+				add(Util.createTranslationKey("stat_type", Registries.STAT_TYPE.getId(statType)), value);
 			}
 		}
 
