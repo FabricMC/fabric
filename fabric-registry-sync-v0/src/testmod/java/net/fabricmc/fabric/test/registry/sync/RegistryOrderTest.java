eb109c5120378b968e05ae50f605aa1425cd1152
PersonTheCat <professionallyperson@gmail.com>
2024-10-15 08:11:48 -0500

    Add missing license header

--- fabric-registry-sync-v0/src/testmod/java/net/fabricmc/fabric/test/registry/sync/RegistryOrderTest.java
+++ fabric-registry-sync-v0/src/testmod/java/net/fabricmc/fabric/test/registry/sync/RegistryOrderTest.java
@@ -1,24 +1,37 @@
+/*
+ * Copyright (c) 2016, 2017, 2018, 2019 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.fabric.test.registry.sync;
 
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import com.mojang.logging.LogUtils;
-
 import com.mojang.serialization.Codec;
-
-import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
-import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
+import org.apache.commons.lang3.Validate;
+import org.slf4j.Logger;
 
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
 
-import org.apache.commons.lang3.Validate;
-import org.slf4j.Logger;
-
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
+import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
 
 public class RegistryOrderTest implements ModInitializer {
 	private static final Logger LOGGER = LogUtils.getLogger();
