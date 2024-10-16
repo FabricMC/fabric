Sinytra/FAPI-Mojmaps

Prevent vanilla clients from joining servers that require modded registry entries. (#3992)
* Prevent vanilla clients from joining servers that require modded registry entries

fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/impl/networking/server/ServerConfigurationNetworkAddon.java
@@ -19,10 +19,13 @@ package net.fabricmc.fabric.impl.networking.server;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+
+import org.jetbrains.annotations.Nullable;
 import net.minecraft.network.ConnectionProtocol;
 import net.minecraft.network.PacketSendListener;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.common.ClientboundPingPacket;
+import net.minecraft.network.protocol.common.custom.BrandPayload;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
@@ -42,6 +45,8 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 	private final MinecraftServer server;
 	private final ServerConfigurationNetworking.Context context;
 	private RegisterState registerState = RegisterState.NOT_SENT;
+	@Nullable
+	private String clientBrand = null;
 
 	public ServerConfigurationNetworkAddon(ServerConfigurationPacketListenerImpl handler, MinecraftServer server) {
 		super(ServerNetworkingImpl.CONFIGURATION, ((ServerCommonNetworkHandlerAccessor) handler).getConnection(), "ServerConfigurationNetworkAddon for " + handler.getOwner().getName());
@@ -51,6 +56,16 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 
 		// Must register pending channels via lateinit
 		this.registerPendingChannels((ChannelInfoHolder) this.connection, ConnectionProtocol.CONFIGURATION);
+	}
+
+	@Override
+	public boolean handle(CustomPacketPayload payload) {
+		if (payload instanceof BrandPayload brandCustomPayload) {
+			clientBrand = brandCustomPayload.brand();
+			return false;
+		}
+
+		return super.handle(payload);
 	}
 
 	@Override
@@ -165,6 +180,10 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 	@Override
 	public void sendPacket(Packet<?> packet, PacketSendListener callback) {
 		handler.send(packet, callback);
+	}
+
+	public @Nullable String getClientBrand() {
+		return clientBrand;
 	}
 
 	private enum RegisterState {

FabricMC/fabric

(cherry picked from commit 8759e7555a93fb6f3bdcebc0e788eff194289098)

fabric-networking-api-v1/src/main/java/net/fabricmc/fabric/impl/networking/server/ServerConfigurationNetworkAddon.java
@@ -20,8 +20,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.network.NetworkPhase;
 import net.minecraft.network.PacketCallbacks;
+import net.minecraft.network.packet.BrandCustomPayload;
 import net.minecraft.network.packet.CustomPayload;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.common.CommonPingS2CPacket;
@@ -44,6 +47,8 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 	private final MinecraftServer server;
 	private final ServerConfigurationNetworking.Context context;
 	private RegisterState registerState = RegisterState.NOT_SENT;
+	@Nullable
+	private String clientBrand = null;
 
 	public ServerConfigurationNetworkAddon(ServerConfigurationNetworkHandler handler, MinecraftServer server) {
 		super(ServerNetworkingImpl.CONFIGURATION, ((ServerCommonNetworkHandlerAccessor) handler).getConnection(), "ServerConfigurationNetworkAddon for " + handler.getDebugProfile().getName());
@@ -53,6 +58,16 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 
 		// Must register pending channels via lateinit
 		this.registerPendingChannels((ChannelInfoHolder) this.connection, NetworkPhase.CONFIGURATION);
+	}
+
+	@Override
+	public boolean handle(CustomPayload payload) {
+		if (payload instanceof BrandCustomPayload brandCustomPayload) {
+			clientBrand = brandCustomPayload.brand();
+			return false;
+		}
+
+		return super.handle(payload);
 	}
 
 	@Override
@@ -167,6 +182,10 @@ public final class ServerConfigurationNetworkAddon extends AbstractChanneledNetw
 	@Override
 	public void sendPacket(Packet<?> packet, PacketCallbacks callback) {
 		handler.send(packet, callback);
+	}
+
+	public @Nullable String getClientBrand() {
+		return clientBrand;
 	}
 
 	private enum RegisterState {