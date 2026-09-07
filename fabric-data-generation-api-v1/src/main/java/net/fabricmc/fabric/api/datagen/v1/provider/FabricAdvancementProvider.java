355f611dbdbc74af4cada6c1947c2ca595c0a41b
modmuss50 <modmuss50@gmail.com>
2026-09-07 18:15:19 +0100

    Fix javadoc build

--- fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricAdvancementProvider.java
+++ fabric-data-generation-api-v1/src/main/java/net/fabricmc/fabric/api/datagen/v1/provider/FabricAdvancementProvider.java
@@ -63,7 +63,7 @@ public abstract class FabricAdvancementProvider implements DataProvider {
 	/**
 	 * Implement this method to register advancements to generate use the consumer callback to register advancements.
 	 *
-	 * <p>Use {@link Advancement.Builder#save(Consumer, net.minecraft.resources.Identifier)} to help build advancements.
+	 * <p>Use {@link Advancement.Builder#build(Identifier)} to build advancements and pass them to the consumer.
 	 */
 	public abstract void generateAdvancement(HolderLookup.Provider registryLookup, Consumer<AdvancementHolder> consumer);
 
